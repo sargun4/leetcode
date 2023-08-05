8eb2103bf07ea985fb727ba8d0fbdc4fd62c520e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-05 20:01:00 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -4,37 +4,120 @@ using namespace std;
 
 // } Driver Code Ends
 
-// T-O(ElogE),S-O(E)
+//KRUSKAL
+class disjointset{
+    vector<int> rank,parent,size;
+public:
+    disjointset(int n){
+        rank.resize(n+1,0);
+        parent.resize(n+1);
+        size.resize(n+1);
+        for(int i=0;i<=n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+    }
+    int findUltimateParent(int node){
+        if(node==parent[node]){
+            return node;
+        }
+        return findUltimateParent(parent[node]);
+    }
+    void unionbyRank(int u,int v){
+        int ultParent_u=findUltimateParent(u);
+        int ultParent_v=findUltimateParent(v);
+        if(ultParent_u==ultParent_v){
+            return;
+        }
+        if(rank[ultParent_u]<rank[ultParent_v]){
+            parent[ultParent_u]=ultParent_v;
+        }
+        else if(rank[ultParent_u]>rank[ultParent_v]){
+            parent[ultParent_v]=ultParent_u;
+        }
+        else{
+            parent[ultParent_v]=ultParent_u;
+            rank[ultParent_u]++;
+        }
+    }
+
+    void unionbySize(int u,int v){
+        int ultParent_u=findUltimateParent(u);
+        int ultParent_v=findUltimateParent(v);
+        if(ultParent_u==ultParent_v){
+            return;
+        }
+        if(size[ultParent_u]<size[ultParent_v]){
+            parent[ultParent_u]=ultParent_v;
+            size[ultParent_v]+=size[ultParent_u];
+        }else{
+            parent[ultParent_v]=ultParent_u;
+            size[ultParent_u]+=size[ultParent_v];
+        }
+    }   
+};
+
 class Solution{
 	public:
-	//Function to find sum of weights of edges of the Minimum Spanning Tree.
     int spanningTree(int v, vector<vector<int>> adj[]){
-        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-        vector<int> vis(v,0);
-        //{wt,node}
-        pq.push({0,0});
-        int sum=0;
-        while(!pq.empty()){
-            auto it=pq.top();
-            pq.pop();
-            int wt=it.first;
-            int node=it.second;
-
-            if(vis[node]==1) continue;
-            //add it to mst
-            vis[node]=1;
-            sum+=wt;
-            for(auto it:adj[node]){
+        vector<pair<int,pair<int,int>>> edges;
+        for(int i=0;i<v;i++){
+            for(auto it: adj[i]){
                 int adjnode=it[0];
-                int edgeWt=it[1];
-                if(!vis[adjnode]){
-                    pq.push({edgeWt,adjnode});
-                }
+                int wt=it[1];
+                int node=i;
+                edges.push_back({wt,{node,adjnode}});
+
             }
         }
-        return sum;
+        disjointset ds(v);
+        sort(edges.begin(),edges.end());
+        int mstWt=0;
+        for(auto it:edges){
+            int wt=it.first;
+            int u=it.second.first;
+            int v=it.second.second;
+            if(ds.findUltimateParent(u)!=ds.findUltimateParent(v)){
+                mstWt+=wt;
+                ds.unionbyRank(u,v);
+            }
+        }
+        return mstWt;
     }
 };
+
+//PRIMS
+// // T-O(ElogE),S-O(E)
+// class Solution{
+// 	public:
+// 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
+//     int spanningTree(int v, vector<vector<int>> adj[]){
+//         priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+//         vector<int> vis(v,0);
+//         //{wt,node}
+//         pq.push({0,0});
+//         int sum=0;
+//         while(!pq.empty()){
+//             auto it=pq.top();
+//             pq.pop();
+//             int wt=it.first;
+//             int node=it.second;
+
+//             if(vis[node]==1) continue;
+//             //add it to mst
+//             vis[node]=1;
+//             sum+=wt;
+//             for(auto it:adj[node]){
+//                 int adjnode=it[0];
+//                 int edgeWt=it[1];
+//                 if(!vis[adjnode]){
+//                     pq.push({edgeWt,adjnode});
+//                 }
+//             }
+//         }
+//         return sum;
+//     }
+// };
 
 
 
