de1ee0b948d66362d24a027db5818ed32be9d47e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-05 22:30:42 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Rotten Oranges/rotten-oranges.java
@@ -0,0 +1,77 @@
+//{ Driver Code Starts
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while(T-->0)
+        {
+            String[] s = br.readLine().trim().split(" ");
+            int n = Integer.parseInt(s[0]);
+            int m = Integer.parseInt(s[1]);
+            int[][] grid = new int[n][m];
+            for(int i = 0; i < n; i++){
+                String[] S = br.readLine().trim().split(" ");
+                for(int j = 0; j < m; j++){
+                    grid[i][j] = Integer.parseInt(S[j]);
+                }
+            }
+            Solution obj = new Solution();
+            int ans = obj.orangesRotting(grid);
+            System.out.println(ans);
+        }
+    }
+}
+// } Driver Code Ends
+class Solution {
+    static class Pair {
+        int x;
+        int y;
+        Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+    public int orangesRotting(int[][] grid) {
+        Queue<Pair> q = new LinkedList<>();
+        int total = 0, rotten = 0, time = 0;
+		for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1 || grid[i][j] == 2) total++;
+                if (grid[i][j] == 2) q.offer(new Pair(i, j));
+            }
+        }
+        if (total == 0) return 0;		
+        while (! q.isEmpty()) {
+            int size = q.size();
+            rotten += size;
+            if (rotten == total) return time;
+            time++;
+            for (int i = 0; i < size; i++) {
+                Pair p = q.peek();
+                if (p.x + 1 < grid.length && grid[p.x + 1][p.y] == 1) {
+                    grid[p.x + 1][p.y] = 2;
+                    q.offer(new Pair(p.x + 1, p.y));
+                }
+                if (p.x - 1 >= 0 && grid[p.x - 1][p.y] == 1) {
+                    grid[p.x - 1][p.y] = 2;
+                    q.offer(new Pair(p.x - 1, p.y));
+                }
+                if (p.y + 1 < grid[0].length && grid[p.x][p.y + 1] == 1) {
+                    grid[p.x][p.y + 1] = 2;
+                    q.offer(new Pair(p.x, p.y + 1));
+                }
+                if (p.y - 1 >= 0 && grid[p.x][p.y - 1] == 1) {
+                    grid[p.x][p.y - 1] = 2;
+                    q.offer(new Pair(p.x, p.y - 1));
+                }
+                q.poll();
+            }
+        }
+        return -1;
+    }
+}
