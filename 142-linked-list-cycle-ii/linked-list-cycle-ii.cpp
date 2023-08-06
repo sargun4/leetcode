c4e68670b8b8c9402cad9407bc8d4cfb6ceed82e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-06 20:35:50 +0530

    Time: 8 ms (81.78%) | Memory: 7.7 MB (43.30%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -1,19 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
-        if(head==NULL||head->next==NULL) return NULL;
-        ListNode*slow=head,*fast=head,*entry=head;
-        while(fast->next && fast->next->next){
+    ListNode*detectCycle(ListNode *head) {
+        if(head==NULL|| head->next==NULL){
+            return NULL;
+        }
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next!=NULL){
             slow=slow->next;
             fast=fast->next->next;
-            if(slow==fast){ //cycle exists
-                while(slow!=entry){ //found entry location
-                    slow=slow->next;
-                    entry=entry->next;
-                }
-                return entry;
-            }
+            if(slow==fast) break;
         }
-        return NULL;
+        if(slow!=fast) return NULL;
+        ListNode* p=head;
+        while(p!=slow){
+            p=p->next;
+            slow=slow->next;
+        }
+        return p;
     }
 };
+// class Solution {
+// public:
+//     ListNode *detectCycle(ListNode *head) {
+//         if(head==NULL||head->next==NULL) return NULL;
+//         ListNode*slow=head,*fast=head,*entry=head;
+//         while(fast->next && fast->next->next){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//             if(slow==fast){ //cycle exists
+//                 while(slow!=entry){ //found entry location
+//                     slow=slow->next;
+//                     entry=entry->next;
+//                 }
+//                 return entry;
+//             }
+//         }
+//         return NULL;
+//     }
+// };
