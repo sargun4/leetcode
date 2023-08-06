2dc49d4a9fe07998fa771ad1867381cd7d65159f
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-06 19:59:40 +0530

    Time: 19 ms (97.03%) | Memory: 17.2 MB (7.88%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,120 +1,155 @@
-//bfs
+#include <bits/stdc++.h>
+using namespace std;
+//dfs
 class Solution {
-public:
-    int findCircleNum(vector<vector<int>>& M) {
-        int n=M.size();
-        if(n==0)return n;
-        int circles=0;
-        vector<bool> visited(n,false);  
-        for(int i=0;i<n;i++){
-            if(visited[i])continue;
-            queue<int> q1;
-            q1.push(i);
-            circles++;
-           while(!q1.empty()){               
-           int cur=q1.front();
-           q1.pop();
-           visited[cur]=true;
-               for(int j=0;j<M[cur].size();j++){
-                    if(visited[j]==false&&M[cur][j]==1){
-                    q1.push(j);
-                    visited[j]=true;
-                    }
-               }
+    void dfs(unordered_map<int,vector<int>> &adj,int u,vector<bool> &vis){
+        vis[u]=true;
+        for(int &v:adj[u]){
+            if(!vis[v]){
+                dfs(adj,v,vis);
             }
         }
-        return circles;
+    }
+public:
+    int findCircleNum(vector<vector<int>>& isConnected){
+        int n=isConnected.size();
+        unordered_map<int,vector<int>> adj;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(isConnected[i][j]==1){
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+        vector<bool> vis(n,false);
+        int ctr=0;
+        for(int i=0;i<n;i++){
+            if(!vis[i]){
+                ctr++;
+                dfs(adj,i,vis);
+            }
+        }
+        return ctr;
     }
 };
-// Time complexity:
-// DFS - O(N+M)
-// BFS - O(N+M)
-// N - Number of Vertices.
-// M - Number of Edges.
-
-// Space complexity:
-// DFS - O(H)
-// BFS - O(W)
-// H - Maximum Height of the Graph
-// W - Maximum Width of the Graph
-// //dfs
+// //bfs
 // class Solution {
 // public:
-// void soln(vector<int>adj[],int node,vector<bool>&vis){
-//     vis[node] = 1;
-//     for(auto i:adj[node]){
-//         if(!vis[i]){
-//             soln(adj,i,vis);
-//         }
-//     }
-// }
-//     int findCircleNum(vector<vector<int>>& isConnected) {
-//         int n = isConnected.size();
-//         vector<int>adj[n];
-//         for(int i = 0;i<n;i++){
-//             for(int j = 0;j<n;j++){
-//                 if(isConnected[i][j]){
-//                 adj[i].push_back(j);
-//                 adj[j].push_back(i);
-//                 }
-//             }
-//         }
-//         vector<bool>vis(n,0);
-//         int ans= 0;
-//         for(int i = 0;i<n;i++){
-//             if(!vis[i]){
-//                 soln(adj,i,vis);
-//                 ans++;
-//             }
-//         }
-//         return ans;
-//     }
-// };
-
-// class Solution {
-// public:
-//     //It will be use to store groups
-//     vector<int> v;    
-//     //Find the leader of any group in which x lies
-//     //if not lie in any group then it is self leader
-//     int parent(int x){
-//         //self leader
-//         if(v[x]==-1) return x; 
-//         //find the leader of self parent
-//         return v[x]=parent(v[x]);
-//     }
-//     //Adding 2 friends in a group
-//     void _union(int a,int b){
-//         //find the leader of both a and b
-//         int p_a=parent(a),p_b=parent(b);
-//         //if already in same group, i.e leader of both of them are same then return
-//         if(p_a==p_b) return; 
-//         /*
-//          if both of them are from different group then add both the groups 
-//          and make a single common group
-//          We can do this by -> leader of 1st group is member of 2nd group 
-//          and now main leader of whole group is leader of 2nd member
-//         */ 
-//         v[p_a]=p_b; //v[p_a] will store the index of leader of whole group
-//     }
-//     int findCircleNum(vector<vector<int>>& M) { 
+//     int findCircleNum(vector<vector<int>>& M) {
 //         int n=M.size();
-//         v=vector<int> (n,-1);//there will be maximum n group, mark all as a leader   
-//         //making group
+//         if(n==0)return n;
+//         int circles=0;
+//         vector<bool> visited(n,false);  
 //         for(int i=0;i<n;i++){
-//             for(int j=0;j<n;j++){
-//                 if(M[i][j]){  //if i is friend of j, add them in a group{ 
-//                     //if i is in any group then add j in that group
-//                     //or vice-versa
-//                     _union(i,j);  //Add them in a group
-//                 }
+//             if(visited[i])continue;
+//             queue<int> q1;
+//             q1.push(i);
+//             circles++;
+//            while(!q1.empty()){               
+//            int cur=q1.front();
+//            q1.pop();
+//            visited[cur]=true;
+//                for(int j=0;j<M[cur].size();j++){
+//                     if(visited[j]==false&&M[cur][j]==1){
+//                     q1.push(j);
+//                     visited[j]=true;
+//                     }
+//                }
 //             }
 //         }
-//         int c=0; 
-//         //counting group
-//         for(int i=0;i<n;i++){
-//             if(v[i]==-1) c++; //counting total number of parents
-//         }
-//         return c; 
+//         return circles;
 //     }
 // };
+// // Time complexity:
+// // DFS - O(N+M)
+// // BFS - O(N+M)
+// // N - Number of Vertices.
+// // M - Number of Edges.
+
+// // Space complexity:
+// // DFS - O(H)
+// // BFS - O(W)
+// // H - Maximum Height of the Graph
+// // W - Maximum Width of the Graph
+// // //dfs
+// // class Solution {
+// // public:
+// // void soln(vector<int>adj[],int node,vector<bool>&vis){
+// //     vis[node] = 1;
+// //     for(auto i:adj[node]){
+// //         if(!vis[i]){
+// //             soln(adj,i,vis);
+// //         }
+// //     }
+// // }
+// //     int findCircleNum(vector<vector<int>>& isConnected) {
+// //         int n = isConnected.size();
+// //         vector<int>adj[n];
+// //         for(int i = 0;i<n;i++){
+// //             for(int j = 0;j<n;j++){
+// //                 if(isConnected[i][j]){
+// //                 adj[i].push_back(j);
+// //                 adj[j].push_back(i);
+// //                 }
+// //             }
+// //         }
+// //         vector<bool>vis(n,0);
+// //         int ans= 0;
+// //         for(int i = 0;i<n;i++){
+// //             if(!vis[i]){
+// //                 soln(adj,i,vis);
+// //                 ans++;
+// //             }
+// //         }
+// //         return ans;
+// //     }
+// // };
+
+// // class Solution {
+// // public:
+// //     //It will be use to store groups
+// //     vector<int> v;    
+// //     //Find the leader of any group in which x lies
+// //     //if not lie in any group then it is self leader
+// //     int parent(int x){
+// //         //self leader
+// //         if(v[x]==-1) return x; 
+// //         //find the leader of self parent
+// //         return v[x]=parent(v[x]);
+// //     }
+// //     //Adding 2 friends in a group
+// //     void _union(int a,int b){
+// //         //find the leader of both a and b
+// //         int p_a=parent(a),p_b=parent(b);
+// //         //if already in same group, i.e leader of both of them are same then return
+// //         if(p_a==p_b) return; 
+// //         /*
+// //          if both of them are from different group then add both the groups 
+// //          and make a single common group
+// //          We can do this by -> leader of 1st group is member of 2nd group 
+// //          and now main leader of whole group is leader of 2nd member
+// //         */ 
+// //         v[p_a]=p_b; //v[p_a] will store the index of leader of whole group
+// //     }
+// //     int findCircleNum(vector<vector<int>>& M) { 
+// //         int n=M.size();
+// //         v=vector<int> (n,-1);//there will be maximum n group, mark all as a leader   
+// //         //making group
+// //         for(int i=0;i<n;i++){
+// //             for(int j=0;j<n;j++){
+// //                 if(M[i][j]){  //if i is friend of j, add them in a group{ 
+// //                     //if i is in any group then add j in that group
+// //                     //or vice-versa
+// //                     _union(i,j);  //Add them in a group
+// //                 }
+// //             }
+// //         }
+// //         int c=0; 
+// //         //counting group
+// //         for(int i=0;i<n;i++){
+// //             if(v[i]==-1) c++; //counting total number of parents
+// //         }
+// //         return c; 
+// //     }
+// // };
