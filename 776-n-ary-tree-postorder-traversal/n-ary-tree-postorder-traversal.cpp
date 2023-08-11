231701683ee32069eda12a196b99b1146b31986b
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-11 22:19:27 +0530

    Time: 15 ms (81.18%) | Memory: 11.5 MB (72.22%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,30 @@
+/*
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+    Node() {}
+    Node(int _val) {
+        val = _val;
+    }
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+class Solution {
+public:
+    vector<int> res;
+    void traverse(Node* root) {
+        if(root == NULL) return;
+        for(auto node:root->children)
+            traverse(node);
+        res.push_back(root->val);
+    }
+    vector<int> postorder(Node* root) {
+        res.clear();
+        traverse(root);
+        return res;
+    }
+};
