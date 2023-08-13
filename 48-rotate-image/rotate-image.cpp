be035cf115c6f2cffa3c7e9cf25ec77ab7762404
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-14 02:44:19 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (47.78%) - LeetSync

---
+++ 48-rotate-image/rotate-image.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int m=matrix.size();
+        int n=m;
+        //transpose
+        for(int i=0;i<m;i++){
+            for(int j=i;j<n;j++){
+                swap(matrix[i][j],matrix[j][i]);
+            }
+        }        
+        //rev each row
+        for(int i=0;i<m;i++){
+            reverse(matrix[i].begin(),matrix[i].end());
+        }
+    }
+};
