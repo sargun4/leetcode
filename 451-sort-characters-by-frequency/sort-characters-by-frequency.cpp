671b8d2363ab00c753d701372ffeb62210d0061a
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-14 01:40:55 +0530

    Time: 4 ms (98.72%) | Memory: 8.9 MB (12.04%) - LeetSync

---
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    typedef pair<char,int> p;
+    struct lambda{
+        bool operator()(p &p1,p &p2){
+            return p1.second<p2.second; //max heap based on freq vals
+        }
+    };
+    string frequencySort(string s) {
+        priority_queue<p,vector<p>,lambda> pq;
+        unordered_map<char,int> map;
+        //freq updated in map
+        for(char &ch:s){
+            map[ch]++;
+        }
+        //fill in heap frm map
+        for(auto &it:map){
+            pq.push({it.first,it.second});
+        }
+        string res="";
+        while(!pq.empty()){
+            p temp=pq.top();
+            pq.pop();
+            res+=string(temp.second,temp.first);
+        }
+        return res;
+    }
+};
