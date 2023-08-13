f419d1969b7ace914988ec2587c725fbf6c6edd1
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-14 03:42:18 +0530

    Time: 31 ms (81.96%) | Memory: 19.7 MB (78.63%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,19 +1,54 @@
-#include <bits/stdc++.h>
-using namespace std;
+//without sorting
+//O(n*(k+26))
 class Solution {
 public:
+    string generate(string&word){
+        int arr[26]={0};
+        for(char &ch:word){
+            arr[ch-'a']++;
+        }
+        string newword="";
+        for(int i=0;i<26;i++){
+            int freq=arr[i];
+            if(freq>0){
+                newword+=string(freq,i+'a');
+            }
+        }
+        return newword;
+    }
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n=strs.size();
         vector<vector<string>> res;
         unordered_map<string,vector<string>> mp;
         for(int i=0;i<n;i++){
-            string temp=strs[i];
-            sort(begin(temp),end(temp));
-            mp[temp].push_back(strs[i]);
+            string word=strs[i];
+            string newword=generate(word);
+            mp[newword].push_back(word);
         }
-        for(auto it:mp){
+        for(auto &it:mp){
             res.push_back(it.second);
         }
         return res;
     }
 };
+// //using sorting
+// //O(n*klogk) where k is max size of string in input
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     vector<vector<string>> groupAnagrams(vector<string>& strs) {
+//         int n=strs.size();
+//         vector<vector<string>> res;
+//         unordered_map<string,vector<string>> mp;
+//         for(int i=0;i<n;i++){
+//             string temp=strs[i];
+//             sort(begin(temp),end(temp));
+//             mp[temp].push_back(strs[i]);
+//         }
+//         for(auto it:mp){
+//             res.push_back(it.second);
+//         }
+//         return res;
+//     }
+// };
