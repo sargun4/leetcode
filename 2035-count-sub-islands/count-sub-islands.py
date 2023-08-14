2b1fa7bcaf56a0410647058800ab01c7ca14f14c
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-15 02:52:22 +0530

    Time: 2633 ms (82.05%) | Memory: 120.6 MB (17.12%) - LeetSync

--- 2035-count-sub-islands/count-sub-islands.py
+++ 2035-count-sub-islands/count-sub-islands.py
@@ -4,12 +4,15 @@ class Solution:
         m,n=len(grid1),len(grid1[0])
         visit=set()
         def dfs(r,c):
+            #if out of bounds or grid2 cell is not island or already been visited
             if(r<0 or c<0 or r==m or c==n or grid2[r][c]==0 or (r,c) in visit):
                 return True
             visit.add((r,c))
             res=True
+            #not island cell
             if(grid1[r][c]==0):
                 res=False
+            #4 dirn
             res=dfs(r-1,c) and res
             res=dfs(r+1,c) and res
             res=dfs(r,c-1) and res
@@ -18,6 +21,7 @@ class Solution:
         ctr=0
         for r in range(m):
             for c in range(n):
-                if(grid2[r][c] and (r,c) not in visit and dfs(r,c)==True):
+                #if island cell and not yet been vis and dfs is true
+                if(grid2[r][c]==1 and (r,c) not in visit and dfs(r,c)==True):
                     ctr+=1
         return ctr
