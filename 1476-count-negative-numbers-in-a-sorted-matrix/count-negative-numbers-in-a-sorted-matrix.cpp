3cf154a27b4b38f3389e41c2fbaf3d3e178fb032
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-21 03:45:03 +0530

    Time: 12 ms (78.45%) | Memory: 10.5 MB (20.91%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,32 +1,53 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+// //O(m*n) -brute force
+// // class Solution {
+// // public:
+// //     int countNegatives(vector<vector<int>>& grid) {
+// //         int m=grid.size();
+// //         int n=grid[0].size();
+// //         int res=0;
+// //         for(int i=0;i<m;i++){
+// //             for(int j=0;j<n;j++){
+// //                 if(grid[i][j]<0){
+// //                     res++;
+// //                 }
+// //             }
+// //         }
+// //         return res;
+// //     }
+// // };
+
+// //O(m*logn)
 // class Solution {
 // public:
 //     int countNegatives(vector<vector<int>>& grid) {
 //         int m=grid.size();
 //         int n=grid[0].size();
 //         int res=0;
-//         for(int i=0;i<m;i++){
-//             for(int j=0;j<n;j++){
-//                 if(grid[i][j]<0){
-//                     res++;
-//                 }
-//             }
+//         for(auto &row:grid){
+//             int idx=upper_bound(begin(row),end(row),0,greater<int>())-begin(row);
+//             res+=(n-idx);            
 //         }
 //         return res;
 //     }
 // };
+
+//O(m+n)
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         int res=0;
-        for(auto &row:grid){
-            int idx=upper_bound(begin(row),end(row),0,greater<int>())-begin(row);
-            res+=(n-idx);            
+        int row=m-1;
+        int col=0;
+        while(row>=0 && col<n){
+            if(grid[row][col]>=0){
+                col++;;
+            }else{
+                res+=(n-col);
+                row--;
+            }
         }
         return res;
     }
 };
-
