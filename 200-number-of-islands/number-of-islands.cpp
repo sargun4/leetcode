9b861ab01ef0a0c210eb01fedf30ea2f84dd4392
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-28 03:13:45 +0530

    Time: 26 ms (90.15%) | Memory: 12.3 MB (66.72%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,39 +1,76 @@
-//bfs
+//dfs
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
-    //T: O(row * col)
-    //S: O(col)
-    int numIslands(vector<vector<char>>& grid) {
-        if(grid.empty())
-            return 0;
-        int island_ctr = 0;
-        for(int m = 0; m < grid.size(); ++m) {
-            for(int n = 0; n < grid[0].size(); ++n) {
-                if(grid[m][n] == '1') {
-                    ++island_ctr;
-                    std::queue<pair<int, int>> bfs_que;//<m, col>
-                    grid[m][n] = '2';
-                    bfs_que.push(make_pair(m, n));
-                    while(!bfs_que.empty()) {
-                        std::pair<int, int> visited_node = bfs_que.front();
-                        bfs_que.pop();
-                        for(auto d : dir) {
-                            int next_row = visited_node.first + d.first;
-                            int next_col = visited_node.second + d.second;
-                            if(next_row >= 0 && next_row < grid.size() && next_col >= 0 && next_col < grid[0].size() && grid[next_row][next_col] == '1') {
-                                grid[next_row][next_col] = '2';
-                                bfs_que.push(make_pair(next_row, next_col));
-                            }
-                        }
-                    }
-                }   
-            }  
+    bool isvalid(int i,int j,int n,int m,vector<vector<char>>& grid){
+        if(i>=0 && i<n && j>=0 && j<m && grid[i][j]=='1'){
+            return true;
         }
-        return island_ctr;
+        return false;
     }
-private:
-    vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};//<m, col>
+    void countIslands(int i,int j,int n,int m,vector<vector<char>>& grid){
+        grid[i][j]='0';
+        if(isvalid(i+1,j,n,m,grid))
+            countIslands(i+1,j,n,m,grid);
+        if(isvalid(i-1,j,n,m,grid))
+            countIslands(i-1,j,n,m,grid);
+        if(isvalid(i,j+1,n,m,grid))
+            countIslands(i,j+1,n,m,grid);
+        if(isvalid(i,j-1,n,m,grid))
+            countIslands(i,j-1,n,m,grid);
+    }
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1'){
+                    ans++;
+                    countIslands(i,j,n,m,grid);
+                }
+            }
+        }
+        return ans;
+    }
 };
+// //bfs
+// class Solution {
+// public:
+//     //T: O(row * col)
+//     //S: O(col)
+//     int numIslands(vector<vector<char>>& grid) {
+//         if(grid.empty())
+//             return 0;
+//         int island_ctr = 0;
+//         for(int m = 0; m < grid.size(); ++m) {
+//             for(int n = 0; n < grid[0].size(); ++n) {
+//                 if(grid[m][n] == '1') {
+//                     ++island_ctr;
+//                     std::queue<pair<int, int>> bfs_que;//<m, col>
+//                     grid[m][n] = '2';
+//                     bfs_que.push(make_pair(m, n));
+//                     while(!bfs_que.empty()) {
+//                         std::pair<int, int> visited_node = bfs_que.front();
+//                         bfs_que.pop();
+//                         for(auto d : dir) {
+//                             int next_row = visited_node.first + d.first;
+//                             int next_col = visited_node.second + d.second;
+//                             if(next_row >= 0 && next_row < grid.size() && next_col >= 0 && next_col < grid[0].size() && grid[next_row][next_col] == '1') {
+//                                 grid[next_row][next_col] = '2';
+//                                 bfs_que.push(make_pair(next_row, next_col));
+//                             }
+//                         }
+//                     }
+//                 }   
+//             }  
+//         }
+//         return island_ctr;
+//     }
+// private:
+//     vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};//<m, col>
+// };
 
 // class Solution {
 // public:
