82162d2b86b99c636e0b1a70a7e22241b8d62ff6
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-08-28 15:35:41 +0530

    Time: 3 ms (36.88%) | Memory: 6.7 MB (97.83%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,36 +1,71 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
+// class MyStack {
+// public:
+//     MyStack() {
+        
+//     }
+//     queue<int> q1;
+//     queue<int> q2;
+//     void push(int x) {
+//         q2.push(x);
+//         while(!q1.empty()){
+//             q2.push(q1.front());
+//             q1.pop();
+//         }
+//         swap(q1,q2);
+//     }
+    
+//     int pop() {
+//         int res=q1.front();
+//         q1.pop();
+//         return res;
+//     }
+    
+//     int top() {
+//         return q1.front();
+//     }
+    
+//     bool empty() {
+//         return q1.empty();
+//     }
+// };
+
+//O(n)
 class MyStack {
 public:
     MyStack() {
-        
     }
-    queue<int> q1;
-    queue<int> q2;
+    queue<int> q;
     void push(int x) {
-        q2.push(x);
-        while(!q1.empty()){
-            q2.push(q1.front());
-            q1.pop();
+        q.push(x);
+        int n=q.size();
+        for(int i=0;i<n-1;i++){
+            q.push(q.front());
+            q.pop();
         }
-        swap(q1,q2);
     }
-    
     int pop() {
-        int res=q1.front();
-        q1.pop();
-        return res;
+        int x=q.front();
+        q.pop();
+        return x;
     }
-    
     int top() {
-        return q1.front();
+        return q.front();
     }
-    
     bool empty() {
-        return q1.empty();
+        return q.empty();
     }
 };
 
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
 /**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();
