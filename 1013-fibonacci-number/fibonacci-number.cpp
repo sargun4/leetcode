ff9c0272cbec0fad7c91f1157264c14006ba342a
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-09-02 23:34:08 +0530

    Time: 2 ms (73.85%) | Memory: 6 MB (26.25%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+//memoization ; BOTTOMUP
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+    int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
