a73fdd2c0cfdddf0367cd4d30ea6a39345636b48
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-09-02 23:41:54 +0530

    Time: 2 ms (73.85%) | Memory: 5.9 MB (58.29%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -19,21 +19,39 @@ using namespace std;
 //     }
 // };
 
-// BOTTOMUP-tabulation-O(n)
+// // BOTTOMUP-tabulation-O(n)
+// class Solution {
+// public:  
+//     int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+//         vector<int> dp(n+1,-1);
+//         dp[0]=0;
+//         dp[1]=1;
+//         for(int i=2;i<=n;i++){
+//             dp[i]=dp[i-1]+dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// };
+
+// //rec-O(2^n)
+
+
 class Solution {
 public:  
     int fib(int n) {
         if(n<=1){
             return n;
         }
-        vector<int> dp(n+1,-1);
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+        int c;
+        int a=0,b=1;
+        for(int i=1;i<n;i++){
+            c=a+b;
+            a=b;
+            b=c;
         }
-        return dp[n];
+        return c;
     }
 };
-
-//rec-O(2^n)
