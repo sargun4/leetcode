b1dc6649fdb0166ebb2891a50a77af71a428f80e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-09-17 19:40:00 +0530

    Time: 9 ms (95.19%) | Memory: 43.5 MB (75.93%) - LeetSync

---
+++ 877-shortest-path-visiting-all-nodes/shortest-path-visiting-all-nodes.java
@@ -0,0 +1,46 @@
+class Solution {
+	public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        
+		if(n == 1 || n == 0)
+			return 0;
+        
+        int allVisitedState = (1 << n) - 1;
+        Queue<int []> que = new LinkedList<>();
+        
+		int [][] visited = new int [n][allVisitedState+1];
+
+		for(int i=0; i<graph.length; i++) {
+			que.add(new int [] {i, 1 << i});
+            visited[i][(1 << i)] = 1;  
+		}
+
+
+
+		int shortestPath = 0;
+		while(!que.isEmpty()){
+			int size = que.size();
+			shortestPath++;
+			for(int i=0; i<size; i++){
+                
+				int [] curr  = que.poll();
+				int currNode = curr[0];
+				int currMask = curr[1];
+                
+				for(int adj : graph[currNode]){
+					int nextMask = currMask | (1 << adj);
+
+					if(visited[adj][nextMask] == 1)
+                        continue;
+                    
+                    if(nextMask == allVisitedState)
+						return shortestPath;
+                    
+                    visited[adj][nextMask] = 1;
+					que.add(new int [] {adj, nextMask});
+				}
+			}
+		}
+		return -1;
+	}
+}
