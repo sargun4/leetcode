55fd7fbd7fbaf1ade8f4c834e21c688a4dee5a3e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-09-26 21:45:54 +0530

    Time: 66 ms (7.53%) | Memory: 16.4 MB (18.14%) - LeetSync

--- 316-remove-duplicate-letters/remove-duplicate-letters.py
+++ 316-remove-duplicate-letters/remove-duplicate-letters.py
@@ -6,21 +6,21 @@ class Solution:
                 return c + self.removeDuplicateLetters(suffix.replace(c, ''))
         return ''
 
-def removeDuplicateLetters(self, s):
-    result = ''
-    while s:
-        i = min(map(s.rindex, set(s)))
-        c = min(s[:i+1])
-        result += c
-        s = s[s.index(c):].replace(c, '')
-    return result
+# def removeDuplicateLetters(self, s):
+#     result = ''
+#     while s:
+#         i = min(map(s.rindex, set(s)))
+#         c = min(s[:i+1])
+#         result += c
+#         s = s[s.index(c):].replace(c, '')
+#     return result
 
-def removeDuplicateLetters(self, s):
-    rindex = {c: i for i, c in enumerate(s)}
-    result = ''
-    for i, c in enumerate(s):
-        if c not in result:
-            while c < result[-1:] and i < rindex[result[-1]]:
-                result = result[:-1]
-            result += c
-    return result
+# def removeDuplicateLetters(self, s):
+#     rindex = {c: i for i, c in enumerate(s)}
+#     result = ''
+#     for i, c in enumerate(s):
+#         if c not in result:
+#             while c < result[-1:] and i < rindex[result[-1]]:
+#                 result = result[:-1]
+#             result += c
+#     return result
