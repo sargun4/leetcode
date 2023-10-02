6a18241aa687bf6d317f24ad9b4ae041ce7afd96
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-03 05:15:13 +0530

    Time: 0 ms (100.00%) | Memory: 6.3 MB (23.25%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,13 +1,50 @@
-#include <bits/stdc++.h>
-using namespace std;
-vector<int> dp(46,-1);
+// #include <bits/stdc++.h>
+// using namespace std;
+// //memo
+// vector<int> dp(46,-1);
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(dp[n]!=-1) return dp[n];
+//         if(n<=2){
+//             return dp[n]=n;
+//         }
+//         return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+//     }
+// };
+
+// //Tabulation-bottom up
+// // arr[i]- no of ways to climb i stairs
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         if(n<=2){
+//             return n;
+//         }
+//         vector<int> arr(n+1,0);
+//         arr[1]=1;
+//         arr[2]=2;
+//         for(int i=3;i<=n;i++){
+//             arr[i]=arr[i-1]+arr[i-2];
+//         }
+//         return arr[n];
+//     }
+// };
+
+
 class Solution {
 public:
     int climbStairs(int n) {
-        if(dp[n]!=-1) return dp[n];
         if(n<=2){
-            return dp[n]=n;
+            return n;
         }
-        return dp[n]=climbStairs(n-1)+climbStairs(n-2);
+        int a=1,b=2,c=3;
+        for(int i=3;i<=n;i++){
+            c=a+b;
+            int tempb=b;
+            b=c;
+            a=tempb;
+        }
+        return c;
     }
 };
