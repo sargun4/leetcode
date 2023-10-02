7eba17b0f20c1f5eda7c0df43ca2c3b00378966c
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-03 05:01:15 +0530

    Time: 0 ms (100.00%) | Memory: 6.4 MB (6.76%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,23 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
-// //memoization ; 
-// class Solution {
-// public:
-//     int solve(int n,vector<int> &dp){
-//         if(n<=1) return n;
-//         if(dp[n]!=-1){
-//             return dp[n];
-//         }
-//         return dp[n]=solve(n-1,dp)+solve(n-2,dp);
-//     }
-//     int fib(int n) {
-//         if(n<=1){
-//             return n;
-//         }
-//         vector<int> dp(n+1,-1);
-//         return solve(n,dp);
-//     }
-// };
+//memoization ; 
+class Solution {
+public:
+    int solve(int n,vector<int> &dp){
+        if(n<=1) return n;
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+    }
+    int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        vector<int> dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
 
 // // BOTTOMUP-tabulation-O(n)
 // class Solution {
@@ -39,21 +39,20 @@ using namespace std;
 // //rec-O(2^n)
 
 
-
-// T-O(n),S-O(1)
-class Solution {
-public:  
-    int fib(int n) {
-        if(n<=1){
-            return n;
-        }
-        int c;
-        int a=0,b=1;
-        for(int i=1;i<n;i++){
-            c=a+b;
-            a=b;
-            b=c;
-        }
-        return c;
-    }
-};
+// // T-O(n),S-O(1)
+// class Solution {
+// public:  
+//     int fib(int n) {
+//         if(n<=1){
+//             return n;
+//         }
+//         int c;
+//         int a=0,b=1;
+//         for(int i=1;i<n;i++){
+//             c=a+b;
+//             a=b;
+//             b=c;
+//         }
+//         return c;
+//     }
+// };
