e72f302c2a174a7bbaba86af0e5d4965b2ddb17e
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-03 06:07:23 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (35.88%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -33,34 +33,57 @@
 
 #include <bits/stdc++.h>
 using namespace std;
-//O(n)
-//tab
+// //O(n)
+// //tab
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1) return nums[0];
+//         vector<int> dp(n+1,0);
+//         //dp[i]=money stolen when we hv traversed/been to i houses
+//         //case1-take 1st house-hence skip last house
+//         dp[0]=0;
+//         for(int i=1;i<=n-1;i++){
+//             int skip=dp[i-1];
+//             int take=nums[i-1]+((i-2>=0) ?dp[i-2]:0);
+//             dp[i]=max(skip,take);
+//         }
+//         int res1=dp[n-1];
+//         dp.clear();
+//         //case1-skip 1st house-hence we can take last house- since theyre placed in circular fashion
+//         dp[0]=0,
+//         dp[1]=0;//skip 1st house
+//         for(int i=2;i<=n;i++){
+//             int skip=dp[i-1];
+//             int take=nums[i-1]+((i-2>=0) ?dp[i-2]:0);
+//             dp[i]=max(skip,take);
+//         }
+//         int res2=dp[n];
+//         return max(res1,res2);
+        
+//     }
+// };
 class Solution {
 public:
+    int solve(vector<int>& nums,int l,int r){
+        int prev=0;
+        int prevprev=0;
+        for(int i=l;i<=r;i++){
+            int skip=prev;
+            int take=nums[i]+prevprev;
+            int temp=max(skip,take);
+            prevprev=prev;
+            prev=temp;
+        }
+        return prev;
+    }
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==1) return nums[0];
-        vector<int> dp(n+1,0);
-        //dp[i]=money stolen when we hv traversed/been to i houses
-        //case1-take 1st house-hence skip last house
-        dp[0]=0;
-        for(int i=1;i<=n-1;i++){
-            int skip=dp[i-1];
-            int take=nums[i-1]+((i-2>=0) ?dp[i-2]:0);
-            dp[i]=max(skip,take);
-        }
-        int res1=dp[n-1];
-        dp.clear();
-        //case1-skip 1st house-hence we can take last house- since theyre placed in circular fashion
-        dp[0]=0,
-        dp[1]=0;//skip 1st house
-        for(int i=2;i<=n;i++){
-            int skip=dp[i-1];
-            int take=nums[i-1]+((i-2>=0) ?dp[i-2]:0);
-            dp[i]=max(skip,take);
-        }
-        int res2=dp[n];
-        return max(res1,res2);
-        
+        if(n==2) return max(nums[0],nums[1]);
+        int take1sthouse=solve(nums,0,n-2);
+        int skip1sthouse=solve(nums,1,n-1);
+        return max(take1sthouse,skip1sthouse);
     }
 };
