7f7b7559e733b4d47bd1b646f99531970300f7cd
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-05 05:02:20 +0530

    Time: 49 ms (54.22%) | Memory: 48.7 MB (43.25%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,20 +1,56 @@
-#include <bits/stdc++.h>
-using namespace std;
+// // #include <bits/stdc++.h>
+// // using namespace std;
+
+// // //memoization- O()
+// // class Solution {
+// // public:
+// //     int dp[10001];
+// //     bool solve(vector<int> &nums,int n,int idx){
+// //         if(idx==n-1) return true;
+// //         // if(idx>=n) return true;
+// //         if(dp[idx]!=-1) return dp[idx];
+// //         for(int i=1;i<=nums[idx];i++){
+// //             if(solve(nums,n,idx+i)) return dp[idx] = true;
+// //         }
+// //         return dp[idx] = false;
+// //     }
+// //     bool canJump(vector<int>& nums) {
+// //         int n=nums.size();
+// //         memset(dp,-1,sizeof(dp));
+// //         return solve(nums,n,0);
+// //     }
+// // };
+
+// // Tabulation
+// class Solution {
+// public:
+//     bool canJump(vector<int>& nums) {
+//         int n=nums.size();
+//         vector<bool> dp(n,false);
+//         // dp[i]- True means that we can reach idx i.
+//         dp[0] = true;
+//         for(int i=1;i<n;i++){
+//             for(int j=0;j<i;j++){
+//                 if(dp[j] && j+nums[j]>=i){
+//                     dp[i] = true;
+//                     break;
+//                 }
+//             }
+//         }
+//         return dp[n-1];
+//     }
+// };
 class Solution {
 public:
-    int dp[10001];
-    bool solve(vector<int> &nums,int n,int idx){
-        if(idx==n-1) return true;
-        // if(idx>=n) return true;
-        if(dp[idx]!=-1) return dp[idx];
-        for(int i=1;i<=nums[idx];i++){
-            if(solve(nums,n,idx+i)) return dp[idx] = true;
-        }
-        return dp[idx] = false;
-    }
     bool canJump(vector<int>& nums) {
         int n=nums.size();
-        memset(dp,-1,sizeof(dp));
-        return solve(nums,n,0);
+        int maxreachableidx=0;
+        for(int i=0;i<n;i++){
+            if(i>maxreachableidx){
+                return false;
+            }
+            maxreachableidx = max(maxreachableidx,i+nums[i]);
+        }
+        return true;
     }
 };
