89647248f6b6d15fbc4bb8ef370abe6527deb594
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-24 02:50:10 +0530

    Time: 31 ms (16.15%) | Memory: 31.7 MB (21.82%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int n=nums.size();
+        int sum=0;
+        for(int &x:nums){
+            sum+=x;
+        }
+        int cs=0;
+        for(int i=0;i<n;i++){
+            int ls=cs;
+            int rs=sum-cs-nums[i];
+            if(ls==rs) return i;
+            cs+=nums[i];
+        }
+        return -1;
+    }
+};
