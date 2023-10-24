96dcf8a04b00b7c088e6af6c28da0160edba71a7
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-24 18:53:02 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (60.38%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.java
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> list=new ArrayList<>();
+        inorder(root,list);
+        return list.get(k-1);
+    }
+    private void inorder(TreeNode root, List<Integer> list){
+        if(root==null) return;
+        inorder(root.left, list);
+        list.add(root.val);
+        inorder(root.right, list);
+    }
+}
+
+// class Solution {
+//     public int kthSmallest(TreeNode root, int k) {
+//         int count = countNodes(root.left);
+//         if (k <= count) {
+//             return kthSmallest(root.left, k);
+//         } else if (k > count + 1) {
+//             return kthSmallest(root.right, k-1-count); // 1 is counted as current node
+//         }
+//         return root.val;
+//     }
+    
+//     public int countNodes(TreeNode root) {
+//         if (root == null) return 0;
+//         return 1 + countNodes(root.left) + countNodes(root.right);
+//     }
+// }
