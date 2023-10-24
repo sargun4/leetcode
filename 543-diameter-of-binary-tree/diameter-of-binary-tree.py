74a84bab5690f696c31eb54b23ae810e336c66ae
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-10-24 19:40:22 +0530

    Time: 43 ms (85.74%) | Memory: 18.5 MB (93.60%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -1,18 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
-    def diameterOfBinaryTree(self, root: TreeNode) -> int:
-        def height(root):
-            nonlocal diameter
-            if not root:
-                return 0
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        res=[0]
+        def dfs(root):
+            if not root: 
+                return -1
+            l = dfs(root.left)
+            r = dfs(root.right)
+            res[0]=max(res[0],2+l+r)
+            return max(l,r)+1
+        dfs(root)
+        return res[0]
+
             
-            left = height(root.left)
-            right = height(root.right)
-            diameter = max(diameter, left + right)
-            return max(left, right) + 1
+
+
+# class Solution:
+#     def diameterOfBinaryTree(self, root: TreeNode) -> int:
+#         def height(root):
+#             nonlocal diameter
+#             if not root:
+#                 return 0
+            
+#             left = height(root.left)
+#             right = height(root.right)
+#             diameter = max(diameter, left + right)
+#             return max(left, right) + 1
         
-        diameter = 0
-        height(root)
-        return diameter
+#         diameter = 0
+#         height(root)
+#         return diameter
 
 # class Solution:
 #     def ht(self, root):
