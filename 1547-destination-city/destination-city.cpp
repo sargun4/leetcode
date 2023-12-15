b01e294071680f92d4c9e46f1706bbdc0844017f
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-15 18:30:15 +0530

    Time: 13 ms (29.07%) | Memory: 10.8 MB (85.89%) - LeetSync

--- 1547-destination-city/destination-city.cpp
+++ 1547-destination-city/destination-city.cpp
@@ -1,17 +1,37 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+
+// class Solution {
+// public:
+//     string destCity(vector<vector<string>>& paths) {
+//         unordered_map<string,int> map;
+//         for(auto &path:paths){
+//             string src=path[0];
+//             map[src]=1;
+//         } 
+//         for(auto &path:paths){
+//             string dest=path[1];
+//             if(map[dest]!=1){
+//                 return dest;
+//             }
+//         } 
+//         return "";
+//     }
+// };
 #include <bits/stdc++.h>
 using namespace std;
 
 class Solution {
 public:
     string destCity(vector<vector<string>>& paths) {
-        unordered_map<string,int> map;
+        unordered_set<string> set;
         for(auto &path:paths){
             string src=path[0];
-            map[src]=1;
+            set.insert(src); //outgoin edge
         } 
         for(auto &path:paths){
             string dest=path[1];
-            if(map[dest]!=1){
+            if(set.find(dest)==set.end()){
                 return dest;
             }
         } 
