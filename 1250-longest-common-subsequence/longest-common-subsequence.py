f2bd9723288eca567ccd46cff5c86a81ac21dbf0
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-16 20:03:15 +0530

    Time: 641 ms (57.54%) | Memory: 42.3 MB (41.47%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.py
+++ 1250-longest-common-subsequence/longest-common-subsequence.py
@@ -1,11 +1,27 @@
-class Solution:
-    def longestCommonSubsequence(self, s1: str, s2: str) -> int:
-        dp=[[0 for j in range(len(s2)+1)] for i in range(len(s1)+1)]
+# class Solution:
+#     def longestCommonSubsequence(self, s1: str, s2: str) -> int:
+#         dp=[[0 for j in range(len(s2)+1)] for i in range(len(s1)+1)]
 
-        for i in range(len(s1)-1,-1,-1):
-            for j in range(len(s2)-1,-1,-1):
-                if(s1[i]==s2[j]):
-                    dp[i][j]=1+dp[i+1][j+1]
+#         for i in range(len(s1)-1,-1,-1):
+#             for j in range(len(s2)-1,-1,-1):
+#                 if(s1[i]==s2[j]):
+#                     dp[i][j]=1+dp[i+1][j+1]
+#                 else:
+#                     dp[i][j]=max(dp[i+1][j],dp[i][j+1])
+#         return dp[0][0]
+
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        dp = [[0 for i in range(len(text2) + 1)] for j in range(len(text1) + 1)]
+        
+        text1 = " " + text1
+        text2 = " " + text2
+        
+        for i in range(1, len(text1)):
+            for j in range(1, len(text2)):
+                if text1[i] == text2[j]:
+                    dp[i][j] = 1 + dp[i-1][j-1]
                 else:
-                    dp[i][j]=max(dp[i+1][j],dp[i][j+1])
-        return dp[0][0]
+                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+            
+        return dp[-1][-1]
