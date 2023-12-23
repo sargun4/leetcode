74def24e0bdccfba4938025ab44aef09c92ab94a
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-24 04:17:21 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (93.89%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -1,35 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        unordered_map<char, pair<int, int>> moves;
-        moves['N'] = {0, 1};
-        moves['S'] = {0, -1};
-        moves['W'] = {-1, 0};
-        moves['E'] = {1, 0}; 
-        
-        unordered_set<string> visited;
-        visited.insert("0,0");
-        
-        int x = 0;
-        int y = 0;
-        
-        for (char c : path) {
-            pair<int, int> curr = moves[c];
-            int dx = curr.first;
-            int dy = curr.second;
-            
-            x += dx;
-            y += dy;
-            
-            string hash = to_string(x) + "," + to_string(y);
-            
-            if (visited.find(hash) != visited.end()) {
+        unordered_set <string> set;
+        int x=0,y=0;
+        string key=to_string(x)+"_"+to_string(y);
+        set.insert(key);
+        for(char &ch:path){
+            if(ch=='E'){
+                x++;
+            }else if(ch=='W'){
+                x--;
+            }else if(ch=='N'){
+                y++;
+            }else if(ch=='S'){
+                y--;
+            }
+            string key=to_string(x)+"_"+to_string(y);
+            if(set.find(key)!=set.end()){
                 return true;
             }
-            
-            visited.insert(hash);
+        set.insert(key);
         }
-        
         return false;
     }
 };
