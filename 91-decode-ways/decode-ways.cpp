da244f2d2dd7242963a3eeb9695516a8b5af8610
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-25 19:39:41 +0530

    Time: 0 ms (100.00%) | Memory: 6.8 MB (29.05%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,3 +1,5 @@
+#include<bits/stdc++.h>
+using namespace std;
 // class Solution {
 // public:
 //     vector<int> dp;
@@ -30,30 +32,58 @@
 //     }
 // };
 
+// //memo
+// class Solution {
+// public:
+//     int dp[101];
+//     int solve(int i,string &s,int &n){
+//         if(dp[i]!=-1){
+//             return dp[i];
+//         }
+//         if(i==n){ //one valid split done
+//             return dp[i]=1;
+//         } 
+//         if(s[i]=='0'){
+//             return dp[i]=0; //not possible to split
+//         }
+//         int res=solve(i+1,s,n);
+//         if(i+1<n){
+//             if(s[i]=='1'|| (s[i]=='2' && s[i+1]<='6')){
+//                 res+=solve(i+2,s,n);
+//             }
+//         }
+//         return dp[i]=res;
+//     }
+//     int numDecodings(string s){
+//         int n=s.length();
+//         memset(dp,-1,sizeof(dp));
+//         return solve(0,s,n);
+//     }
+// }; 
+// // #dp[i]=no. of decode ways for str s frm idx i to n
+
+
+//tabulation
 class Solution {
 public:
     int dp[101];
-    int solve(int i,string &s,int &n){
-        if(dp[i]!=-1){
-            return dp[i];
-        }
-        if(i==n){ //one valid split done
-            return dp[i]=1;
-        } 
-        if(s[i]=='0'){
-            return dp[i]=0; //not possible to split
-        }
-        int res=solve(i+1,s,n);
-        if(i+1<n){
-            if(s[i]=='1'|| (s[i]=='2' && s[i+1]<='6')){
-                res+=solve(i+2,s,n);
-            }
-        }
-        return dp[i]=res;
-    }
+    
     int numDecodings(string s){
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,s,n);
+        vector<int> dp(n+1,0);
+        dp[n]=1;
+        for(int i=n-1;i>=0;i--){
+            if(s[i]=='0'){
+                dp[i]=0;
+            }else{
+                dp[i]=(dp[i+1]); //single digit
+                if(i+1<n){
+                    if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6')){
+                        dp[i]+=dp[i+2];
+                    }
+                }
+            }
+        }
+        return dp[0]; //s[0 to n] full string ka decode ways
     }
-};
+};  
