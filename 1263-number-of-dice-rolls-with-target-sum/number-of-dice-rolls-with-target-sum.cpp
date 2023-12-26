1bd312b8d3a878aa55ddeca1970fcc18750b59ea
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-27 00:22:12 +0530

    Time: 30 ms (37.85%) | Memory: 8.9 MB (35.37%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.cpp
@@ -1,28 +1,52 @@
-#include<bits/stdc++.h>
-using namespace std;
+// #include<bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     int MOD=1e9+7;
+//     int dp[31][1001];
+//     int solve(int n, int k, int target){
+//         if(target<0){
+//             return 0;
+//         }
+//         if(dp[n][target]!=-1){
+//             return dp[n][target];
+//         }
+//         if (n == 0){
+//             return target==0;
+//         }
+//         int ways=0;
+//         for(int face=1;face<=k;face++){
+//             ways=(ways+solve(n-1,k,target-face))%MOD;
+//         }
+//         return dp[n][target]= ways;
+//     }
+//     int numRollsToTarget(int n, int k, int target) {
+//         memset(dp,-1,sizeof(dp));
+//         return solve(n,k,target);
+//     }
+// };
 
+// bottom up
+// #dp[i][j]=no of ways to sum=j with i dices
 class Solution {
 public:
     int MOD=1e9+7;
-    int dp[31][1001];
-    int solve(int n, int k, int target){
-        if(target<0){
-            return 0;
-        }
-        if(dp[n][target]!=-1){
-            return dp[n][target];
-        }
-        if (n == 0){
-            return target==0;
-        }
-        int ways=0;
-        for(int face=1;face<=k;face++){
-            ways=(ways+solve(n-1,k,target-face))%MOD;
-        }
-        return dp[n][target]= ways;
-    }
+    int dp[31][1001]; 
     int numRollsToTarget(int n, int k, int target) {
-        memset(dp,-1,sizeof(dp));
-        return solve(n,k,target);
+        vector<vector<int>> dp(n+1,vector<int>(target+1));
+        dp[0][0]=1;
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<target+1;j++){
+                //i = no of dice, j = target
+                int ways=0;
+                for(int face=1;face<=k;face++){
+                    if(j>=face){
+                        ways=(ways+dp[i-1][j-face])%MOD;
+                    }
+                }
+                dp[i][j]=ways;
+            }
+        }
+        return dp[n][target];
     }
 };
