85f075797f48f497e0ac8cb718915de0745b164b
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-27 00:47:30 +0530

    Time: 5 ms (26.97%) | Memory: 7.3 MB (21.36%) - LeetSync

--- 1619-path-crossing/path-crossing.cpp
+++ 1619-path-crossing/path-crossing.cpp
@@ -1,27 +1,54 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     bool isPathCrossing(string path) {
+//         unordered_set <string> set;
+//         int x=0,y=0;
+//         string key=to_string(x)+"_"+to_string(y);
+//         set.insert(key);
+//         for(char &ch:path){
+//             if(ch=='E'){
+//                 x++;
+//             }else if(ch=='W'){
+//                 x--;
+//             }else if(ch=='N'){
+//                 y++;
+//             }else if(ch=='S'){
+//                 y--;
+//             }
+//             string key=to_string(x)+"_"+to_string(y);
+//             if(set.find(key)!=set.end()){
+//                 return true;
+//             }
+//         set.insert(key);
+//         }
+//         return false;
+//     }
+// };
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
 public:
     bool isPathCrossing(string path) {
-        unordered_set <string> set;
         int x=0,y=0;
-        string key=to_string(x)+"_"+to_string(y);
-        set.insert(key);
-        for(char &ch:path){
-            if(ch=='E'){
-                x++;
-            }else if(ch=='W'){
-                x--;
-            }else if(ch=='N'){
+        unordered_set<string> s;
+        s.insert("0,0");
+        for (auto c : path) {
+            if (c == 'N')
                 y++;
-            }else if(ch=='S'){
+            else if (c == 'S')
                 y--;
-            }
-            string key=to_string(x)+"_"+to_string(y);
-            if(set.find(key)!=set.end()){
+            else if (c == 'E')
+                x++;
+            else if (c=='W')
+                x--;
+            string coord=to_string(x)+","+to_string(y);
+            if(s.find(coord)!=s.end()){
                 return true;
             }
-        set.insert(key);
+            s.insert(coord);
         }
         return false;
     }
