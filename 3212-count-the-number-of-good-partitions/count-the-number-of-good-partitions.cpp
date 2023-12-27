7fb8fe601537a813ac2c5a98da8e9724cae4d21f
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-28 02:05:35 +0530

    Time: 625 ms (12.75%) | Memory: 134.4 MB (80.13%) - LeetSync

---
+++ 3212-count-the-number-of-good-partitions/count-the-number-of-good-partitions.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int mod=1e9+7;
+    int numberOfGoodPartitions(vector<int>& nums) {
+        int n=nums.size();
+        unordered_map<int,int> last_idx;
+        for(int i=0;i<n;i++){
+            last_idx[nums[i]]=i;
+        }
+        int i=0;
+        int j=0;
+        j=max(j,last_idx[nums[0]]);
+        int res=1;
+        while(i<n){
+            if(i>j){ //we got a partiotin
+                res=(res*2)%mod;
+            }
+            j=max(j,last_idx[nums[i]]);
+            i++;
+        }
+        return res;
+    }
+};
