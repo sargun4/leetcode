66804289ef3342bdad6b383f22eaf4284b32367d
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-29 22:46:17 +0530

    Time: 90 ms (5.44%) | Memory: 18 MB (21.74%) - LeetSync

--- 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
+++ 102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.py
@@ -5,42 +5,45 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-        res=[]
-        q=collections.deque()
+    def levelOrder(self, root):
+        if root is None:
+            return root
+        q = []
+        res = []
         q.append(root)
-        while q:
-            qlen=len(q)
-            lvl=[]
-            for i in range(qlen):
-                node=q.popleft()
-                if node:
-                    lvl.append(node.val)
+        while len(q) > 0:
+            lvl = []
+            l = len(q)
+            for l in range(l):
+                node = q.pop(0)
+                lvl.append(node.val)
+                if node.left != None:
                     q.append(node.left)
+                if node.right != None:
                     q.append(node.right)
-            if lvl:
-                res.append(lvl)
+            res.append(lvl)
         return res
-        
 
 # from collections import deque
 # class Solution:
-#     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
-#         if not root:
-#             return []
-#         Q = deque([root])
-#         levels = [[root.val]]
-#         temp = deque()
-
-#         while Q:
-#             node = Q.popleft()
-#             if node.left: temp.append(node.left)
-#             if node.right: temp.append(node.right)
-            
-#             if not Q:
-#                 if temp:
-#                     levels.append([n.val for n in temp])
-#                 Q = temp
-#                 temp = deque()
-
-#         return levels
+    # def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+    #       res = []
+    #       queue = [[root]]
+    #       while queue:
+    #           level = queue.pop(0)
+    #           # print([node.val for node in level if node])
+    #           res.append([node.val for node in level])
+    #           nlevel = []
+    #           for node in level:
+    #               print("NODE:", node.val)
+    #               if node.left:
+    #                   nlevel.append(node.left)
+    #                   print("LEFT:", node.left.val)
+    #               if node.right:
+    #                   nlevel.append(node.right)
+    #                   print("RIGHT:",node.right.val)
+    #           print()
+    #           if nlevel:
+    #               queue.append(nlevel)
+        
+    #       return res
