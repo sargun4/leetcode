3a33a083f69363eb07590d7875817fbdeeef3d3f
sargun4 <132887555+sargun4@users.noreply.github.com>
2023-12-31 16:02:59 +0530

    Time: 0 ms (100.00%) | Memory: 6.7 MB (41.15%) - LeetSync

--- 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
+++ 1746-largest-substring-between-two-equal-characters/largest-substring-between-two-equal-characters.cpp
@@ -1,3 +1,21 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     int maxLengthBetweenEqualCharacters(string s) {
+//         int n=s.length();
+//         int res=-1;
+//         for(int i=0;i<n-1;i++){
+//             for(int j=i+1;j<n;j++){
+//                 if(s[i]==s[j]){
+//                     res=max(res,j-i-1);
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
 #include <bits/stdc++.h>
 using namespace std;
 class Solution {
@@ -5,11 +23,13 @@ public:
     int maxLengthBetweenEqualCharacters(string s) {
         int n=s.length();
         int res=-1;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(s[i]==s[j]){
-                    res=max(res,j-i-1);
-                }
+        unordered_map<char,int> m;
+        for (int i = 0; i < n; i++){
+            char ch=s[i];
+            if(m.find(ch)==m.end()){
+                m[ch]=i;
+            }else{
+                res=max(res,i-m[ch]-1);
             }
         }
         return res;
