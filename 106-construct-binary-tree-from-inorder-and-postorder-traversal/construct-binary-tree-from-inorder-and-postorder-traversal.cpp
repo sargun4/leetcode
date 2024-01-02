084bed657d03855db762c0a7df43d4ae7e70c8ee
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-03 03:29:25 +0530

    Time: 23 ms (33.39%) | Memory: 26.2 MB (79.38%) - LeetSync

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -12,17 +12,15 @@ class Solution {
 public:
     TreeNode* solve(vector<int>&inorder,vector<int>&postorder,int instart,int inend,int poststart,int postend){
         if(instart>inend) return NULL;
-        TreeNode*  root=new TreeNode(postorder[postend]);
+        TreeNode* root=new TreeNode(postorder[postend]);
         int i=instart;
         for(;i<=inend;i++){
             if(inorder[i]==root->val) break;
         }
         int leftsize=i-instart;
         int rightsize=inend-i;
-        
         root->left=solve(inorder,postorder,instart,i-1,poststart,poststart+leftsize-1);
         root->right=solve(inorder,postorder,i+1,inend,postend-rightsize,postend-1);
-
         return root;
     }
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
