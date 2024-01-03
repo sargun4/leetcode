e9a7c25ab8e444401e53afa1986b9ab8c8fe6032
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-03 09:15:44 +0530

    Time: 24 ms (63.92%) | Memory: 33 MB (69.54%) - LeetSync

---
+++ 450-delete-node-in-a-bst/delete-node-in-a-bst.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    TreeNode* maxnode(TreeNode* root){
+        TreeNode* curr=root;
+        while(curr!=NULL && curr->right!=NULL){
+            curr=curr->right;
+        }
+        return curr;
+    }
+    TreeNode* deleteNode(TreeNode* root, int val) {
+        if(root==NULL) return root;
+        if(root->val > val){
+            root->left=deleteNode(root->left,val);
+        }else if(root->val< val){
+            root->right=deleteNode(root->right,val);
+        }else{//TreeNode found,del it
+            if(root->right == NULL && root->left == NULL) { // leaf TreeNode
+                delete root;
+                return NULL;
+            } else if (root->right == NULL) {
+                TreeNode* temp = root->left;
+                delete root;
+                return temp;
+            } else if (root->left == NULL) {
+                TreeNode* temp = root->right;
+                delete root;
+                return temp;
+            } else { // TreeNode has 2 children
+                TreeNode* inorderPred = maxnode(root->left);
+                root->val = inorderPred->val;
+                root->left = deleteNode(root->left, inorderPred->val);
+            }
+        }
+        return root;
+    }
+};
