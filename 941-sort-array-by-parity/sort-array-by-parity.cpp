2f04b657505caac46dbedbf8d156a29b68d6021a
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-05 15:20:21 +0530

    Time: 14 ms (22.04%) | Memory: 17.1 MB (5.27%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.cpp
+++ 941-sort-array-by-parity/sort-array-by-parity.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
-       vector<int> sortArrayByParity(vector<int> &A) {
-        for (int i = 0, j = 0; j < A.size(); j++)
-            if (A[j] % 2 == 0) swap(A[i++], A[j]);
-        return A;
+    vector<int> sortArrayByParity(vector<int> &A) {
+        vector<int> odd;
+        vector<int> even;
+        int n=(int)A.size();
+        for(int i=0;i<n;i++){
+            if(A[i]%2==0){
+                even.push_back(A[i]);
+            }else{
+                odd.push_back(A[i]);
+            }
+        }
+        even.insert(even.end(),odd.begin(),odd.end());
+        return even;
     }
 };
+// class Solution {
+//     public:
+//        vector<int> sortArrayByParity(vector<int> &A) {
+//         for (int i = 0, j = 0; j < A.size(); j++)
+//             if (A[j] % 2 == 0) swap(A[i++], A[j]);
+//         return A;
+//     }
+// };
