c78d4ad53f99a6b3d5d293fd87328c08aa01d3b6
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-05 07:45:16 +0530

    Time: 10 ms (19.18%) | Memory: 15.1 MB (82.43%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode* head=new ListNode(0);
+        ListNode* curr=head;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val<=l2->val){
+                curr->next=l1;
+                l1=l1->next;
+            }else{
+                curr->next=l2;
+                l2=l2->next;
+            }
+            curr=curr->next;
+        }
+        if(l1!=NULL) curr->next=l1;
+        else curr->next=l2;
+        return head->next;
+    }
+};
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         //iter
+//         if(l1==NULL) return l2;
+//         if(l2==NULL) return l1;
+//         if(l1->val>l2->val){
+//             // ListNode*temp=l1;
+//             // l1=l2;
+//             // l2=temp;
+//             std::swap(l1,l2);
+//         }
+//         ListNode*res=l1;
+//         while(l1!=NULL && l2!=NULL){
+//             ListNode*temp=NULL;
+//             while(l1!=NULL && l1->val <= l2->val){
+//                 temp=l1;
+//                 l1=l1->next;
+//             }
+//             temp->next=l2;
+//             std::swap(l1,l2);
+//         }
+//         return res;
+//     }
+// };
