c94e24ff4c9f689c166f5d18026545c2341f7342
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-05 13:05:53 +0530

    Time: 2 ms (50.71%) | Memory: 7.9 MB (55.77%) - LeetSync

---
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    ListNode* revknodes(ListNode* head, int k) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* temp;
+        int ctr = 0; 
+        // Count the number of nodes in the current k-group
+        ListNode* countNode = head;
+        while (ctr < k && countNode != NULL) {
+            countNode = countNode->next;
+            ctr++;
+        } 
+        // If there are at least k nodes, proceed with reversal
+        if (ctr == k) {
+            ctr = 0;
+            while (curr != NULL && ctr < k) {
+                temp = curr->next;
+                curr->next = prev;
+                prev = curr;
+                curr = temp;
+                ctr++;
+            } 
+            if (temp != NULL) {
+                // Recursively reverse the next k nodes
+                head->next = revknodes(temp, k);
+            }
+        } else {
+            // If there are less than k nodes, reverse them back to maintain order
+            while (prev != NULL) {
+                temp = prev->next;
+                prev->next = curr;
+                curr = prev;
+                prev = temp;
+            }
+            return curr;
+        } 
+        return prev;
+    } 
+    ListNode* swapPairs(ListNode* head) {
+        // Call revknodes with k=2 to reverse every 2 nodes
+        head = revknodes(head, 2);
+        return head;
+    }
+};
