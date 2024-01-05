770623ae51bd28584d2315a32f78af90935ca307
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-05 10:05:08 +0530

    Time: 41 ms (71.76%) | Memory: 17.3 MB (20.92%) - LeetSync

--- 61-rotate-list/rotate-list.py
+++ 61-rotate-list/rotate-list.py
@@ -1,37 +1,19 @@
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
-
-# Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution:
     def rotateRight(self, head: ListNode, k: int) -> ListNode:
-        
         if not head:
             return None
-        
         lastElement = head
         length = 1
         # get the length of the list and the last node in the list
         while ( lastElement.next ):
             lastElement = lastElement.next
             length += 1
-
         # If k is equal to the length of the list then k == 0
         # ElIf k is greater than the length of the list then k = k % length
         k = k % length
-            
         # Set the last node to point to head node
         # The list is now a circular linked list with last node pointing to first node
         lastElement.next = head
-        
         # Traverse the list to get to the node just before the ( length - k )th node.
         # Example: In 1->2->3->4->5, and k = 2
         #          we need to get to the Node(3)
@@ -46,5 +28,4 @@ class Solution:
         #          Node(3).next = None ( cut the linked list from here )
         answer = tempNode.next
         tempNode.next = None
-        
         return answer
