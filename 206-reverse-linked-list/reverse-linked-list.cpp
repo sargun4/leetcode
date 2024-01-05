d4a98ecf8d1994676fad19680079a9e8f53ec258
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-05 08:55:30 +0530

    Time: 3 ms (87.79%) | Memory: 8.6 MB (48.60%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -1,15 +1,28 @@
+//rec
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* curr=head;
-        ListNode* prev=NULL;
-        ListNode* temp=NULL;
-        while(curr!=NULL){
-            temp=curr->next;
-            curr->next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
+        return reverseList(head, NULL);
+    }
+    ListNode* reverseList(ListNode* curr, ListNode* prev) {
+        if(curr == NULL) return prev;
+        ListNode* temp = curr->next;
+        curr->next = prev;
+        return reverseList(temp, curr);
     }
 };
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         ListNode* curr=head;
+//         ListNode* prev=NULL;
+//         ListNode* temp=NULL;
+//         while(curr!=NULL){
+//             temp=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=temp;
+//         }
+//         return prev;
+//     }
+// };
