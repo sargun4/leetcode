5a072e1a52e231bac296b852f93095f2da959641
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-06 16:16:10 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Pairwise swap elements of a linked list/pairwise-swap-elements-of-a-linked-list.cpp
@@ -0,0 +1,98 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+// } Driver Code Ends
+
+ 
+class Solution{
+    public:
+    Node* revknodes(Node*head,int k){
+        Node* curr=head;
+        Node*prev=NULL;
+        Node* temp;
+        int ctr=0;
+        while(curr!=NULL && ctr<k){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
+            ctr++;
+        }
+        if(temp!=NULL){
+            head->next=revknodes(temp,k);
+        }
+        return prev;
+    }
+    Node* pairWiseSwap(struct Node* head) {
+        head=revknodes(head,2);
+        return head;
+    }
+};
+
+//{ Driver Code Starts.
+
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+}
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		map<Node*, int> mp;
+		mp[head] = head->data;
+		for (int i = 0; i<n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+			mp[tail] = tail->data;
+		}
+		struct Node *failure = new Node(-1);
+		Solution ob;
+		head = ob.pairWiseSwap(head);
+		int flag = 0;
+		struct Node *temp = head;
+		while(temp){
+		    if(mp[temp] != temp->data){
+		        flag = 1;
+		        break;
+		    }
+		    temp = temp->next;
+		}
+		if(flag)
+		    printList(failure);
+		else
+		    printList(head);
+	}
+	return 0; 
+}
+
+// } Driver Code Ends
