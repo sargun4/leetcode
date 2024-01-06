c39bdc6e16271a2861952f0554b611e21eade6a9
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-06 20:52:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Steps by Knight/steps-by-knight.cpp
+++ Medium/Steps by Knight/steps-by-knight.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 // } Driver Code Ends
 
+#include <bits/stdc++.h>
+using namespace std;
 
 class Solution {
 public:
     bool isvalid(int i, int j, int n, vector<vector<bool>>& vis) {
         return i >= 0 && i < n && j >= 0 && j < n && !vis[i][j];
     }
-    
     // Function to find out minimum steps Knight needs to reach target position.
     int minStepToReachTarget(vector<int>& KnightPos, vector<int>& TargetPos, int n) {
         int tx = TargetPos[0] - 1, ty = TargetPos[1] - 1;
         int x1 = KnightPos[0] - 1, y1 = KnightPos[1] - 1;
-        if (x1 == tx && y1 == ty) return 0;
+        if (x1 == tx && y1 == ty) return 0;//if knight is already at target pos,steps=0
         
         vector<vector<bool>> vis(n, vector<bool>(n, false));
         queue<pair<int, int>> q;
@@ -29,20 +30,15 @@ public:
         while (!q.empty()) {
             int size = q.size();
             ans++;
-            
             while (size--) {
                 pair<int, int> p = q.front();
                 q.pop();
-                
                 int x = p.first;
                 int y = p.second;
-                
                 for (int i = 0; i < 8; i++) {
                     int nrow = x + xdir[i];
                     int ncol = y + ydir[i];
-                    
-                    if (nrow == tx && ncol == ty) return ans;
-                    
+                    if (nrow == tx && ncol == ty) return ans;//reached target pos
                     if (isvalid(nrow, ncol, n, vis)) {
                         vis[nrow][ncol] = true;
                         q.push({nrow, ncol});
@@ -50,11 +46,9 @@ public:
                 }
             }
         }
-        
         return ans;
     }
 };
-
 
 
 //{ Driver Code Starts.
