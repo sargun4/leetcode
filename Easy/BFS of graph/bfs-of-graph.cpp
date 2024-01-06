6533d4ea87b9752101907b3815d5554549fcbf2b
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-06 19:13:47 +0530

    Updated Solution - GfG to GitHub

--- Easy/BFS of graph/bfs-of-graph.cpp
+++ Easy/BFS of graph/bfs-of-graph.cpp
@@ -4,41 +4,64 @@ using namespace std;
 
 // } Driver Code Ends
 
-//  O(V+E)
 class Solution {
   public:
-    void BFS(unordered_map<int,vector<int>> &adj,int u,vector<bool> &vis,vector<int> &res){
-        queue<int> q;
-        q.push(u);
-        vis[u]=true;
-        res.push_back(u);
+    // Function to return Breadth First Traversal of given graph.
+    vector<int> bfsOfGraph(int V, vector<int> adj[]) {
+        vector<int>v;
+        queue<int>q;
+        q.push(0);
+        vector<int>vis(V,0);
+        vis[0] = 1;
         while(!q.empty()){
-            int u=q.front();
+            int node=q.front();
             q.pop();
-            for(int &v:adj[u]){
-                if(!vis[v]){
-                    q.push(v);
-                    vis[v]=true;
-                    res.push_back(v);
+            v.push_back(node);
+            for(auto neighbor:adj[node]){
+                if(!vis[neighbor]){
+                    vis[neighbor]=1;
+                    q.push(neighbor);
                 }
             }
         }
-
-    }
-    // Function to return Breadth First Traversal of given graph.
-    vector<int> bfsOfGraph(int V, vector<int> mp[]) {
-        unordered_map<int,vector<int>> adj;
-        for(int i=0;i<V;i++){
-            for(auto it=mp[i].begin();it!=mp[i].end();it++){
-                adj[i].push_back(*it);
-            }
-        }
-        vector<bool> vis(V,false);
-        vector<int> res;
-        BFS(adj,0,vis,res);
-        return res;
+        return v;
     }
 };
+// //  O(V+E)
+// class Solution {
+//   public:
+//     void BFS(unordered_map<int,vector<int>> &adj,int u,vector<bool> &vis,vector<int> &res){
+//         queue<int> q;
+//         q.push(u);
+//         vis[u]=true;
+//         res.push_back(u);
+//         while(!q.empty()){
+//             int u=q.front();
+//             q.pop();
+//             for(int &v:adj[u]){
+//                 if(!vis[v]){
+//                     q.push(v);
+//                     vis[v]=true;
+//                     res.push_back(v);
+//                 }
+//             }
+//         }
+
+//     }
+//     // Function to return Breadth First Traversal of given graph.
+//     vector<int> bfsOfGraph(int V, vector<int> mp[]) {
+//         unordered_map<int,vector<int>> adj;
+//         for(int i=0;i<V;i++){
+//             for(auto it=mp[i].begin();it!=mp[i].end();it++){
+//                 adj[i].push_back(*it);
+//             }
+//         }
+//         vector<bool> vis(V,false);
+//         vector<int> res;
+//         BFS(adj,0,vis,res);
+//         return res;
+//     }
+// };
 
 //{ Driver Code Starts.
 int main() {
