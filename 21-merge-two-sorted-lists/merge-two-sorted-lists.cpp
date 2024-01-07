f88cfcde5cc79a03ad0eda43d657c3d4c296f67a
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-07 16:47:27 +0530

    Time: 3 ms (86.76%) | Memory: 15.2 MB (45.81%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -1,23 +1,38 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* head=new ListNode(0);
-        ListNode* curr=head;
-        while(l1!=NULL && l2!=NULL){
-            if(l1->val<=l2->val){
-                curr->next=l1;
-                l1=l1->next;
-            }else{
-                curr->next=l2;
-                l2=l2->next;
-            }
-            curr=curr->next;
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        if(l1->val<=l2->val){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }else{
+            l2->next=mergeTwoLists(l1,l2->next);
+            return l2;
         }
-        if(l1!=NULL) curr->next=l1;
-        else curr->next=l2;
-        return head->next;
+        return NULL;
     }
 };
+// class Solution {
+// public:
+//     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+//         ListNode* head=new ListNode(0);
+//         ListNode* curr=head;
+//         while(l1!=NULL && l2!=NULL){
+//             if(l1->val<=l2->val){
+//                 curr->next=l1;
+//                 l1=l1->next;
+//             }else{
+//                 curr->next=l2;
+//                 l2=l2->next;
+//             }
+//             curr=curr->next;
+//         }
+//         if(l1!=NULL) curr->next=l1;
+//         else curr->next=l2;
+//         return head->next;
+//     }
+// };
 // class Solution {
 // public:
 //     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
