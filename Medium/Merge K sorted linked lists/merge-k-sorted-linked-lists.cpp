3736230adb2bc693df1b0c3fbb69af07be9477ac
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-07 17:03:45 +0530

    Updated Solution - GfG to GitHub

--- Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
+++ Medium/Merge K sorted linked lists/merge-k-sorted-linked-lists.cpp
@@ -29,80 +29,104 @@ void printList(Node* node)
 
 // } Driver Code Ends
 
-
 class Solution {
 public:
     Node* mergeTwoLists(Node* l1, Node* l2) {
-        Node dummy(0);
-        Node* current = &dummy;
-
-        while (l1 != nullptr && l2 != nullptr) {
-            if (l1->data < l2->data) {
-                current->next = l1;
-                l1 = l1->next;
-            } else {
-                current->next = l2;
-                l2 = l2->next;
-            }
-            current = current->next;
+        if(l1==NULL) return l2;
+        if(l2==NULL) return l1;
+        if(l1->data <= l2->data){
+            l1->next=mergeTwoLists(l1->next,l2);
+            return l1;
+        }else{
+            l2->next=mergeTwoLists(l2->next,l1);
+            return l2;
         }
-
-        // Append the remaining nodes from either list
-        if (l1 != nullptr) {
-            current->next = l1;
-        } else {
-            current->next = l2;
-        }
-
-        return dummy.next;
+        return NULL;
     }
-
+    Node* partitionNmerge(int start,int end,Node* arr[]){
+        // base case
+        if (start > end) return NULL;
+        if (start == end) return arr[start];
+        int mid=start+(end-start)/2;
+        Node* l1=partitionNmerge(start,mid,arr);
+        Node* l2=partitionNmerge(mid+1,end,arr);
+        return mergeTwoLists(l1,l2);
+    }
     Node* mergeKLists(Node* arr[], int K) {
-        if (K == 0) {
-            return nullptr;
-        }
-
-        int interval = 1;
-
-        // Continue merging pairs until there is only one merged list remaining
-        while (interval < K) {
-            for (int i = 0; i < K - interval; i += interval * 2) {
-                arr[i] = mergeTwoLists(arr[i], arr[i + interval]);
-            }
-            interval *= 2;
-        }
-        return arr[0];
+        if(K==0) return NULL;
+        return partitionNmerge(0,K-1,arr);
     }
 };
 
 // class Solution {
 // public:
-//     // Function to merge K sorted linked list.
+//     Node* mergeTwoLists(Node* l1, Node* l2) {
+//         Node dummy(0);
+//         Node* curr = &dummy;
+//         while (l1 != NULL && l2 != NULL) {
+//             if (l1->data < l2->data) {
+//                 curr->next = l1;
+//                 l1 = l1->next;
+//             } else {
+//                 curr->next = l2;
+//                 l2 = l2->next;
+//             }
+//             curr = curr->next;
+//         }
+//         // Append the remaining nodes from either list
+//         if (l1 != NULL) {
+//             curr->next = l1;
+//         } else {
+//             curr->next = l2;
+//         }
+//         return dummy.next;
+//     }
 //     Node* mergeKLists(Node* arr[], int K) {
-//         map<int, int> freqMap;
-//         // Count the frequency of each element in the linked lists
-//         for (int i = 0; i < K; i++) {
-//             Node* temp = arr[i];
-//             while (temp != NULL) {
-//                 freqMap[temp->data]++;
-//                 temp = temp->next;
-//             }
+//         if (K == 0) {
+//             return NULL;
 //         }
-//         Node* dummy = new Node(-1);
-//         Node* curr = dummy;
-
-//         // Create the merged linked list using the frequency information
-//         for (auto it : freqMap) {
-//             int value = it.first;
-//             int frequency = it.second;
-//             while (frequency--) {
-//                 curr->next = new Node(value);
-//                 curr = curr->next;
+//         int interval = 1;
+//         // Continue merging pairs until there is only one merged list remaining
+//         while (interval < K) {
+//             for (int i = 0; i < K - interval; i += interval * 2) {
+//                 arr[i] = mergeTwoLists(arr[i], arr[i + interval]);
 //             }
+//             interval *= 2;
 //         }
-//         return dummy->next;
+//         return arr[0];
 //     }
 // };
+
+// //TLe
+// // class Solution {
+// // public:
+// //     // Function to merge K sorted linked list.
+// //     Node* mergeKLists(Node* arr[], int K) {
+// //         map<int, int> freqMap;
+// //         // Count the frequency of each element in the linked lists
+// //         for (int i = 0; i < K; i++) {
+// //             Node* temp = arr[i];
+// //             while (temp != NULL) {
+// //                 freqMap[temp->data]++;
+// //                 temp = temp->next;
+// //             }
+// //         }
+// //         Node* dummy = new Node(-1);
+// //         Node* curr = dummy;
+
+// //         // Create the merged linked list using the frequency information
+// //         for (auto it : freqMap) {
+// //             int value = it.first;
+// //             int frequency = it.second;
+// //             while (frequency--) {
+// //                 curr->next = new Node(value);
+// //                 curr = curr->next;
+// //             }
+// //         }
+// //         return dummy->next;
+// //     }
+// // };
+
 
 
 
