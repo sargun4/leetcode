0309eeb4c24fe187786c81e5bd33d054945cb190
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-10 20:59:04 +0530

    Time: 638 ms (16.52%) | Memory: 317.6 MB (5.27%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,55 +1,87 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    unordered_map<int,vector<int>> v;  //adjacency list
-
-    //create undirected graph for every parent-child  -> BFS
-    void createGraph(TreeNode* root){
-        queue<pair<TreeNode*,int>> q;
-        q.push({root,-1});
-        while(q.size()){
-            auto [node,parent]= q.front(); q.pop();
-            if(parent!=-1){
-                v[parent].push_back(node->val);
-                v[node->val].push_back(parent);
-            }
-            if(node->left)  q.push({node->left,node->val});
-            if(node->right) q.push({node->right,node->val});
-        }   
+    void makegraph(unordered_map<int,vector<int>> &adj,int parent,TreeNode* curr){
+        if(curr==NULL) return;
+        if(parent!=-1){
+            adj[curr->val].push_back(parent);
+        }
+        if(curr->left){
+            adj[curr->val].push_back(curr->left->val);
+        }if(curr->right){
+            adj[curr->val].push_back(curr->right->val);
+        }
+        makegraph(adj,curr->val,curr->left);
+        makegraph(adj,curr->val,curr->right);
     }
-
     int amountOfTime(TreeNode* root, int start) {
-        //create graph of given tree
-        createGraph(root);
-
-        //start bfs
+        unordered_map<int,vector<int>> adj;
+        makegraph(adj,-1,root);
         queue<int> q;
-        unordered_map<int,bool> seen;
+        unordered_set<int> vis;
         q.push(start);
-        seen[start]=true;
-        int time=0;
-        for(;q.size();time++){
-            int n= q.size();
+        vis.insert(start);
+        int min=0;
+        while(!q.empty()){
+            int n=q.size();
             while(n--){
-                auto node= q.front();  q.pop();
-                for(auto i:v[node]){
-                    if(!seen[i]){
-                        q.push(i);
-                        seen[i]=true;
+                int curr=q.front();
+                q.pop();
+                for(int &neighbor:adj[curr]){
+                    if(vis.find(neighbor)==vis.end()){
+                        q.push(neighbor);
+                        vis.insert(neighbor);
                     }
                 }
             }
-        }
-        return time-1;
+            min++;
+        }     
+        return min-1;
     }
 };
+//  //BFS
+// class Solution {
+// public:
+//     unordered_map<int,vector<int>> v;  //adjacency list
+
+//     //create undirected graph for every parent-child  -> BFS
+//     void createGraph(TreeNode* root){
+//         queue<pair<TreeNode*,int>> q;
+//         q.push({root,-1});
+//         while(q.size()){
+//             auto [node,parent]= q.front(); q.pop();
+//             if(parent!=-1){
+//                 v[parent].push_back(node->val);
+//                 v[node->val].push_back(parent);
+//             }
+//             if(node->left)  q.push({node->left,node->val});
+//             if(node->right) q.push({node->right,node->val});
+//         }   
+//     }
+
+//     int amountOfTime(TreeNode* root, int start) {
+//         //create graph of given tree
+//         createGraph(root);
+
+//         //start bfs
+//         queue<int> q;
+//         unordered_map<int,bool> seen;
+//         q.push(start);
+//         seen[start]=true;
+//         int time=0;
+//         for(;q.size();time++){
+//             int n= q.size();
+//             while(n--){
+//                 auto node= q.front();  q.pop();
+//                 for(auto i:v[node]){
+//                     if(!seen[i]){
+//                         q.push(i);
+//                         seen[i]=true;
+//                     }
+//                 }
+//             }
+//         }
+//         return time-1;
+//     }
+// };
+// // Time - O(n)
+// // Space - O(n)
