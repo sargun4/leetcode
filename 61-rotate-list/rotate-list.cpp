d705c8b3d71be9419f674b12aa741096eb113471
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-12 20:37:07 +0530

    Time: 9 ms (28.36%) | Memory: 11.9 MB (90.32%) - LeetSync

--- 61-rotate-list/rotate-list.cpp
+++ 61-rotate-list/rotate-list.cpp
@@ -1,64 +1,64 @@
-class Solution {
-public:
-    int size(ListNode* curr){
-        int len=0;
-        while(curr!=NULL){
-            curr=curr->next;
-            len++;
-        }
-        return len;
-    }
-    ListNode* rotateRight(ListNode* head, int k) {
-        if(head==NULL){
-            return NULL;
-        }
-        int n = size(head);
-        int len=k%n;
-        len=n-len;
-        if(n==1||len==n){
-            return head;
-        }
-        int j=0;
-        ListNode *prev=head,*firstaddr=head;
-        while(prev!=NULL){
-            j++;
-            if(j==len){
-                firstaddr=prev->next;
-                prev->next=NULL;
-                break;
-            }
-            prev=prev->next;
-        }
-        prev=firstaddr;
-        while(prev->next!=NULL){
-            prev=prev->next;
-        }
-        prev->next=head;
-        return firstaddr;
-    }
-};
 // class Solution {
 // public:
+//     int size(ListNode* curr){
+//         int len=0;
+//         while(curr!=NULL){
+//             curr=curr->next;
+//             len++;
+//         }
+//         return len;
+//     }
 //     ListNode* rotateRight(ListNode* head, int k) {
-//         if(!head || !head->next || k==0) return head;
-//         //find length of LL
-//         ListNode*curr=head;
-//         int len=1;
-//         while(curr->next!=NULL && ++len){
-//             curr=curr->next;
+//         if(head==NULL){
+//             return NULL;
 //         }
-//         ///go till that node
-//         curr->next=head;
-//         k=k%len;
-//         k=len-k;
-//         while(k--){
-//             curr=curr->next;
+//         int n = size(head);
+//         int len=k%n;
+//         len=n-len;
+//         if(n==1||len==n){
+//             return head;
 //         }
-//         head=curr->next;
-//         curr->next=NULL;
-//         return head;
+//         int j=0;
+//         ListNode *prev=head,*firstaddr=head;
+//         while(prev!=NULL){
+//             j++;
+//             if(j==len){
+//                 firstaddr=prev->next;
+//                 prev->next=NULL;
+//                 break;
+//             }
+//             prev=prev->next;
+//         }
+//         prev=firstaddr;
+//         while(prev->next!=NULL){
+//             prev=prev->next;
+//         }
+//         prev->next=head;
+//         return firstaddr;
 //     }
 // };
+class Solution {
+    public:
+        ListNode* rotateRight(ListNode* head, int k) {
+        if(!head || !head->next || k==0) return head;
+        //find length of LL
+        ListNode*curr=head;
+        int len=1;
+        while(curr->next!=NULL && ++len){
+            curr=curr->next;
+        }
+        ///go till that node
+        curr->next=head; //make last node(curr) point to head-make circular
+        k=k%len;
+        k=len-k;
+        while(k--){
+            curr=curr->next;
+        }
+        head=curr->next;
+        curr->next=NULL;
+        return head;
+    }
+};
 // // class Solution {
 // // public:
 // //     ListNode* rotateRight(ListNode* head, int k) {
