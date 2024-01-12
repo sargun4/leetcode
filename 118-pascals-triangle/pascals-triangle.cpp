08dfa0ef82db18c91be0be3f3913b189272a6419
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-12 16:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 7 MB (80.95%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        int n=numRows;
+        vector<vector<int>> triangle;
+        for(int i=0;i<n;i++){
+            vector<int> row(i+1,1);
+            for(int j=1;j<i;j++){
+                row[j]=triangle[i-1][j]+triangle[i-1][j-1];
+            }
+            triangle.push_back(row);
+        }
+        return triangle;
+    }
+};
