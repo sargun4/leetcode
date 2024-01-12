6919481bff99863e5e1cf3d0375fdaafbbeb1f5f
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-12 18:42:22 +0530

    Time: 46 ms (29.86%) | Memory: 17.6 MB (15.54%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,47 +1,68 @@
 class Solution {
 public:
     ListNode*getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode*curr=headA;
-        int a=0,b=0;//a=len(1st LL), b=len(2nd LL)
-        while (curr!=NULL){
-            a++;
+        ListNode* curr=headA;
+        unordered_set<ListNode*> addr_set;
+        while(curr!=NULL){
+            addr_set.insert(curr);
             curr=curr->next;
         }
         curr=headB;
-        while (curr!=NULL){
-            b++;
+        ListNode*intersecnode=NULL;
+        while(curr!=NULL){
+            if(addr_set.find(curr)!=addr_set.end()){
+                intersecnode=curr;
+                break;
+            }
             curr=curr->next;
         }
-        int diff=abs(a-b);
-        if(a<b){
-            while(diff--){
-                headB=headB->next;
-            }
-        }else{
-            while(diff--){
-                headA=headA->next;
-            }
-        }
-        while(headA && headB){
-            if(headA==headB) return headA;
-            headA=headA->next;
-            headB=headB->next;
-        }
-        return NULL;
+        return intersecnode;
     }
 };
 // class Solution {
 // public:
-//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-//         if(headA==NULL||headB==NULL) return NULL;
-//         ListNode *a=headA;
-//         ListNode*b=headB;
-//         //if a & b hv diff len,the we'll stop the loop after 2nd iteration
-//         while(a!=b){
-//             //for the end of the 1st iteration, we just reset the ptr to the head of the other LL
-//             a=a==NULL ? headB:a->next;
-//             b=b==NULL ? headA:b->next;
+//     ListNode*getIntersectionNode(ListNode *headA, ListNode *headB) {
+//         ListNode*curr=headA;
+//         int a=0,b=0;//a=len(1st LL), b=len(2nd LL)
+//         while (curr!=NULL){
+//             a++;
+//             curr=curr->next;
 //         }
-//         return a;
+//         curr=headB;
+//         while (curr!=NULL){
+//             b++;
+//             curr=curr->next;
+//         }
+//         int diff=abs(a-b);
+//         if(a<b){
+//             while(diff--){
+//                 headB=headB->next;
+//             }
+//         }else{
+//             while(diff--){
+//                 headA=headA->next;
+//             }
+//         }
+//         while(headA && headB){
+//             if(headA==headB) return headA;
+//             headA=headA->next;
+//             headB=headB->next;
+//         }
+//         return NULL;
 //     }
 // };
+// // class Solution {
+// // public:
+// //     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+// //         if(headA==NULL||headB==NULL) return NULL;
+// //         ListNode *a=headA;
+// //         ListNode*b=headB;
+// //         //if a & b hv diff len,the we'll stop the loop after 2nd iteration
+// //         while(a!=b){
+// //             //for the end of the 1st iteration, we just reset the ptr to the head of the other LL
+// //             a=a==NULL ? headB:a->next;
+// //             b=b==NULL ? headA:b->next;
+// //         }
+// //         return a;
+// //     }
+// // };
