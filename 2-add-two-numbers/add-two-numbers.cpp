15ff33f30e7f12d231dc72a14842a95147da0d29
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-12 18:06:22 +0530

    Time: 24 ms (52.25%) | Memory: 72.1 MB (9.41%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.cpp
+++ 2-add-two-numbers/add-two-numbers.cpp
@@ -1,46 +1,37 @@
- 
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ans=new ListNode(0);
-        ListNode*p=l1,*q=l2,*curr=ans;
-        int carry=0;
-        while(p!=NULL || q!=NULL){
-            int x=(p!=NULL)?p->val:0;
-            int y=(q!=NULL)?q->val:0;
-            int sum=carry+x+y;
-            carry=sum/10;
-            curr->next=new ListNode(sum%10);
-            curr=curr->next;
-            if(p!=NULL) p=p->next;
-            if(q!=NULL) q=q->next;
+    ListNode* rev(ListNode* head){
+        ListNode* curr=head,*prev=NULL,*temp;
+        while(curr!=NULL){
+            temp=curr->next;
+            curr->next=prev;
+            prev=curr;
+            curr=temp;
         }
-        if(carry!=0) curr->next=new ListNode(carry);
-        return ans->next;
+        return prev;
+    }
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        int sum=0;
+        int carry=0;
+        ListNode* ans=new ListNode(0);
+        while(l1!=NULL || l2!=NULL){
+            if(l1!=NULL){
+                sum+=l1->val;
+                l1=l1->next;
+            }
+            if(l2!=NULL){
+                sum+=l2->val;
+                l2=l2->next;
+            }
+            ans->val=sum%10;
+            carry=sum/10;
+            ListNode* node=new ListNode(carry);
+            node->next=ans;
+            ans=node;
+            sum=carry;
+        }
+        // ans=rev(ans);
+        if(carry==1) return rev(ans);
+        else return rev(ans->next);
     }
 };
-// class Solution {
-// public:
-//     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-//         ListNode*dummy=new ListNode();
-//         ListNode*temp=dummy;
-//         int carry=0;
-//         while(l1!=NULL||l2!=NULL||carry!=0){
-//             int sum=0;
-//             if(l1!=NULL){
-//                 sum+=l1->val;
-//                 l1=l1->next;
-//             }
-//             if(l2!=NULL){
-//                 sum+=l2->val;
-//                 l2=l2->next;
-//             }
-//             sum+=carry;
-//             carry=sum/10;
-//             ListNode*node =new ListNode(sum%10);
-//             temp->next=node;
-//             temp=temp->next;
-//         }
-//         return dummy->next;
-//     }
-// };
