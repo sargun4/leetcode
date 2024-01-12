8787ee4ea3f579567087b386e7e9214672fa2b89
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-12 21:56:35 +0530

    Time: 27 ms (71.45%) | Memory: 36.4 MB (97.37%) - LeetSync

---
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int maxlen = 0;
+        int currlen = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == 1) {
+                currlen++;
+            } else {
+                // Update the maximum length if the current length is greater
+                maxlen = max(maxlen, currlen);
+                // Reset the current length for consecutive zeros
+                currlen = 0;
+            }
+        }
+        // Update the maximum length after the loop (in case the sequence ends with ones)
+        maxlen = max(maxlen, currlen);
+        return maxlen;
+    }
+};
