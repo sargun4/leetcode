07150883bd62463669a1de81f29ed51d123cef7e
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-14 21:39:53 +0530

    Updated Solution - GfG to GitHub

--- Medium/Topological sort/topological-sort.cpp
+++ Medium/Topological sort/topological-sort.cpp
@@ -4,37 +4,68 @@ using namespace std;
 
 // } Driver Code Ends
 
-#include <bits/stdc++.h>
-using namespace std;
+
+//using DFS
 class Solution{
 	public:
+    void dfs(vector<int> adj[],int u,vector<bool> &vis,stack<int>&st){
+        vis[u] = true;
+        //first add u's children to stack
+        for (int&v:adj[u]){
+            if (!vis[v])
+                dfs(adj, v, vis, st);
+        }
+        //then add u to stack
+        st.push(u);
+    }
 	//Function to return list containing vertices in Topological order. 
 	vector<int> topoSort(int V, vector<int> adj[]) {
-	    vector<int> indegree(V,0);
-        vector<int> ans;
-        queue<int> q;
+        vector<bool> vis(V,false);
+        stack<int> st;
         for(int i=0;i<V;i++){
-            for(auto neigh : adj[i]){
-                indegree[neigh]++;
+            if(!vis[i]){
+                dfs(adj,i,vis,st);
             }
         }
-        // Push all the vertex with no incoming edge to the queue
-        for(int i=0;i<V;i++) 
-            if (indegree[i] == 0) q.push(i);
-
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            // Reduce in-degree of adjacent vertices by one and if it becomes zero then push it to the queue
-            for(auto neigh : adj[node]){
-                indegree[neigh]--;
-                if(indegree[neigh]==0) q.push(neigh);
-            }
+        vector<int> res;
+        while(!st.empty()){
+            int t = st.top();
+            res.push_back(t);
+            st.pop();
         }
-        return ans;
+        return res;
 	}
 };
+//using BFS
+// class Solution{
+// 	public:
+// 	//Function to return list containing vertices in Topological order. 
+// 	vector<int> topoSort(int V, vector<int> adj[]) {
+// 	    vector<int> indegree(V,0);
+//         vector<int> ans;
+//         queue<int> q;
+//         for(int i=0;i<V;i++){
+//             for(auto neigh : adj[i]){
+//                 indegree[neigh]++;
+//             }
+//         }
+//         // Push all the vertex with no incoming edge to the queue
+//         for(int i=0;i<V;i++) 
+//             if (indegree[i] == 0) q.push(i);
+
+//         while(!q.empty()){
+//             int node = q.front();
+//             q.pop();
+//             ans.push_back(node);
+//             // Reduce in-degree of adjacent vertices by one and if it becomes zero then push it to the queue
+//             for(auto neigh : adj[node]){
+//                 indegree[neigh]--;
+//                 if(indegree[neigh]==0) q.push(neigh);
+//             }
+//         }
+//         return ans;
+// 	}
+// };
 
 //{ Driver Code Starts.
 
