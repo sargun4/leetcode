15f3977e3c8e85839ecea66b46959d66486999ae
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-20 20:05:44 +0530

    Time: 9 ms (14.10%) | Memory: 15.9 MB (31.82%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -8,15 +8,40 @@ public:
             return;
         }
         if (open > 0){
-            comb(open-1,close,res,str+"(");
+            str.push_back('(');
+            comb(open-1,close,res,str);
+            str.pop_back();
         }
         if (close > 0 && close>open){
-            comb(open,close-1,res,str+")");
+            str.push_back(')');
+            comb(open,close-1,res,str);
+            str.pop_back();
         } 
     }
     vector<string> generateParenthesis(int n) {
         vector<string> res;
-        comb(n,n,res,"");
+        string val="";
+        comb(n,n,res,val);
         return res;
     }
 };
+// class Solution {
+// public:
+//     void comb(int open,int close,vector<string> &res,string str){
+//         if (open == 0 and close == 0){
+//             res.push_back(str);
+//             return;
+//         }
+//         if (open > 0){
+//             comb(open-1,close,res,str+"(");
+//         }
+//         if (close > 0 && close>open){
+//             comb(open,close-1,res,str+")");
+//         } 
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         vector<string> res;
+//         comb(n,n,res,"");
+//         return res;
+//     }
+// };
