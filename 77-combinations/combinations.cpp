251436b61351c6422fb82638e03fb79d30a6cb38
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-21 23:37:12 +0530

    Time: 113 ms (77.14%) | Memory: 81.5 MB (45.09%) - LeetSync

--- 77-combinations/combinations.cpp
+++ 77-combinations/combinations.cpp
@@ -9,13 +9,14 @@ public:
             res.push_back(temp);
             return;
         }
-        if(start>n){
-            return ;
+        // if(start>n){
+        //     return ;
+        // }
+        for(int i=start;i<=n;i++){ 
+            temp.push_back(i);
+            solve(i+1,n,k-1,temp);
+            temp.pop_back();
         }
-        temp.push_back(start);
-        solve(start+1,n,k-1,temp);
-        temp.pop_back();
-        solve(start+1,n,k,temp);
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int> temp;
@@ -23,3 +24,25 @@ public:
         return res;
     }
 };
+// class Solution {
+// public:
+//     vector<vector<int>> res;
+//     void solve(int start,int &n,int k,vector<int> &temp){
+//         if(k==0){
+//             res.push_back(temp);
+//             return;
+//         }
+//         if(start>n){
+//             return ;
+//         }
+//         temp.push_back(start);
+//         solve(start+1,n,k-1,temp);
+//         temp.pop_back();
+//         solve(start+1,n,k,temp);
+//     }
+//     vector<vector<int>> combine(int n, int k) {
+//         vector<int> temp;
+//         solve(1,n,k,temp);
+//         return res;
+//     }
+// };
