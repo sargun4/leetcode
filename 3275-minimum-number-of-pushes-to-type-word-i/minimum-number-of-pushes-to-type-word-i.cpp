5cd75f4e337b28867e2a75b54de1d0d7d56fa7c9
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-21 17:32:31 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (28.57%) - LeetSync

--- 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
+++ 3275-minimum-number-of-pushes-to-type-word-i/minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,21 +1,39 @@
 #include <bits/stdc++.h>
 using namespace std; 
-//O(n)
 class Solution {
 public:
     int minimumPushes(string word) {
         int res=0;
-        unordered_map<int,int> map;
-        //2,3,4,5,6,7,8,9
-        int assignkey=2;
+        vector<int> v(26,0);
         for(char &ch:word){
-            if(assignkey>9){
-                assignkey=2;
-            }
-            map[assignkey]++;
-            res+=map[assignkey];
-            assignkey++;
+            v[ch-'a']++;
+        }
+        //sort in desc based on freq
+        sort(begin(v),end(v),greater<int>());
+        for(int i=0;i<26;i++){
+            int freq=v[i];
+            int press=i/8+1;
+            res+=(freq*press);
         }
         return res;
     }
 };
+// O(n)
+// class Solution {
+// public:
+//     int minimumPushes(string word) {
+//         int res=0;
+//         unordered_map<int,int> map;
+//         //2,3,4,5,6,7,8,9
+//         int assignkey=2;
+//         for(char &ch:word){
+//             if(assignkey>9){
+//                 assignkey=2;
+//             }
+//             map[assignkey]++;
+//             res+=map[assignkey];
+//             assignkey++;
+//         }
+//         return res;
+//     }
+// };
