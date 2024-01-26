e94b33db2f7237e5cab389f2139595256e55cd74
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-01-26 18:01:13 +0530

    Time: 8 ms (54.02%) | Memory: 8 MB (57.23%) - LeetSync

--- 576-out-of-boundary-paths/out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/out-of-boundary-paths.cpp
@@ -1,32 +1,64 @@
 #include <bits/stdc++.h>
 using namespace std; 
+// //memo
+// class Solution {
+// public:
+//     int M,N;
+//     int MOD=1e9+7;
+//     vector<vector<int>> dirns{{0,1},{0,-1},{1,0},{-1,0}};
+//     int dp[51][51][51];
+//     int solve(int startRow,int startCol,int maxMove){
+//         if(startRow<0 || startRow>=M || startCol<0 || startCol>=N){
+//             return 1; //found 1 out of bound path
+//         } 
+//         if(maxMove<=0){
+//             return 0;
+//         }
+//         if(dp[startRow][startCol][maxMove]!=-1)
+//             return dp[startRow][startCol][maxMove];
+        
+//         int res=0;
+//         for(vector<int>&dir:dirns){ //dfs
+//             int newR = startRow + dir[0];  
+//             int newC = startCol + dir[1];
+//             res += solve(newR,newC,maxMove-1)%MOD;
+//         }
+//         return dp[startRow][startCol][maxMove]=res;
+//     }
+//     int findPaths(int m, int n, int maxMove, int startRow, int startCol) {
+//         M=m; N=n;
+//         memset(dp,-1,sizeof(dp));
+//         return solve(startRow,startCol,maxMove);
+//     }
+// };
+// //tabulaion
 class Solution {
 public:
-    int M,N;
-    int MOD=1e9+7;
-    vector<vector<int>> dirns{{0,1},{0,-1},{1,0},{-1,0}};
-    int dp[51][51][51];
-    int solve(int startRow,int startCol,int maxMove){
-        if(startRow<0 || startRow>=M || startCol<0 || startCol>=N){
-            return 1; //found 1 out of bound path
-        } 
-        if(maxMove<=0){
-            return 0;
+    int MOD = 1e9 + 7;
+    vector<vector<int>> dirns{{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        // dp[i][j][k] - total no of possible paths leading to out of grid,
+        // when standing at grid[i][j] and having k max moves;
+        int dp[50][50][51];
+        memset(dp, 0, sizeof(dp));
+
+        for (int k = 1; k <= maxMove; k++) {
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    for (vector<int>& dir : dirns) {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 || x >= m || y >= n || y < 0) {
+                            dp[i][j][k] = (dp[i][j][k] + 1) % MOD;
+                        } else {
+                            dp[i][j][k] = (dp[i][j][k] + dp[x][y][k - 1]) % MOD;
+                        }
+                    }
+                }
+            }
         }
-        if(dp[startRow][startCol][maxMove]!=-1)
-            return dp[startRow][startCol][maxMove];
-        
-        int res=0;
-        for(vector<int>&dir:dirns){ //dfs
-            int newR = startRow + dir[0];  
-            int newC = startCol + dir[1];
-            res = (res+ solve(newR,newC,maxMove-1))%MOD;
-        }
-        return dp[startRow][startCol][maxMove]=res;
-    }
-    int findPaths(int m, int n, int maxMove, int startRow, int startCol) {
-        M=m; N=n;
-        memset(dp,-1,sizeof(dp));
-        return solve(startRow,startCol,maxMove);
+
+        return dp[startRow][startColumn][maxMove];
     }
 };
