81c7e73f2e80da55b6e97539b870dd653c3127c3
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-02-17 01:16:11 +0530

    Time: 3 ms (96.76%) | Memory: 12.5 MB (20.44%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -2,27 +2,27 @@
 using namespace std;
 // class Solution {
 // public:
-//     int solve(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<int>>&t){
+//     int solve(vector<vector<int>>& grid,int i,int j,int m,int n,vector<vector<int>>&dp){
 //         if(i==m-1 && j==n-1){
 //             return grid[i][j];
 //         }
-//         if(t[i][j]!=-1){
-//             return t[i][j];
+//         if(dp[i][j]!=-1){
+//             return dp[i][j];
 //         }
 //         if(i==m-1){ //last row- can only go right
-//             return t[i][j]=grid[i][j]+solve(grid,i,j+1,m,n,t);
+//             return dp[i][j]=grid[i][j]+solve(grid,i,j+1,m,n,dp);
 //         }else if(j==n-1){//last col- can only go down
-//             return t[i][j]=grid[i][j]+solve(grid,i+1,j,m,n,t);
+//             return dp[i][j]=grid[i][j]+solve(grid,i+1,j,m,n,dp);
 //         }else{
-//             return t[i][j]=grid[i][j]+min(solve(grid,i,j+1,m,n,t),solve(grid,i+1,j,m,n,t));
+//             return dp[i][j]=grid[i][j]+min(solve(grid,i,j+1,m,n,dp),solve(grid,i+1,j,m,n,dp));
 //         }
 //     }
 //     int minPathSum(vector<vector<int>>& grid) {
 //         int m=grid.size();
 //         int n=grid[0].size();
 
-//         vector<vector<int>> t(m+1,vector<int>(n+1,-1));
-//         return solve(grid,0,0,m,n,t);
+//         vector<vector<int>> dp(m+1,vector<int>(n+1,-1));
+//         return solve(grid,0,0,m,n,dp);
 //     }
 // };
 
@@ -32,22 +32,22 @@ public:
         int m=grid.size();
         int n=grid[0].size();
 
-        vector<vector<int>> t(m,vector<int>(n));
-        //t[i][j]=min path sum to  reach(i,j) frm (0,0)
-        t[0][0]=grid[0][0];
+        vector<vector<int>> dp(m,vector<int>(n));
+        //dp[i][j]=min path sum to  reach(i,j) frm (0,0)
+        dp[0][0]=grid[0][0];
         //fill 1st col
         for(int i=1;i<m;i++){
-            t[i][0]=t[i-1][0]+grid[i][0];
+            dp[i][0]=dp[i-1][0]+grid[i][0];
         }
         //fill 1st row
         for(int j=1;j<n;j++){
-            t[0][j]=t[0][j-1]+grid[0][j];
+            dp[0][j]=dp[0][j-1]+grid[0][j];
         }
         for(int i=1;i<m;i++){
             for(int j=1;j<n;j++){
-                t[i][j]=min(t[i-1][j],t[i][j-1])+grid[i][j];
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1])+grid[i][j];
             }
         }
-        return t[m-1][n-1];
+        return dp[m-1][n-1];
     }
 };
