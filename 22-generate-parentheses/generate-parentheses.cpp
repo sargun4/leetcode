73494e1a8a44dadb83b51ac7c532b3258559fda3
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-03 21:26:18 +0530

    Time: 25 ms (5.11%) | Memory: 45 MB (5.01%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,27 +1,36 @@
+ 
 #include <bits/stdc++.h>
 using namespace std; 
 class Solution {
 public:
-    void comb(int open,int close,vector<string> &res,string str){
-        if (open == 0 and close == 0){
-            res.push_back(str);
+    vector<string> res;
+    bool isvalid(string str){
+        int ctr=0;
+        for(char ch:str){
+            if(ch=='(') ctr++;
+            else ctr--;
+            if(ctr<0) return false;
+        }
+        return ctr==0;
+    }
+    void solve(string &curr,int n){
+        if(curr.length()==2*n){
+            if(isvalid(curr)){
+                res.push_back(curr);
+            }
             return;
         }
-        if (open > 0){
-            str.push_back('(');
-            comb(open-1,close,res,str);
-            str.pop_back();
-        }
-        if (close > 0 && close>open){
-            str.push_back(')');
-            comb(open,close-1,res,str);
-            str.pop_back();
-        } 
+        curr.push_back('(');
+        solve(curr,n);
+        curr.pop_back();
+        
+        curr.push_back(')');
+        solve(curr,n);
+        curr.pop_back();
     }
     vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        string val="";
-        comb(n,n,res,val);
+        string curr="";
+        solve(curr,n);
         return res;
     }
 };
@@ -33,15 +42,40 @@ public:
 //             return;
 //         }
 //         if (open > 0){
-//             comb(open-1,close,res,str+"(");
+//             str.push_back('(');
+//             comb(open-1,close,res,str);
+//             str.pop_back();
 //         }
 //         if (close > 0 && close>open){
-//             comb(open,close-1,res,str+")");
+//             str.push_back(')');
+//             comb(open,close-1,res,str);
+//             str.pop_back();
 //         } 
 //     }
 //     vector<string> generateParenthesis(int n) {
 //         vector<string> res;
-//         comb(n,n,res,"");
+//         string val="";
+//         comb(n,n,res,val);
 //         return res;
 //     }
 // };
+// // class Solution {
+// // public:
+// //     void comb(int open,int close,vector<string> &res,string str){
+// //         if (open == 0 and close == 0){
+// //             res.push_back(str);
+// //             return;
+// //         }
+// //         if (open > 0){
+// //             comb(open-1,close,res,str+"(");
+// //         }
+// //         if (close > 0 && close>open){
+// //             comb(open,close-1,res,str+")");
+// //         } 
+// //     }
+// //     vector<string> generateParenthesis(int n) {
+// //         vector<string> res;
+// //         comb(n,n,res,"");
+// //         return res;
+// //     }
+// // };
