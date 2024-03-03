49b7c20a8fd8190cb7c8c3728292fb8356713905
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-03 21:38:54 +0530

    Time: 4 ms (46.95%) | Memory: 15.7 MB (28.85%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,36 +1,61 @@
  
 #include <bits/stdc++.h>
 using namespace std; 
+// class Solution {
+// public:
+//     vector<string> res;
+//     bool isvalid(string str){
+//         int ctr=0;
+//         for(char ch:str){
+//             if(ch=='(') ctr++;
+//             else ctr--;
+//             if(ctr<0) return false;
+//         }
+//         return ctr==0;
+//     }
+//     void solve(string &curr,int n){
+//         if(curr.length()==2*n){
+//             if(isvalid(curr)){
+//                 res.push_back(curr);
+//             }
+//             return;
+//         }
+//         curr.push_back('(');
+//         solve(curr,n);
+//         curr.pop_back();
+        
+//         curr.push_back(')');
+//         solve(curr,n);
+//         curr.pop_back();
+//     }
+//     vector<string> generateParenthesis(int n) {
+//         string curr="";
+//         solve(curr,n);
+//         return res;
+//     }
+// };
 class Solution {
 public:
-    vector<string> res;
-    bool isvalid(string str){
-        int ctr=0;
-        for(char ch:str){
-            if(ch=='(') ctr++;
-            else ctr--;
-            if(ctr<0) return false;
-        }
-        return ctr==0;
-    }
-    void solve(string &curr,int n){
-        if(curr.length()==2*n){
-            if(isvalid(curr)){
-                res.push_back(curr);
-            }
+    void comb(int open,int close,vector<string> &res,string str){
+        if (open == 0 and close == 0){
+            res.push_back(str);
             return;
         }
-        curr.push_back('(');
-        solve(curr,n);
-        curr.pop_back();
-        
-        curr.push_back(')');
-        solve(curr,n);
-        curr.pop_back();
+        if (open > 0){
+            str.push_back('(');
+            comb(open-1,close,res,str);
+            str.pop_back();
+        }
+        if (close > 0 && close>open){
+            str.push_back(')');
+            comb(open,close-1,res,str);
+            str.pop_back();
+        } 
     }
     vector<string> generateParenthesis(int n) {
-        string curr="";
-        solve(curr,n);
+        vector<string> res;
+        string val="";
+        comb(n,n,res,val);
         return res;
     }
 };
@@ -42,40 +67,15 @@ public:
 //             return;
 //         }
 //         if (open > 0){
-//             str.push_back('(');
-//             comb(open-1,close,res,str);
-//             str.pop_back();
+//             comb(open-1,close,res,str+"(");
 //         }
 //         if (close > 0 && close>open){
-//             str.push_back(')');
-//             comb(open,close-1,res,str);
-//             str.pop_back();
+//             comb(open,close-1,res,str+")");
 //         } 
 //     }
 //     vector<string> generateParenthesis(int n) {
 //         vector<string> res;
-//         string val="";
-//         comb(n,n,res,val);
+//         comb(n,n,res,"");
 //         return res;
 //     }
 // };
-// // class Solution {
-// // public:
-// //     void comb(int open,int close,vector<string> &res,string str){
-// //         if (open == 0 and close == 0){
-// //             res.push_back(str);
-// //             return;
-// //         }
-// //         if (open > 0){
-// //             comb(open-1,close,res,str+"(");
-// //         }
-// //         if (close > 0 && close>open){
-// //             comb(open,close-1,res,str+")");
-// //         } 
-// //     }
-// //     vector<string> generateParenthesis(int n) {
-// //         vector<string> res;
-// //         comb(n,n,res,"");
-// //         return res;
-// //     }
-// // };
