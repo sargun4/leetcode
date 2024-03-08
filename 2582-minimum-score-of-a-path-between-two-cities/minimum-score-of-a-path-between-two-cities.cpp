41da82d60f2bb7b73284a49e05a9e04c7ce4d352
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-09 04:25:06 +0530

    Time: 329 ms (67.77%) | Memory: 144.6 MB (36.17%) - LeetSync

--- 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
+++ 2582-minimum-score-of-a-path-between-two-cities/minimum-score-of-a-path-between-two-cities.cpp
@@ -2,30 +2,60 @@
 using namespace std;
 class Solution {
 public:
-    void dfs(int u,unordered_map<int,vector<pair<int,int>>>&adj,vector<bool>&vis,int&res){
-        vis[u]=true;
-        //neighbour
-        for(auto&p:adj[u]){
-            int v=p.first;
-            int d=p.second;
-            res=min(res,d);
-            if(!vis[v]){
-                dfs(v,adj,vis,res);
+    int minScore(int n, vector<vector<int>>& roads) {
+        int ans=INT_MAX;
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(auto it:roads){//undirected graph
+            graph[it[0]].push_back({it[1],it[2]}); 
+            graph[it[1]].push_back({it[0],it[2]});
+        }
+        vector<int> vis(n+1,0);
+        queue<int> q;
+        q.push(1); vis[1]=1; //1 is src
+        while(!q.empty()){
+            auto node=q.front();
+            q.pop();
+            for(auto it:graph[node]){
+                int v=it.first;
+                int dist=it.second;
+                ans=min(ans,dist);
+                if(vis[v]==0){
+                    vis[v]=1;
+                    q.push(v);
+                }
             }
         }
-    }
-    int minScore(int n, vector<vector<int>>& roads) {
-        unordered_map<int,vector<pair<int,int>>> adj;
-        for(auto &vec:roads){
-            int u=vec[0];
-            int v=vec[1];
-            int d=vec[2];
-            adj[u].push_back({v,d});
-            adj[v].push_back({u,d});
-        }
-        vector<bool> vis(n,false);
-        int res=INT_MAX;
-        dfs(1,adj,vis,res);
-        return res;
+        return ans;
     }
 };
+// #include <bits/stdc++.h>
+// using namespace std;
+// class Solution {
+// public:
+//     void dfs(int u,unordered_map<int,vector<pair<int,int>>>&adj,vector<bool>&vis,int&res){
+//         vis[u]=true;
+//         //neighbour
+//         for(auto&p:adj[u]){
+//             int v=p.first;
+//             int d=p.second;
+//             res=min(res,d);
+//             if(!vis[v]){
+//                 dfs(v,adj,vis,res);
+//             }
+//         }
+//     }
+//     int minScore(int n, vector<vector<int>>& roads) {
+//         unordered_map<int,vector<pair<int,int>>> adj;
+//         for(auto &vec:roads){
+//             int u=vec[0];
+//             int v=vec[1];
+//             int d=vec[2];
+//             adj[u].push_back({v,d});
+//             adj[v].push_back({u,d});
+//         }
+//         vector<bool> vis(n,false);
+//         int res=INT_MAX;
+//         dfs(1,adj,vis,res);
+//         return res;
+//     }
+// };
