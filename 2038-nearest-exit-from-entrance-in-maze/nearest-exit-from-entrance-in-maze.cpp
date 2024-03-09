eddca67e143c1ec7b9586d7d3224f5a009415dd8
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-09 06:52:05 +0530

    Time: 91 ms (77.09%) | Memory: 33.7 MB (75.89%) - LeetSync

--- 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
+++ 2038-nearest-exit-from-entrance-in-maze/nearest-exit-from-entrance-in-maze.cpp
@@ -6,25 +6,28 @@ public:
         int m = maze.size();
         int n = maze[0].size();       
         vector<vector<int>> dirs = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};     
-        queue<pair<int, int>> q;
-        q.push({entrance[0], entrance[1]});
-        maze[entrance[0]][entrance[1]] = '+';        
+        queue<pair<int, int>> q; //q={r,c}
+        int sr=entrance[0]; //startrow
+        int sc=entrance[1]; //startcol
+        q.push({sr, sc});
+        maze[sr][sc] = '+';        
         int steps = 0;        
         while (!q.empty()) {
-            int size = q.size();            
-            while (size--) {
-                int row = q.front().first;
-                int col = q.front().second;
+            int len = q.size();            
+            while (len--) {
+                int r = q.front().first;
+                int c = q.front().second;
                 q.pop();                
-                if (steps > 0 && (row != entrance[0] || col != entrance[1]) && (row == 0 || row == m - 1 || col == 0 || col == n - 1)) {
+                if (steps > 0 && (r != sr || c != sc) 
+                && (r == 0 || r == m - 1 || c == 0 || c == n - 1)) { //if reached
                     return steps;
                 }               
                 for (const auto& dir : dirs) {
-                    int newRow = row + dir[0];
-                    int newCol = col + dir[1];                   
-                    if (newRow >= 0 && newRow < m && newCol >= 0 && newCol < n && maze[newRow][newCol] == '.') {
-                        q.push({newRow, newCol});
-                        maze[newRow][newCol] = '+';
+                    int nrow = r + dir[0];
+                    int ncol = c + dir[1];                   
+                    if (nrow >= 0 && nrow < m && ncol >= 0 && ncol < n && maze[nrow][ncol] == '.') {
+                        q.push({nrow, ncol});
+                        maze[nrow][ncol] = '+';
                     }
                 }
             }            
