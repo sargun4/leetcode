a74736ca8435fffcc803aa6eafcdf1564d400e5d
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-12 08:02:44 +0530

    Time: 271 ms (7.01%) | Memory: 81.5 MB (9.06%) - LeetSync

--- 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/shortest-path-in-binary-matrix.cpp
@@ -83,47 +83,4 @@ public:
 //         }
 //         return res[m-1][n-1]+1;
 //     }
-// };
-// // #include <bits/stdc++.h>
-// // using namespace std;
-// // //using BFS
-// // class Solution {
-// // public:
-// //     vector<vector<int>> dirn{{1,1},{0,1},{1,0},{-1,0},{0,-1},{-1,-1},{1,-1},{-1,1}};
-// //     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
-// //         int m=grid.size();
-// //         int n=grid[0].size();
-// //         if(m==0||n==0||grid[0][0]!=0){
-// //             return-1;
-// //         }
-// //         auto isSafe=[&](int x,int y){
-// //             return (x>=0 && x<m && y>=0 && y<n);
-// //         };
-// //         queue<pair<int,int>> q;
-// //         q.push({0,0});
-// //         grid[0][0]=1;
-// //         int lvl=0;
-// //         while(!q.empty()){
-// //             int len=q.size();
-// //             while(len--){
-// //                 auto curr=q.front();
-// //                 q.pop();
-// //                 int x=curr.first;
-// //                 int y=curr.second;
-// //                 if(x==m-1 && y==n-1){
-// //                     return lvl+1;
-// //                 }
-// //                 for(auto dir: dirn){
-// //                     int nrow=x+dir[0];
-// //                     int ncol=y+dir[1];
-// //                     if(isSafe(nrow,ncol)&& grid[nrow][ncol]==0){
-// //                         q.push({nrow,ncol});
-// //                         grid[nrow][ncol]=1;
-// //                     }
-// //                 }
-// //             }
-// //             lvl++;
-// //         }
-// //         return -1;
-// //     }
-// // };
+// }; 
