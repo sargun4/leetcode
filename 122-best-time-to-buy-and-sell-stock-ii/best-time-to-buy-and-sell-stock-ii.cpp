dee908b2ab543672c0c9da2efa1ab03a5a1f431c
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-17 21:12:08 +0530

    Time: 19 ms (5.01%) | Memory: 20.2 MB (7.17%) - LeetSync

---
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,29 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+public:
+    int f(int idx,int buy,vector<int>&prices,vector<vector<int>> &dp){
+        int n=prices.size();
+        if(idx==n) return 0;//
+        if(dp[idx][buy]!=-1){
+            return dp[idx][buy];
+        }
+        int profit=0;
+        if(buy==1){
+            int take=-prices[idx]+f(idx+1,0,prices,dp); //buy that stock and move to next=>now cant buy so buy=0
+            int skip=0+f(idx+1,1,prices,dp);//skip that stock and move to next=>now hv liberty to buy so buy=1
+            profit=max(take,skip);
+        }else if(buy==0){//sell
+            int take=+prices[idx]+f(idx+1,1,prices,dp); //sell that stock and move to next=>now can buy so buy=1
+            int skip=0+f(idx+1,0,prices,dp);//dont sell that stock and move to next=>now cant buy so buy=0
+            profit=max(take,skip);
+        }
+        return dp[idx][buy]=profit;
+    }
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        vector<vector<int>> dp(n,vector<int>(2,-1));
+        return f(0,1,prices,dp);
+    }
+};
