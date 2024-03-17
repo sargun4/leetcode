d287e59de5e673c6edc3a16d5306e187847f8310
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-17 22:52:39 +0530

    Time: 11 ms (15.50%) | Memory: 17.6 MB (23.13%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -1,6 +1,7 @@
 
 #include <bits/stdc++.h>
 using namespace std;
+//tab
 class Solution {
 public:
     int f(int idx,int buy,vector<int>&prices,vector<vector<int>> &dp){
@@ -23,7 +24,51 @@ public:
     }
     int maxProfit(vector<int>& prices) {
         int n=prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return f(0,1,prices,dp);
+        vector<vector<int>> dp(n+1,vector<int>(2,0));
+        dp[n][0]=dp[n][1]=0;
+        for(int idx=n-1; idx>=0; idx--){
+            for(int buy=0; buy<=1; buy++){
+                int profit=0;
+                if(buy==1){
+                    int take=-prices[idx]+dp[idx+1][0]; //buy that stock and move to next=>now cant buy so buy=0
+                    int skip=0+dp[idx+1][1];//skip that stock and move to next=>now hv liberty to buy so buy=1
+                    profit=max(take,skip);
+                }else if(buy==0){//sell
+                    int take=+prices[idx]+dp[idx+1][1]; //sell that stock and move to next=>now can buy so buy=1
+                    int skip=0+dp[idx+1][0];//dont sell that stock and move to next=>now cant buy so buy=0
+                    profit=max(take,skip);
+                }
+                dp[idx][buy]=profit;
+            }
+        }
+        return dp[0][1];
     }
 };
+// //memo
+// // t-O(n*2)
+// class Solution {
+// public:
+//     int f(int idx,int buy,vector<int>&prices,vector<vector<int>> &dp){
+//         int n=prices.size();
+//         if(idx==n) return 0;//
+//         if(dp[idx][buy]!=-1){
+//             return dp[idx][buy];
+//         }
+//         int profit=0;
+//         if(buy==1){
+//             int take=-prices[idx]+f(idx+1,0,prices,dp); //buy that stock and move to next=>now cant buy so buy=0
+//             int skip=0+f(idx+1,1,prices,dp);//skip that stock and move to next=>now hv liberty to buy so buy=1
+//             profit=max(take,skip);
+//         }else if(buy==0){//sell
+//             int take=+prices[idx]+f(idx+1,1,prices,dp); //sell that stock and move to next=>now can buy so buy=1
+//             int skip=0+f(idx+1,0,prices,dp);//dont sell that stock and move to next=>now cant buy so buy=0
+//             profit=max(take,skip);
+//         }
+//         return dp[idx][buy]=profit;
+//     }
+//     int maxProfit(vector<int>& prices) {
+//         int n=prices.size();
+//         vector<vector<int>> dp(n,vector<int>(2,-1));
+//         return f(0,1,prices,dp);
+//     }
+// };
