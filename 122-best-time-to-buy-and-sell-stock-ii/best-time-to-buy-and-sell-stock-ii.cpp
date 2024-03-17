a03eb305b6141c9eacac0528f053af2308a84642
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-17 22:54:50 +0530

    Time: 15 ms (5.92%) | Memory: 17.7 MB (19.51%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -6,16 +6,16 @@ class Solution {
 public:
     int f(int idx,int buy,vector<int>&prices,vector<vector<int>> &dp){
         int n=prices.size();
-        if(idx==n) return 0;//
+        if(idx==n) return 0;// there are no more days left to make transactions, so 0.
         if(dp[idx][buy]!=-1){
             return dp[idx][buy];
         }
         int profit=0;
-        if(buy==1){
+        if(buy==1){ //two options: either we buy the stock on the current day or we skip it.
             int take=-prices[idx]+f(idx+1,0,prices,dp); //buy that stock and move to next=>now cant buy so buy=0
             int skip=0+f(idx+1,1,prices,dp);//skip that stock and move to next=>now hv liberty to buy so buy=1
             profit=max(take,skip);
-        }else if(buy==0){//sell
+        }else if(buy==0){//sell-e two options: either we sell the stock on the current day or we skip it.
             int take=+prices[idx]+f(idx+1,1,prices,dp); //sell that stock and move to next=>now can buy so buy=1
             int skip=0+f(idx+1,0,prices,dp);//dont sell that stock and move to next=>now cant buy so buy=0
             profit=max(take,skip);
