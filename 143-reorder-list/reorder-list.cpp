54df8e1ca56ee2c89913aaadad251aa2f3591e3f
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-03-23 14:52:07 +0530

    Time: 30 ms (21.95%) | Memory: 22 MB (28.15%) - LeetSync

--- 143-reorder-list/reorder-list.cpp
+++ 143-reorder-list/reorder-list.cpp
@@ -1,35 +1,66 @@
- #include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
+// struct ListNode {
+//     int val;
+//     ListNode *next;
+//     ListNode() : val(0), next(nullptr) {}
+//     ListNode(int x) : val(x), next(nullptr) {}
+//     ListNode(int x, ListNode *next) : val(x), next(next) {}
+// };
+// using stack
 class Solution {
 public:
-    ListNode* revLL(ListNode* head){
-        ListNode* prev=NULL;
+    void reorderList(ListNode* head) {
+        stack<ListNode*> st;
         ListNode* curr=head;
-        ListNode* temp;
-        while(curr!=NULL){
-            temp=curr->next;
-            curr->next=prev;
-            prev=curr;
+        while(curr){
+            st.push(curr);
+            curr=curr->next;
+        }
+        int k=st.size()/2;
+        curr=head;
+        while(k--){
+            ListNode* topnode=st.top();
+            st.pop();
+            ListNode* temp=curr->next;
+            curr->next=topnode;
+            topnode->next=temp;
             curr=temp;
         }
-        return prev;
-    }
-    void reorderList(ListNode* head) {
-        ListNode* slow=head;
-        ListNode* fast=head;
-        while(fast && fast->next){
-            slow=slow->next;
-            fast=fast->next->next;
-        }
-        ListNode* rev=revLL(slow);
-        ListNode* curr=head;
-        while(rev->next!=NULL){
-            ListNode* tempcurr=curr->next;
-            curr->next=rev;
-            ListNode* temprev=rev->next;
-            rev->next=tempcurr;
-            curr=tempcurr;
-            rev=temprev;
-        }
+        curr->next=NULL;
     }
 };
+//using reverseLL
+// class Solution {
+// public:
+//     ListNode* revLL(ListNode* head){
+//         ListNode* prev=NULL;
+//         ListNode* curr=head;
+//         ListNode* temp;
+//         while(curr!=NULL){
+//             temp=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=temp;
+//         }
+//         return prev;
+//     }
+//     void reorderList(ListNode* head) {
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         while(fast && fast->next){
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         ListNode* rev=revLL(slow);
+//         ListNode* curr=head;
+//         while(rev->next!=NULL){
+//             ListNode* tempcurr=curr->next;
+//             curr->next=rev;
+//             ListNode* temprev=rev->next;
+//             rev->next=tempcurr;
+//             curr=tempcurr;
+//             rev=temprev;
+//         }
+//     }
+// };
