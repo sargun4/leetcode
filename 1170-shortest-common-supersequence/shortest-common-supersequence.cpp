2f73c686925864cd3b8b45228616560daffa2b17
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-04-06 20:42:00 +0530

    Time: 12 ms (96.53%) | Memory: 11.9 MB (94.88%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.cpp
@@ -2,33 +2,33 @@
 using namespace std;
 class Solution {
 public:
+//dp[i][j]=scs of s1 of length i and s2 of length j
     string shortestCommonSupersequence(string str1, string str2) {
         int n=str1.length(); int m=str2.length();
         int dp[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
                 if(i==0||j==0){
-                    dp[i][j]= 0;
+                    dp[i][j]= i+j;
                 }else if(str1[i-1]==str2[j-1]){
                     dp[i][j]=1+dp[i-1][j-1];
-                }else{
-                    dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+                }else{//those chars not equal
+                    dp[i][j]=1+min(dp[i][j-1],dp[i-1][j]);
                 }
             }
         }
-        string ans="";
-        int i=n;
-        int j=m;
+        string ans=""; 
+        int i=n;  int j=m;
         while(i>0 && j>0){
             if(str1[i-1]==str2[j-1]){
                 ans+=str1[i-1];
                 i--; j--;
-            }else if(dp[i-1][j]>dp[i][j-1]){
+            }else if(dp[i-1][j]<dp[i][j-1]){//taking the min one
                 ans+=str1[i-1];
-                i--;
+                i--; //move up
             }else{
                 ans+=str2[j-1];
-                j--;
+                j--;//move down
             }
         }
         while(i>0){
@@ -43,3 +43,48 @@ public:
         return ans;
     }
 };
+
+
+// class Solution {
+// public:
+//     string shortestCommonSupersequence(string str1, string str2) {
+//         int n=str1.length(); int m=str2.length();
+//         int dp[n+1][m+1];
+//         for(int i=0;i<=n;i++){
+//             for(int j=0;j<=m;j++){
+//                 if(i==0||j==0){
+//                     dp[i][j]= 0;
+//                 }else if(str1[i-1]==str2[j-1]){
+//                     dp[i][j]=1+dp[i-1][j-1];
+//                 }else{
+//                     dp[i][j]=max(dp[i][j-1],dp[i-1][j]);
+//                 }
+//             }
+//         }
+//         string ans="";
+//         int i=n;  int j=m;
+//         while(i>0 && j>0){
+//             if(str1[i-1]==str2[j-1]){
+//                 ans+=str1[i-1];
+//                 i--; j--;
+//             }else if(dp[i-1][j]>dp[i][j-1]){
+//                 ans+=str1[i-1];
+//                 i--;
+//             }else{
+//                 ans+=str2[j-1];
+//                 j--;
+//             }
+//         }
+//         while(i>0){
+//             ans+=str1[i-1];
+//             i--;
+//         }
+//         while(j>0){
+//             ans+=str2[j-1];
+//             j--;
+//         }
+//         reverse(ans.begin(), ans.end());
+//         return ans;
+//     }
+// };
+// // https://www.geeksforgeeks.org/print-shortest-common-supersequence/
