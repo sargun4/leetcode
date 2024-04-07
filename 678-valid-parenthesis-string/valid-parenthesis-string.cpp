ca5af160aa4e30c1cdd90fe0aa97c0d481bffec2
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-04-07 16:50:55 +0530

    Time: 0 ms (100.00%) | Memory: 7.5 MB (39.07%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -29,33 +29,67 @@ using namespace std;
 //         memset(dp,-1,sizeof(dp));
 //         return solve(0,0,s,n);
 //     }
+// // };
+// //2-tab
+// class Solution {
+// public:
+//     bool checkValidString(string s) {
+//         int n=s.length();
+//         vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
+        
+//         dp[n][0]=true;
+
+//         for(int i=n-1;i>=0;i--){
+//             for(int open=0;open<=n;open++){
+//                 bool isvalid=false;
+//                 if(s[i]=='*'){
+//                     isvalid |=dp[i+1][open+1];// if * considered as open brkt
+//                     if(open>0){
+//                         isvalid |=dp[i+1][open-1]; //if * considered as closed brkt
+//                     }
+//                     isvalid |=dp[i+1][open];//if * considered as empty ""
+//                 }else if(s[i]=='('){
+//                     isvalid |=dp[i+1][open+1];
+//                 }else if(open>0){
+//                     isvalid |=dp[i+1][open-1];
+//                 }
+//                 dp[i][open] =isvalid;
+//             }
+//         }   
+//         return dp[0][0];
+//     }
 // };
-//2-tab
+
+
+//3-stack
 class Solution {
 public:
     bool checkValidString(string s) {
         int n=s.length();
-        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
-        
-        dp[n][0]=true;
-
-        for(int i=n-1;i>=0;i--){
-            for(int open=0;open<=n;open++){
-                bool isvalid=false;
-                if(s[i]=='*'){
-                    isvalid |=dp[i+1][open+1];// if * considered as open brkt
-                    if(open>0){
-                        isvalid |=dp[i+1][open-1]; //if * considered as closed brkt
-                    }
-                    isvalid |=dp[i+1][open];//if * considered as empty ""
-                }else if(s[i]=='('){
-                    isvalid |=dp[i+1][open+1];
-                }else if(open>0){
-                    isvalid |=dp[i+1][open-1];
+        stack<int> openbrkt_st;
+        stack<int> asterisk_st;
+        for(int i=0;i<n;i++){
+            if(s[i]=='('){
+                openbrkt_st.push(i);
+            }else if(s[i]=='*'){
+                asterisk_st.push(i);
+            }else{//closed brkt
+                if(!openbrkt_st.empty()){
+                    openbrkt_st.pop();
+                }else if(!asterisk_st.empty()){
+                    asterisk_st.pop();
+                }else{
+                    return false;
                 }
-                dp[i][open] =isvalid;
             }
-        }   
-        return dp[0][0];
+        }
+        while(!openbrkt_st.empty() && !asterisk_st.empty()){
+            if(openbrkt_st.top()>asterisk_st.top()){
+                return false;
+            }
+            openbrkt_st.pop();
+            asterisk_st.pop();
+        }
+        return openbrkt_st.empty();
     }
 };
