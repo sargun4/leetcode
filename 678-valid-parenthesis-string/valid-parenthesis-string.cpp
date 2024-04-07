693ab688b5984d8736d67b6e4eac8eb4f783b6b9
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-04-07 16:37:53 +0530

    Time: 4 ms (9.36%) | Memory: 8.2 MB (12.62%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,32 +1,61 @@
 
 #include <bits/stdc++.h>
 using namespace std; 
-
+// //1memo
+// class Solution {
+// public:
+//     int dp[101][101];
+//     bool solve(int idx,int open,string &s,int n){
+//         if(idx==n) return open==0;
+//         if(dp[idx][open]!=-1){
+//             return dp[idx][open];
+//         }
+//         bool isvalid = false;
+//         if(s[idx]=='*'){
+//             isvalid |=solve(idx+1,open+1,s,n); //if * considered as open brkt
+//             isvalid |=solve(idx+1,open,s,n); //if * considered as empty ""
+//             if(open>0){
+//                 isvalid |=solve(idx+1,open-1,s,n); //if * considered as closed brkt
+//             }
+//         }else if(s[idx]=='('){
+//             isvalid |=solve(idx+1,open+1,s,n);
+//         }else if(open>0){
+//             isvalid |=solve(idx+1,open-1,s,n);
+//         }
+//         return dp[idx][open]=isvalid;
+//     }
+//     bool checkValidString(string s) {
+//         int n=s.length();
+//         memset(dp,-1,sizeof(dp));
+//         return solve(0,0,s,n);
+//     }
+// };
+//2-tab
 class Solution {
 public:
-    int dp[101][101];
-    bool solve(int idx,int open,string &s,int n){
-        if(idx==n) return open==0;
-        if(dp[idx][open]!=-1){
-            return dp[idx][open];
-        }
-        bool isvalid = false;
-        if(s[idx]=='*'){
-            isvalid |=solve(idx+1,open+1,s,n); //if * considered as open brkt
-            isvalid |=solve(idx+1,open,s,n); //if * considered as empty ""
-            if(open>0){
-                isvalid |=solve(idx+1,open-1,s,n); //if * considered as closed brkt
-            }
-        }else if(s[idx]=='('){
-            isvalid |=solve(idx+1,open+1,s,n);
-        }else if(open>0){
-            isvalid |=solve(idx+1,open-1,s,n);
-        }
-        return dp[idx][open]=isvalid;
-    }
     bool checkValidString(string s) {
         int n=s.length();
-        memset(dp,-1,sizeof(dp));
-        return solve(0,0,s,n);
+        vector<vector<bool>> dp(n+1,vector<bool>(n+1,false));
+        
+        dp[n][0]=true;
+
+        for(int i=n-1;i>=0;i--){
+            for(int open=0;open<=n;open++){
+                bool isvalid=false;
+                if(s[i]=='*'){
+                    isvalid |=dp[i+1][open+1];// if * considered as open brkt
+                    if(open>0){
+                        isvalid |=dp[i+1][open-1]; //if * considered as closed brkt
+                    }
+                    isvalid |=dp[i+1][open];//if * considered as empty ""
+                }else if(s[i]=='('){
+                    isvalid |=dp[i+1][open+1];
+                }else if(open>0){
+                    isvalid |=dp[i+1][open-1];
+                }
+                dp[i][open] =isvalid;
+            }
+        }   
+        return dp[0][0];
     }
 };
