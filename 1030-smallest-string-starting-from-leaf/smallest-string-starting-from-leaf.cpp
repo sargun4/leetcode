85b9cba6043c1a5ae3cf96ccf32a87d0c2eded56
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-04-17 19:48:38 +0530

    Time: 7 ms (80.26%) | Memory: 20.3 MB (78.11%) - LeetSync

--- 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.cpp
@@ -10,23 +10,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+//bfs
 class Solution {
 public:
-    string res="";
-    void dfs(TreeNode *root,string curr){
-        if(!root) return;
-        curr=char(root->val+'a')+curr;
-        if(!root->left && !root->right){
-            if(res==""||res>curr){
-                res=curr;
-            }
-            return;
-        }
-        dfs(root->left,curr);
-        dfs(root->right,curr);
-    }
     string smallestFromLeaf(TreeNode* root) {
-        dfs(root,"");
+        queue<pair<TreeNode*,string>>q;
+        q.push({root,string(1,char(root->val+'a'))});
+        string res="";
+        while(!q.empty()){
+            auto[node,curr]=q.front();
+            q.pop();
+            if(!node->left && !node->right){
+                if(res=="" ||res>curr){
+                    res=curr;
+                }
+            }
+            if(node->left){
+                q.push({node->left,char(node->left->val+'a')+curr});
+            }
+            if(node->right){
+                q.push({node->right,char(node->right->val+'a')+curr});
+            }
+        }
         return res;
     }
 };
+// //dfs
+// class Solution {
+// public:
+//     string res="";
+//     void dfs(TreeNode *root,string curr){
+//         if(!root) return;
+//         curr=char(root->val+'a')+curr;
+//         if(!root->left && !root->right){
+//             if(res==""||res>curr){
+//                 res=curr;
+//             }
+//             return;
+//         }
+//         dfs(root->left,curr);
+//         dfs(root->right,curr);
+//     }
+//     string smallestFromLeaf(TreeNode* root) {
+//         dfs(root,"");
+//         return res;
+//     }
+// };
