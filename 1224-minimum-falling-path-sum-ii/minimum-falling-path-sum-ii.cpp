434f7c23820c8a1b555911abd042d4fc66c594ba
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-04-26 23:20:25 +0530

    Time: 407 ms (11.50%) | Memory: 18.4 MB (16.18%) - LeetSync

---
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,32 @@
+class Solution {  
+    int f(int i, int j, vector<vector<int>>& mat, vector<vector<int>>& dp) {
+        if(i < 0 || j < 0 || j >= mat[0].size()) {
+            return 1e8;
+        }  
+        if(dp[i][j] != -1) {
+            return dp[i][j];
+        } 
+        if(i == 0) {
+            return mat[0][j];
+        } 
+        int mini = 1e8; 
+        for(int k=0; k<mat[0].size(); k++) {
+            if(k != j) {
+                mini = min(mini, mat[i][j] + f(i-1, k, mat, dp));
+            }
+        } 
+        dp[i][j] = mini; 
+        return dp[i][j];
+    } 
+public:
+    int minFallingPathSum(vector<vector<int>>& mat) {
+        int mini = 1e8;
+        int n = mat.size(), m = mat[0].size(); 
+        // declared a dp array
+        vector<vector<int>> dp(n, vector<int>(m, -1)); 
+        for(int j=0; j<m; j++) {
+            mini = min(mini, f(n-1, j, mat, dp));
+        }
+        return mini;
+    }
+};
