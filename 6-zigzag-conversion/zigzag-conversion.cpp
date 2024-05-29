57c374c7bc87e66e180d49acd90e67f1d9e51f64
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-05-29 13:47:06 +0530

    Time: 11 ms (54.48%) | Memory: 12.4 MB (71.73%) - LeetSync

---
+++ 6-zigzag-conversion/zigzag-conversion.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public: 
+    string convert(string& s, int numRows) {
+        if (numRows == 1) return s;
+        vector<string> rows(min(numRows, int(s.size())));
+        int currRow = 0;
+        bool goingDown = false;
+        for (char c : s) {
+            rows[currRow] += c;
+            if (currRow == 0 || currRow == numRows - 1) {
+                goingDown = !goingDown;
+            }
+            currRow += goingDown ? 1 : -1;
+        }
+        string res;
+        for (string& row : rows) {
+            res += row;
+        }
+        return res;
+    } 
+};
