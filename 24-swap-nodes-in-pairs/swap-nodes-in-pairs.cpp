6787b27d56c1be1a61e322ae9af47553cc2cdfad
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 22:39:39 +0530

    Time: 0 ms (100.00%) | Memory: 9.5 MB (7.73%) - LeetSync

--- 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/swap-nodes-in-pairs.cpp
@@ -1,57 +1,59 @@
-class Solution {
-public:
-    ListNode* swapPairs(ListNode* head) {
-        if(head==NULL ||head->next==NULL){
-            return head;
-        }
-        ListNode* temp=head->next;
-        head->next=swapPairs(head->next->next);
-        temp->next=head;
-        return temp;
-    }
-};
 // class Solution {
 // public:
-//     ListNode* revknodes(ListNode* head, int k) {
-//         ListNode* prev = NULL;
-//         ListNode* curr = head;
-//         ListNode* temp;
-//         int ctr = 0; 
-//         // Count the number of nodes in the current k-group
-//         ListNode* countNode = head;
-//         while (ctr < k && countNode != NULL) {
-//             countNode = countNode->next;
-//             ctr++;
-//         } 
-//         // If there are at least k nodes, proceed with reversal
-//         if (ctr == k) {
-//             ctr = 0;
-//             while (curr != NULL && ctr < k) {
-//                 temp = curr->next;
-//                 curr->next = prev;
-//                 prev = curr;
-//                 curr = temp;
-//                 ctr++;
-//             } 
-//             if (temp != NULL) {
-//                 // Recursively reverse the next k nodes
-//                 head->next = revknodes(temp, k);
-//             }
-//         } else {
-//             // If there are less than k nodes, reverse them back to maintain order
-//             while (prev != NULL) {
-//                 temp = prev->next;
-//                 prev->next = curr;
-//                 curr = prev;
-//                 prev = temp;
-//             }
-//             return curr;
-//         } 
-//         return prev;
-//     } 
 //     ListNode* swapPairs(ListNode* head) {
-//         // Call revknodes with k=2 to reverse every 2 nodes
-//         head = revknodes(head, 2);
-//         return head;
+//         if(head==NULL ||head->next==NULL){
+//             return head;
+//         }
+//         ListNode* temp=head->next;
+//         head->next=swapPairs(head->next->next);
+//         temp->next=head;
+//         return temp;
 //     }
 // };
+ 
+// class Solution {
+// public:
+//     ListNode* swapPairs(ListNode* head) {
+//         if (!head || !head->next) return head;
+//         ListNode *temp = new ListNode(0,head);
+//         ListNode *curr = head;
+//         ListNode *prev = temp;
+//         while(curr && curr->next){
+//             prev->next = curr->next;
+//             curr->next = prev->next->next;
+//             prev->next->next = curr;
+//             prev = curr;
+//             curr = curr->next;
+//         }
+//         return temp->next;
+//     }
+// }; 
+
+class Solution {
+public:
+    ListNode* revknodes(ListNode* head, int k) {
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        ListNode* temp = NULL;
+        int ctr = 0;
+        // rev first k nodes of the linked list
+        while (curr != NULL && ctr < k) {
+            temp = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = temp;
+            ctr++;
+        }
+        // Now head points to the kth node. Recursively call for the list starting from current.
+        // And make rest of the list as next of the first node
+        if (temp != NULL) {
+            head->next = revknodes(temp, k);
+        }
+        // prev is the new head of the reversed group
+        return prev;
+    } 
+    ListNode* swapPairs(ListNode* head) {
+        // We are swapping pairs, so k = 2
+        return revknodes(head, 2);
+    }
+}; 
