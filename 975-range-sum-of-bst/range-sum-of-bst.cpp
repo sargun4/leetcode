aac36697481b78725d8f34333ea0358d54853638
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 22:51:13 +0530

    Time: 94 ms (35.17%) | Memory: 63.3 MB (6.10%) - LeetSync

--- 975-range-sum-of-bst/range-sum-of-bst.cpp
+++ 975-range-sum-of-bst/range-sum-of-bst.cpp
@@ -3,13 +3,13 @@ public:
     int rangeSumBST(TreeNode* root, int low, int high) {
         if(!root) return 0;
         int sum=0;
-        queue<TreeNode*> q;
+        queue<TreeNode*>q;
         q.push(root);
         while(!q.empty()){
             TreeNode* node=q.front();
             q.pop();
             if (node->val >= low && node->val <= high) {
-                sum += node->val;
+                sum+=node->val;
             }
             if (node->left && node->val > low) {
                 q.push(node->left);
