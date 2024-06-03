51c3a12612869acfab2d73636f8c5ec482fdf95c
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-04 00:03:17 +0530

    Time: 4 ms (30.84%) | Memory: 7.5 MB (92.41%) - LeetSync

---
+++ 782-jewels-and-stones/jewels-and-stones.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int ctr=0;
+        for(int i=0;i<jewels.size();i++){
+            for(int j=0;j<stones.size();j++){
+                if(stones[j]==jewels[i]){
+                    ctr++;
+                }
+            }
+        }
+        return ctr;
+    }
+};
