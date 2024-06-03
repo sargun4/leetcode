6f870ba4f0e6a6eeecf8757b76260746e83b49d1
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 19:35:40 +0530

    Time: 4 ms (33.85%) | Memory: 7.9 MB (46.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -6,9 +6,9 @@ public:
         int n=numRows;
         vector<vector<int>> triangle;
         for(int i=0;i<n;i++){
-            vector<int> row(i+1,1);
+            vector<int> row(i+1,1);//intilaze w 1
             for(int j=1;j<i;j++){
-                row[j]=triangle[i-1][j]+triangle[i-1][j-1];
+                row[j]=triangle[i-1][j]+triangle[i-1][j-1]; //prevrows elmnt [i-1][j] + the one on its left [i-1][j-1]
             }
             triangle.push_back(row);
         }
