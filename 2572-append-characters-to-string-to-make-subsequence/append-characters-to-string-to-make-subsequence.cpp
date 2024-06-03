36fa97aa97cebf6f8cdb5299f0926e60b8e52592
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 16:14:47 +0530

    Time: 15 ms (87.56%) | Memory: 12 MB (47.32%) - LeetSync

---
+++ 2572-append-characters-to-string-to-make-subsequence/append-characters-to-string-to-make-subsequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int appendCharacters(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+        int i=0;int j=0;
+        while(i<m && j<n){
+            if(s[i]==t[j]){//if matching chars
+                j++;
+            }
+            i++;
+        }
+        return n-j;
+    }
+};
