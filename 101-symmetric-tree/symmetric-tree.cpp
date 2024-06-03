34782ecacda7bc2f187c896eb055b1b98261d875
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 20:31:40 +0530

    Time: 3 ms (70.40%) | Memory: 17.3 MB (37.79%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.cpp
+++ 101-symmetric-tree/symmetric-tree.cpp
@@ -1,14 +1,4 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+ 
 class Solution{
 public:
     // check for symmetric tree
@@ -19,17 +9,17 @@ public:
         // Recursive call: compare left subtree and right subtree
         return isSymmetricUtil(root->left, root->right);
     }
-    bool isSymmetricUtil(TreeNode* left, TreeNode* right){
+    bool isSymmetricUtil(TreeNode* t1, TreeNode* t2){
         // 1. Both null -> symmetric
-        if(left == NULL && right == NULL)
+        if(t1 == NULL && t2 == NULL)
             return true;
         // 2. One null, the other not -> not symmetric
-        if(left == NULL || right == NULL)
+        if(t1 == NULL || t2 == NULL)
             return false;
         // 3. Both not null, but different values -> not symmetric
-        if(left->val != right->val)
+        if(t1->val != t2->val)
             return false;
         // Recursive call: check for symmetric tree with left child and right child
-        return isSymmetricUtil(left->left, right->right) && isSymmetricUtil(left->right, right->left);
+        return isSymmetricUtil(t1->left, t2->right) && isSymmetricUtil(t1->right, t2->left);
     }
 };
