867b861351e2091efdcaf0702cc796909cac8942
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-04 02:29:53 +0530

    Time: 104 ms (80.58%) | Memory: 60.9 MB (95.98%) - LeetSync

--- 969-number-of-recent-calls/number-of-recent-calls.cpp
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -1,20 +1,33 @@
- 
+
 class RecentCounter {
 public:
-    unordered_map<int,int> map;
-    RecentCounter() { 
-    } 
+queue<int>q;
+    RecentCounter() {   
+    }
     int ping(int t) {
-        map[t]++;
-
-        int ans = 0; 
-
-        int lower = t - 3000, upper = t;
-        for(auto &x : map){
-            if(x.first >= lower && x.second <= upper){
-                ans = ans + x.second;
-            }
-        }
-        return ans;
+       q.push(t);
+       while(q.front()<t-3000){
+           q.pop();
+       }
+       return q.size(); 
     }
 }; 
+// class RecentCounter {
+// public:
+//     unordered_map<int,int> map;
+//     RecentCounter() { 
+//     } 
+//     int ping(int t) {
+//         map[t]++;
+
+//         int ans = 0; 
+
+//         int lower = t - 3000, upper = t;
+//         for(auto &x : map){
+//             if(x.first >= lower && x.second <= upper){
+//                 ans = ans + x.second;
+//             }
+//         }
+//         return ans;
+//     }
+// }; 
