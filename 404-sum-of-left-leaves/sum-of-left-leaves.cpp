e8ea0d5b80924ab579ac6a4a887c35fadef23de3
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-04 00:34:10 +0530

    Time: 4 ms (37.48%) | Memory: 14.8 MB (48.19%) - LeetSync

---
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -0,0 +1,28 @@
+
+
+class Solution {
+public:
+    int sumOfLeftLeaves(TreeNode* root) {
+        int res = 0; 
+        // Update result if root is not NULL
+        if (root != NULL){
+            // If left of root is NULL, then add val of
+            // left child
+            if (isLeaf(root->left))
+                res += root->left->val;
+            else // Else recur for left child of root
+                res += sumOfLeftLeaves(root->left);
+                
+            // Recur for right child of root and update res
+            res += sumOfLeftLeaves(root->right);
+        }
+        return res;
+    }
+    bool isLeaf(TreeNode*node){
+        if (node == NULL)
+            return false;
+        if (node->left == NULL && node->right == NULL)
+            return true;
+        return false;
+    }
+};
