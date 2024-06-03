09b53535ec03862e637d92c07319bc71c3218ab4
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 20:39:09 +0530

    Time: 14 ms (9.48%) | Memory: 20.1 MB (11.98%) - LeetSync

--- 112-path-sum/path-sum.cpp
+++ 112-path-sum/path-sum.cpp
@@ -1,52 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std;
+//bfs
 class Solution {
 public:
-    bool inorder(TreeNode* root,int sum,int targetSum){
-        if(root==NULL){
+    bool hasPathSum(TreeNode* root, int target) {
+        //if root is NULL you can't get any value even zero
+        if(root==NULL)
             return false;
-        }
-        sum+=root->val;
-        //if we reach leaf node
-        if(root->left==NULL && root->right==NULL){
-            if(sum==targetSum) return true;
-            return false;
-        }
-        bool leftside=inorder(root->left,sum,targetSum);
-        bool rightside=inorder(root->right,sum,targetSum);
+        //for BFS to store nodes and keep track of target
+        queue<pair<TreeNode*,int>>q; //{node,sum}
+        
+        //pushed given root and target we need
+        q.push({root,target});
+        
+        //BFS
+        while(!q.empty()){
 
-        return leftside||rightside;
-    }
-    bool hasPathSum(TreeNode* root, int targetSum) {
-        int sum=0;
-        bool res=inorder(root,sum,targetSum);
-        return res;
+            TreeNode *root=q.front().first;
+            target=q.front().second;
+            q.pop();
+            
+            //if there is no left and right child node must be a leaf node
+            //and we'll subtract val of that node and check it is zero or not 
+            // since root-to-leaf paths are needed.
+            if(root->right==NULL && root->left==NULL){//leaf
+                if(target-(root->val)==0)
+                    return true;//got a path
+            }
+            //if child present push to queue else ignore
+            if(root->left) q.push({root->left,target-(root->val)});
+            if(root->right) q.push({root->right,target-(root->val)});
+        }
+        return false;
     }
 };
+
+// #include <bits/stdc++.h>
+// using namespace std;
 // class Solution {
 // public:
+//     bool inorder(TreeNode* root,int sum,int targetSum){
+//         if(root==NULL){
+//             return false;
+//         }
+//         sum+=root->val;
+//         //if we reach leaf node
+//         if(root->left==NULL && root->right==NULL){
+//             if(sum==targetSum) return true;
+//             return false;
+//         }
+//         bool leftside=inorder(root->left,sum,targetSum);
+//         bool rightside=inorder(root->right,sum,targetSum);
+
+//         return leftside||rightside;
+//     }
 //     bool hasPathSum(TreeNode* root, int targetSum) {
-//         // return false if the root is nullptr
-//         if(!root) return false;
-//         // if it reaches to the end and the val is equal to the sum, return true
-//         if(!root->left && !root->right && root->val == targetSum) return true;
-//         // otherwise, we traverse left node and right node with targetSum `sum - root->val`
-//         return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum-root->val);
+//         int sum=0;
+//         bool res=inorder(root,sum,targetSum);
+//         return res;
 //     }
 // };
+// // class Solution {
+// // public:
+// //     bool hasPathSum(TreeNode* root, int targetSum) {
+// //         // return false if the root is nullptr
+// //         if(!root) return false;
+// //         // if it reaches to the end and the val is equal to the sum, return true
+// //         if(!root->left && !root->right && root->val == targetSum) return true;
+// //         // otherwise, we traverse left node and right node with targetSum `sum - root->val`
+// //         return hasPathSum(root->left, targetSum - root->val) || hasPathSum(root->right, targetSum-root->val);
+// //     }
+// // };
 
-
-// int helper(node*root,int targetSum,long int currSum,unordered_map<long int,int> pathcount){
-//     if(root==NULL) return 0;
-//     currSum+=root->data;
-//     int count=pathcount[currSum-targetSum];
-//     pathcount[currSum]++;
-//     count+=helper(root->left,targetSum,currSum,pathcount)+helper(root->right,targetSum,currSum,pathcount);
-//     pathcount[currSum]--; //backtracking
-//     return count;
-// }
-// int pathSum(node *root,int targetSum){
-//     unordered_map<long int,int> pathcount;
-//     pathcount[0]=1;
-//     return helper(root,targetSum,0,pathcount);
-// }
