ceed098cad7d419048f41dfb1bbbd4a0bf511fc9
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 19:51:58 +0530

    Time: 11 ms (78.98%) | Memory: 15 MB (61.13%) - LeetSync

--- 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.cpp
@@ -13,18 +13,40 @@ public:
     }
 };
 */
+// class Solution {
+// public:
+//     vector<int> res;
+//     void traverse(Node* root) {
+//         if(root == NULL) return;
+//         for(auto node:root->children)
+//             traverse(node);
+//         res.push_back(root->val);
+//     }
+//     vector<int> postorder(Node* root) {
+//         res.clear();
+//         traverse(root);
+//         return res;
+//     }
+// };
+
 class Solution {
 public:
-    vector<int> res;
-    void traverse(Node* root) {
-        if(root == NULL) return;
-        for(auto node:root->children)
-            traverse(node);
-        res.push_back(root->val);
-    }
     vector<int> postorder(Node* root) {
-        res.clear();
-        traverse(root);
-        return res;
+        if(!root) return {};
+        stack<Node*> st;
+        st.push(root);
+        vector<int> ans;
+        while(!st.empty()){
+            Node* node=st.top(); 
+            st.pop();
+            int size=node->children.size();
+            ans.push_back(node->val);
+            for(int i=0;i<size;i++){
+                if(node->children[i])
+                    st.push(node->children[i]);
+            }
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
     }
 };
