400e1e8e0fcde7fb87d98ded18168caf1006de15
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-03 19:30:05 +0530

    Time: 32 ms (30.14%) | Memory: 21.7 MB (21.13%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,76 +1,78 @@
-//dfs
-#include <bits/stdc++.h>
-using namespace std;
-class Solution {
-public:
-    bool isvalid(int i,int j,int n,int m,vector<vector<char>>& grid){
-        if(i>=0 && i<n && j>=0 && j<m && grid[i][j]=='1'){
-            return true;
-        }
-        return false;
-    }
-    void countIslands(int i,int j,int n,int m,vector<vector<char>>& grid){
-        grid[i][j]='0';
-        if(isvalid(i+1,j,n,m,grid))
-            countIslands(i+1,j,n,m,grid);
-        if(isvalid(i-1,j,n,m,grid))
-            countIslands(i-1,j,n,m,grid);
-        if(isvalid(i,j+1,n,m,grid))
-            countIslands(i,j+1,n,m,grid);
-        if(isvalid(i,j-1,n,m,grid))
-            countIslands(i,j-1,n,m,grid);
-    }
-    int numIslands(vector<vector<char>>& grid) {
-        int n=grid.size();
-        int m=grid[0].size();
-        int ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(grid[i][j]=='1'){
-                    ans++;
-                    countIslands(i,j,n,m,grid);
-                }
-            }
-        }
-        return ans;
-    }
-};
-// //bfs
+// //dfs
+// #include <bits/stdc++.h>
+// using namespace std;
 // class Solution {
 // public:
-//     //T: O(row * col)
-//     //S: O(col)
-//     int numIslands(vector<vector<char>>& grid) {
-//         if(grid.empty())
-//             return 0;
-//         int island_ctr = 0;
-//         for(int m = 0; m < grid.size(); ++m) {
-//             for(int n = 0; n < grid[0].size(); ++n) {
-//                 if(grid[m][n] == '1') {
-//                     ++island_ctr;
-//                     std::queue<pair<int, int>> bfs_que;//<m, col>
-//                     grid[m][n] = '2';
-//                     bfs_que.push(make_pair(m, n));
-//                     while(!bfs_que.empty()) {
-//                         std::pair<int, int> visited_node = bfs_que.front();
-//                         bfs_que.pop();
-//                         for(auto d : dir) {
-//                             int next_row = visited_node.first + d.first;
-//                             int next_col = visited_node.second + d.second;
-//                             if(next_row >= 0 && next_row < grid.size() && next_col >= 0 && next_col < grid[0].size() && grid[next_row][next_col] == '1') {
-//                                 grid[next_row][next_col] = '2';
-//                                 bfs_que.push(make_pair(next_row, next_col));
-//                             }
-//                         }
-//                     }
-//                 }   
-//             }  
+//     bool isvalid(int i,int j,int n,int m,vector<vector<char>>& grid){
+//         if(i>=0 && i<n && j>=0 && j<m && grid[i][j]=='1'){
+//             return true;
 //         }
-//         return island_ctr;
+//         return false;
 //     }
-// private:
-//     vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};//<m, col>
+//     void countIslands(int i,int j,int n,int m,vector<vector<char>>& grid){
+//         grid[i][j]='0';
+//         if(isvalid(i+1,j,n,m,grid))
+//             countIslands(i+1,j,n,m,grid);
+//         if(isvalid(i-1,j,n,m,grid))
+//             countIslands(i-1,j,n,m,grid);
+//         if(isvalid(i,j+1,n,m,grid))
+//             countIslands(i,j+1,n,m,grid);
+//         if(isvalid(i,j-1,n,m,grid))
+//             countIslands(i,j-1,n,m,grid);
+//     }
+//     int numIslands(vector<vector<char>>& grid) {
+//         int n=grid.size();
+//         int m=grid[0].size();
+//         int ans=0;
+//         for(int i=0;i<n;i++){
+//             for(int j=0;j<m;j++){
+//                 if(grid[i][j]=='1'){
+//                     ans++;
+//                     countIslands(i,j,n,m,grid);
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
 // };
+//bfs
+//T: O(row * col)
+//S: O(col)
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        if(grid.empty())
+            return 0;
+        int ctr = 0;
+        int m=grid.size();
+        int n=grid[0].size();
+        for(int i = 0; i < m; ++i) {
+            for(int j = 0; j <n; ++j) {
+                if(grid[i][j] == '1') {//land
+                    ctr++;
+                    queue<pair<int, int>> q;//<row, col>
+                    grid[i][j] = '2';//mark as vis
+                    q.push({i,j});
+                    while(!q.empty()) {
+                        pair<int, int> vis_cell = q.front();
+                        q.pop();
+                        for(auto d : dir) {
+                            int nr = vis_cell.first + d.first;
+                            int nc = vis_cell.second + d.second;
+                            if(nr >= 0 && nr < m && nc >= 0 && nc <n && grid[nr][nc] == '1') {//if valid cell and land
+                                grid[nr][nc] = '2';//mark as vis
+                                q.push({nr,nc});
+                            }
+                        }
+                    }
+                }   
+            }  
+        }
+        return ctr;
+    }
+private:
+    vector<pair<int, int>> dir{{1, 0}, {-1, 0}, {0, 1}, {0, -1}};//<row, col>
+};
 
 // class Solution {
 // public:
@@ -80,26 +82,26 @@ public:
 //     int numIslands(vector<vector<char>>& grid) {
 //         if(grid.empty())
 //             return 0;
-//         int island_ctr = 0;
+//         int ctr = 0;
 //         for(int m = 0; m < grid.size(); ++m) {
 //             for(int n = 0; n < grid[0].size(); ++n) {
 //                 if(grid[m][n] == '1') {
-//                     ++island_ctr;
+//                     ++ctr;
 //                     dfs(grid, m, n);//use dfs to mark every grid = 2 which belongs to this island.
 //                 }   
 //             }  
 //         }
-//         return island_ctr;
+//         return ctr;
 //     }
 // private:
 //     //dfs to mark every grid in this island.
 //     int dfs(vector<vector<char>>& grid, int m, int n) {
 //         grid[m][n] = '2';
 //         for(auto d : dir) {
-//             int next_row = m + d.first;
-//             int next_col = n + d.second;
-//             if(next_row >= 0 && next_row < grid.size() && next_col >= 0 && next_col < grid[0].size() && grid[next_row][next_col] == '1') {
-//                 dfs(grid, next_row, next_col);
+//             int nr = m + d.first;
+//             int nc = n + d.second;
+//             if(nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && grid[nr][nc] == '1') {
+//                 dfs(grid, nr, nc);
 //             }   
 //         }
 //         return 0;
