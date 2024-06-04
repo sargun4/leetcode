7ca7140721c85aa1fc1dd15fd6e5d8e9ba7e6262
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-04 17:42:57 +0530

    Time: 105 ms (70.11%) | Memory: 16.5 MB (99.30%) - LeetSync

--- 695-max-area-of-island/max-area-of-island.py
+++ 695-max-area-of-island/max-area-of-island.py
@@ -1,17 +1,17 @@
-class Solution:
-    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        visit=set()
-        def dfs(r,c):
-            if(r<0 or r==m or c<0 or c==n or grid[r][c]==0 or (r,c) in visit):
-                return 0
-            visit.add((r,c))
-            return (1+ dfs(r+1,c)+dfs(r-1,c)+dfs(r,c+1)+dfs(r,c-1))
-        area=0
-        for r in range(m):
-            for c in range(n):
-                area=max(area,dfs(r,c))
-        return area
+# class Solution:
+#     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+#         m, n = len(grid), len(grid[0])
+#         visit=set()
+#         def dfs(r,c):
+#             if(r<0 or r==m or c<0 or c==n or grid[r][c]==0 or (r,c) in visit):
+#                 return 0
+#             visit.add((r,c))
+#             return (1+ dfs(r+1,c)+dfs(r-1,c)+dfs(r,c+1)+dfs(r,c-1))
+#         area=0
+#         for r in range(m):
+#             for c in range(n):
+#                 area=max(area,dfs(r,c))
+#         return area
 # Time:     O(n * m),     Iterates through grid once.
 # Space:    O(n * m),     Uses set to keep track of visited indices.
 # # dfs
@@ -37,27 +37,26 @@ class Solution:
 
 #     return max_area
 
-# # bfs
-# class Solution:
-# def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-#     row, col = len(grid),len(grid[0])
-#     queue, max_area = deque([]), 0
-	
-#     def bfs(queue):
-#         cur_area = 1
-#         while queue:
-#             x,y = queue.popleft()
-#             for nx, ny in [[x+1,y], [x-1,y], [x,y+1], [x,y-1]]:
-#                 if 0<=nx<row and 0<=ny<col and grid[nx][ny]==1:
-#                     grid[nx][ny]=2
-#                     cur_area+=1
-#                     queue.append((nx,ny))
-#         return cur_area
-    
-#     for x in range(row):
-#         for y in range(col):
-#             if grid[x][y]==1:
-#                 grid[x][y]=2
-#                 queue.append((x,y))
-#                 max_area = max(max_area,bfs(queue))
-#     return max_area
+# bfs
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        row, col = len(grid),len(grid[0])
+        q, max_area = deque([]), 0
+        def bfs(q):
+            cur_area = 1
+            while q:
+                x,y = q.popleft()
+                for nx, ny in [[x+1,y], [x-1,y], [x,y+1], [x,y-1]]:
+                    if 0<=nx<row and 0<=ny<col and grid[nx][ny]==1:
+                        grid[nx][ny]=2
+                        cur_area+=1
+                        q.append((nx,ny))
+            return cur_area
+        
+        for x in range(row):
+            for y in range(col):
+                if grid[x][y]==1:
+                    grid[x][y]=2
+                    q.append((x,y))
+                    max_area = max(max_area,bfs(q))
+        return max_area
