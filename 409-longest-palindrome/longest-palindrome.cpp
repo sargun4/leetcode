513cb38b0cbfb20b8bc2d180a83cd671a2a7de5c
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-04 17:16:33 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (26.04%) - LeetSync

--- 409-longest-palindrome/longest-palindrome.cpp
+++ 409-longest-palindrome/longest-palindrome.cpp
@@ -2,19 +2,40 @@ class Solution {
 public:
     int longestPalindrome(string s) {
         int n=s.length();
-        unordered_set<char> set;
+        unordered_map<char,int> map;
         int res=0;
+        int oddfreq=0;
         for(char &ch:s){
-            if(set.count(ch)){//a;lready present
-                res+=2;
-                set.erase(ch);
+            map[ch]++;
+            if(map[ch]%2!=0){
+                oddfreq++;
             }else{
-                set.insert(ch);
+                oddfreq--;
             }
         }
-        if(!set.empty()){
-            res++;
+        if(oddfreq>0){
+            return n-oddfreq+1;
         }
-        return res;
+        return n;
     }
 };
+// class Solution {
+// public:
+//     int longestPalindrome(string s) {
+//         int n=s.length();
+//         unordered_set<char> set;
+//         int res=0;
+//         for(char &ch:s){
+//             if(set.count(ch)){//a;lready present
+//                 res+=2;
+//                 set.erase(ch);
+//             }else{
+//                 set.insert(ch);
+//             }
+//         }
+//         if(!set.empty()){
+//             res++;
+//         }
+//         return res;
+//     }
+// };
