1ec02d9f84d290eb7a28d3f9d5ed4910d6981c11
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-11 21:05:39 +0530

    Time: 7 ms (94.20%) | Memory: 22.6 MB (58.60%) - LeetSync

---
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.cpp
@@ -0,0 +1,17 @@
+
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k){
+        int ctr = countNodes(root->left);
+        if (k <= ctr) {
+            return kthSmallest(root->left, k);
+        } else if (k > ctr + 1) {
+            return kthSmallest(root->right, k-1-ctr); // 1 is counted as current node
+        }
+        return root->val;
+    }
+    int countNodes(TreeNode* root) {
+        if (root == NULL) return 0;
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};
