6cb09aa73c57f798f9de76789588f7eddbe0b417
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-17 21:25:38 +0530

    Time: 11 ms (93.24%) | Memory: 17.2 MB (26.91%) - LeetSync

---
+++ 803-cheapest-flights-within-k-stops/cheapest-flights-within-k-stops.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+        vector<vector<pair<int,int>>> adj(n); //adj list
+        for(auto it : flights) {
+            adj[it[0]].push_back({it[1], it[2]}); // it[0]=src node, it[1]=dest node, it[2]=cost
+        }
+
+        vector<int> price(n, INT_MAX);  //vector to store min price to reach each node from src
+        price[src] = 0; // The price to reach the src node from itself is 0
+    
+        queue<pair<int, pair<int, int>>> q; //q= {no. of stops, {curr node, curr cost}}
+        q.push({0, {src, 0}}); // start with the src node, 0 stops, and cost 0
+
+        while(!q.empty()) {
+            auto p = q.front();
+            int stop = p.first; // no of stops made to reach the curr node
+            int node = p.second.first; // curr node
+            int cost = p.second.second; // cost to reach the curr node
+            q.pop();
+
+            // if the no. of stops exceeds k, skip processing further
+            if(stop > k) {
+                continue;
+            }
+
+            for(auto it : adj[node]) {
+                // if  no. of stops is within the limit and the new cost is less than the prev recorded cost
+                if((stop <= k) && (cost + it.second)<price[it.first]) {
+                    price[it.first] = cost + it.second; //update cost to reach adj node
+                    q.push({stop + 1, {it.first, cost + it.second}}); // push adj node into q with updated cost and stops
+                }
+            }
+        }
+
+        // if price to reach dest node is still INT_MAX, it means it's unreachable
+        if(price[dst] == INT_MAX) return -1;
+        
+        return price[dst];//min price to reach the dest node
+    }
+};
