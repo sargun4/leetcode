a1ba543692f7f6421d8a45deddc91d3e4a26d265
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-18 00:00:56 +0530

    Time: 37 ms (47.71%) | Memory: 40.4 MB (18.87%) - LeetSync

--- 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/sum-of-nodes-with-even-valued-grandparent.cpp
@@ -10,17 +10,19 @@ public:
             for(int i=0;i<n;i++){
                 TreeNode* temp=q.front();
                 q.pop(); 
-                if(temp!=NULL && (temp->val%2)==0 && temp->left!=NULL && temp->left->left!=NULL){
-                    sum=sum+temp->left->left->val;	
-                } 	
-                if(temp!=NULL && (temp->val%2)==0 && temp->left!=NULL && temp->left->right!=NULL){
-                    sum=sum+temp->left->right->val;
-                }
-                if(temp!=NULL && (temp->val%2)==0 && temp->right!=NULL && temp->right->left!=NULL){
-                    sum=sum+temp->right->left->val;
-                }
-                if(temp!=NULL && (temp->val%2)==0 && temp->right!=NULL && temp->right->right!=NULL){
-                    sum=sum+temp->right->right->val;
+                if(temp!=NULL && (temp->val%2)==0){
+                    if(temp->left!=NULL && temp->left->left!=NULL){
+                        sum=sum+temp->left->left->val;	
+                    } 	
+                    if(temp->left!=NULL && temp->left->right!=NULL){
+                        sum=sum+temp->left->right->val;
+                    }
+                    if(temp->right!=NULL && temp->right->left!=NULL){
+                        sum=sum+temp->right->left->val;
+                    }
+                    if(temp->right!=NULL && temp->right->right!=NULL){
+                        sum=sum+temp->right->right->val;
+                    }
                 }
                 if(temp->left!=NULL){
                     q.push(temp->left);
