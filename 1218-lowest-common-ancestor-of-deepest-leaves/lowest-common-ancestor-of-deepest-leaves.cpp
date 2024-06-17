50584425796072b20bedf9547c8efc858d52c8d9
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-17 21:53:18 +0530

    Time: 0 ms (100.00%) | Memory: 21.1 MB (19.58%) - LeetSync

--- 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
+++ 1218-lowest-common-ancestor-of-deepest-leaves/lowest-common-ancestor-of-deepest-leaves.cpp
@@ -1,36 +1,21 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
-class Solution {
+ class Solution {
 public:
 TreeNode* lca(TreeNode* root, TreeNode* p, TreeNode* q) {
-        // If the current node is NULL, return NULL
+        // If the curr node is NULL, return NULL
         if (root == NULL) {
             return NULL;
         }
-
-        // If the current node is p or q, return the current node
+        // If the curr node is p or q, return the curr node
         if (root == p || root == q) {
             return root;
         }
-
         // Recur for the left and right subtrees
         TreeNode* left = lca(root->left, p, q);
         TreeNode* right = lca(root->right, p, q);
-
-        // If both left and right are non-NULL, the current node is the LCA
+        // If both left and right are non-NULL, the curr node is the LCA
         if (left != NULL && right != NULL) {
             return root;
         }
-
         // If only one of them is non-NULL, return the non-NULL node
         return left != NULL ? left : right;
     }
