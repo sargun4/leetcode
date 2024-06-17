94055045a2aebe78fdcaf146f99c1e3bd5d128e4
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-17 20:08:02 +0530

    Time: 13 ms (52.68%) | Memory: 14.4 MB (15.04%) - LeetSync

--- 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
+++ 774-maximum-depth-of-n-ary-tree/maximum-depth-of-n-ary-tree.cpp
@@ -17,13 +17,36 @@ public:
     }
 };
 */
-//dfs
 class Solution {
 public:
     int maxDepth(Node* root) {
-        if (root == nullptr) return 0;
-        int depth = 0;
-        for (auto child : root->children) depth = max(depth, maxDepth(child));
-        return 1 + depth;
+        if(root == NULL) return 0;
+        queue<Node*> q;
+        q.push(root);
+        int depth=0;
+        while (!q.empty()) {
+            depth+=1;
+            int n=q.size();
+            for(int i=0;i<n;i++) {
+                auto node=q.front();
+                q.pop();
+                for(auto child:node->children){
+                    if(child){
+                        q.push(child);
+                    }
+                }
+            }
+        }
+        return depth;
     }
 };
+// //dfs
+// class Solution {
+// public:
+//     int maxDepth(Node* root) {
+//         if (root == NULL) return 0;
+//         int depth = 0;
+//         for (auto child : root->children) depth = max(depth, maxDepth(child));
+//         return 1 + depth;
+//     }
+// };
