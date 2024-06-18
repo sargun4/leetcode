856646c17c4fcd7a612c0d9bde4bc540b946f74e
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-18 06:38:28 +0530

    Time: 216 ms (5.03%) | Memory: 436.6 MB (5.84%) - LeetSync

---
+++ 654-maximum-binary-tree/maximum-binary-tree.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int findMax(vector<int>nums,int s,int e){
+        int maxi=s;
+        for(int i=s;i<=e;i++){
+            if(nums[i]>nums[maxi])
+            maxi=i;
+        }
+        return maxi;
+    }
+    TreeNode* makeTree(vector<int>nums,int start,int end){
+        if(start>end)return NULL;
+        int idx=findMax(nums,start,end);
+        TreeNode* root=new TreeNode(nums[idx]);
+        root->left=makeTree(nums,start,idx-1);
+        root->right=makeTree(nums,idx+1,end);
+        return root;
+    }
+    TreeNode* constructMaximumBinaryTree(vector<int>& nums) {
+        return makeTree(nums,0,nums.size()-1);
+    }
+};
