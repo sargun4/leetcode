cc08bc3db7731a88662ec653f82fb9ec8d081368
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-19 22:19:50 +0530

    Time: 305 ms (45.27%) | Memory: 163.8 MB (46.29%) - LeetSync

---
+++ 2558-minimum-number-of-operations-to-sort-a-binary-tree-by-level/minimum-number-of-operations-to-sort-a-binary-tree-by-level.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumOperations(TreeNode* root) {
+        int ans = 0;
+        if (!root) return 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        while (!q.empty()) {
+            int len = q.size();
+            vector<int> v;
+            while (len--) {
+                root = q.front();
+                q.pop();
+                v.push_back(root->val);
+                if(root->left)
+                    q.push(root->left);
+                if(root->right) q.push(root->right);
+            }
+            unordered_map<int, int> map;
+            for (int i=0; i<v.size(); i++)
+                map[v[i]] = i;
+            sort(v.begin(), v.end());
+            int i=0;
+            while (i < v.size()) {
+                if (map[v[i]] != i) {
+                    swap(v[i], v[map[v[i]]]);
+                    ans++;
+                }
+                else i++;
+            }
+        }
+        return ans;
+    }
+};
