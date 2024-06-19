a2d5e41c3cae6f5d888ca5efe3d063ef2545d167
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-06-19 17:10:46 +0530

    Time: 15 ms (27.06%) | Memory: 22.7 MB (7.88%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -1,44 +1,46 @@
-class Solution {
-public:
-    void helper(TreeNode*root, int low, int high) {
-        if(root==NULL) return;
-        while(root->left!=NULL){
-            if(root->left->val<low) root->left=root->left->right;
-            else if(root->left->val>high) root->left=root->left->left;
-            else break;
-        }
-        while(root->right!=NULL){
-            if(root->right->val>high) root->right=root->right->left;
-            else if(root->right->val<low) root->right=root->right->right;
-            else break;
-        }
-        helper(root->left,low,high);
-        helper(root->right,low,high);
-    }
-    TreeNode*trimBST(TreeNode*root, int low, int high) {
-        TreeNode* parent=new TreeNode(INT_MAX);
-        parent->left=root;
-        helper(parent,low,high);
-        return parent->left;
-    }
-};
 // class Solution {
+// public:
+//     void helper(TreeNode*root, int low, int high) {
+//         if(root==NULL) return;
+//         while(root->left!=NULL){
+//             if(root->left->val<low) root->left=root->left->right;
+//             else if(root->left->val>high) root->left=root->left->left;
+//             else break;
+//         }
+//         while(root->right!=NULL){
+//             if(root->right->val>high) root->right=root->right->left;
+//             else if(root->right->val<low) root->right=root->right->right;
+//             else break;
+//         }
+//         helper(root->left,low,high);
+//         helper(root->right,low,high);
+//     }
 //     TreeNode*trimBST(TreeNode*root, int low, int high) {
-//         if (root == NULL) return NULL;
-//         // If the current node's value is less than low,
-//         // then we need to trim its left subtree and return the trimmed right subtree->
-//         if (root->val < low) {
-//             return trimBST(root->right, low, high);
-//         }
-//         // If the current node's value is greater than high,
-//         // then we need to trim its right subtree and return the trimmed left subtree->
-//         if (root->val > high) {
-//             return trimBST(root->left, low, high);
-//         }
-//         // If the current node's value is within the range [low, high],        // then we need to recursively trim both its left and right subtrees->
-//         root->left = trimBST(root->left, low, high);
-//         root->right = trimBST(root->right, low, high);
-//         // Return the updated root after the trimming process->
-//         return root;
+//         TreeNode* parent=new TreeNode(INT_MAX);
+//         parent->left=root;
+//         helper(parent,low,high);
+//         return parent->left;
 //     }
 // };
+class Solution {
+public:
+    TreeNode* trimBST(TreeNode* root, int low, int high) {
+        if(root == NULL) return NULL;
+        // If the curr node's value is less than low,
+        // then we need to trim its left subtree and return the trimmed right subtree->
+        if (root->val < low) {
+            return trimBST(root->right, low, high);
+        }
+        // If the curr node's value is greater than high,
+        // then we need to trim its right subtree and return the trimmed left subtree->
+        if (root->val > high) {
+            return trimBST(root->left, low, high);
+        }
+        // If the curr node's value is within the range [low, high],
+         // then we need to recursively trim both its left and right subtrees->
+        root->left=trimBST(root->left, low, high);
+        root->right=trimBST(root->right, low, high);
+        // Return the updated root after the trimming process->
+        return root;
+    }
+};
