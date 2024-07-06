f1f786046cc9bf7c889eb769bd0467bca1692731
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-07-06 21:04:06 +0530

    Time: 48 ms (41.77%) | Memory: 28.6 MB (73.86%) - LeetSync

--- 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/count-square-submatrices-with-all-ones.cpp
@@ -11,8 +11,10 @@ public:
         for(int i=0;i<n;i++) dp[i][0]=matrix[i][0];
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
-                if(matrix[i][j]==0) dp[i][j]=0;
-                else dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                if(matrix[i][j]==0)
+                    dp[i][j]=0;
+                else if(matrix[i][j]==1)
+                    dp[i][j]=1+min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
             }
         }
         int sum=0;
