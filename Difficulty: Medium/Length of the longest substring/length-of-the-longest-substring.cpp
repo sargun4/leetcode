4d1bd3b8eb09843885bf7f47b1bf972037ab73a5
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-07-09 23:32:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Length of the longest substring/length-of-the-longest-substring.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+    int longestUniqueSubsttr(string S){
+      int maxi=INT_MIN;
+      int i=0,j=0;
+      int n=S.size();
+      int arr[26]={0};
+       while(j<n){
+          arr[S[j]-'a']++;
+          while(i<n && arr[S[j]-'a']!=1){
+              arr[S[i]-'a']--;
+              i++;
+          }
+          maxi=max(maxi,j-i+1);
+          j++;
+       }
+       return maxi;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string str;
+		cin>>str;
+		Solution ob;
+		cout<<ob.longestUniqueSubsttr(str)<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
