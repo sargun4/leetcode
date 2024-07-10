e27ecee0f68975e1befd527eb5c8aabd9f859621
sargun4 <132887555+sargun4@users.noreply.github.com>
2024-07-10 15:28:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count pairs with given sum/count-pairs-with-given-sum.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+// Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    int getPairsCount(const vector<int>& arr, int k) {
+        int n=arr.size();
+         unordered_map<int ,int>m;
+         int ans=0;
+         for(int i=0;i<n;i++){
+             int val= k- arr[i];
+             if(m[val]){
+                 ans+=m[val];
+             }
+             m[arr[i]]++;
+         }
+         return ans;
+    }  
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline character after t
+    while (t--) {
+        vector<int> arr;
+        int k;
+
+        cin >> k;
+        cin.ignore(); // Ignore the newline character after k
+
+        string input;
+
+        getline(cin, input); // Read the entire line for the array elements
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+
+        Solution ob;
+        auto ans = ob.getPairsCount(arr, k);
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
+// } Driver Code Ends
