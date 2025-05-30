d26eb5afe7ec3fcc43f9bfdfc8a35a7dd6de4e4a
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-05-31 02:22:40 +0530

    Time: 10 ms (86.50%) | Memory: 105.4 MB (42.44%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,3 +1,45 @@
+// class Solution {
+// public:
+//     int n;
+//     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
+//         n=edges.size();
+//         vector<int> dist1(n,INT_MAX);
+//         vector<int> dist2(n,INT_MAX);
+
+//         vector<bool> vis1(n,false);
+//         vector<bool> vis2(n,false);
+
+//         dist1[node1]=0;
+//         dist2[node2]=0;
+
+//         dfs(edges,node1,dist1,vis1);
+//         dfs(edges,node2,dist2,vis2);
+
+//         int mindistnode=-1;
+//         int mindistTillNow=INT_MAX;
+
+//         for(int i=0;i<n;i++){
+//             int maxD=max(dist1[i],dist2[i]);
+//             if(mindistTillNow > maxD){
+//                 mindistTillNow=maxD;
+//                 mindistnode=i;
+//             }
+//         }
+//         return mindistnode;
+//     }
+//     void dfs(vector<int>& edges, int node,vector<int> &dist,vector<bool> &vis){
+//         vis[node]=true;
+        
+//         int v=edges[node];
+//         if(v!=-1 && !vis[v]){
+//             vis[v]=true;
+//             dist[v]=1+dist[node];
+//             dfs(edges,v,dist,vis);
+//         }
+//     }
+// };
+
+
 class Solution {
 public:
     int n;
@@ -5,15 +47,9 @@ public:
         n=edges.size();
         vector<int> dist1(n,INT_MAX);
         vector<int> dist2(n,INT_MAX);
-
-        vector<bool> vis1(n,false);
-        vector<bool> vis2(n,false);
-
-        dist1[node1]=0;
-        dist2[node2]=0;
-
-        dfs(edges,node1,dist1,vis1);
-        dfs(edges,node2,dist2,vis2);
+         
+        bfs(edges,node1,dist1);
+        bfs(edges,node2,dist2);
 
         int mindistnode=-1;
         int mindistTillNow=INT_MAX;
@@ -27,14 +63,23 @@ public:
         }
         return mindistnode;
     }
-    void dfs(vector<int>& edges, int node,vector<int> &dist,vector<bool> &vis){
+    void bfs(vector<int>& edges, int node,vector<int> &dist){
+        queue<int> q;
+        dist[node]=0;
+        vector<bool> vis(n,false);
+        q.push(node);
         vis[node]=true;
-        
-        int v=edges[node];
-        if(v!=-1 && !vis[v]){
-            vis[v]=true;
-            dist[v]=1+dist[node];
-            dfs(edges,v,dist,vis);
+        while(!q.empty()){
+            int u=q.front();
+            q.pop();
+
+            int v=edges[u];
+            if(v!=-1 && !vis[v]){
+                vis[v]=true;
+                dist[v]=1+dist[u];
+                q.push(v);
+            }
         }
+
     }
 };
