9d58186105564326500342106df1f1d29101d8df
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-18 12:59:56 +0530

    Time: 48 ms (60.53%) | Memory: 120.1 MB (64.59%) - LeetSync

--- 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
+++ 3241-divide-array-into-arrays-with-max-difference/divide-array-into-arrays-with-max-difference.cpp
@@ -4,9 +4,9 @@ public:
         int n=nums.size();
         vector<vector<int>> res;
         sort(begin(nums),end(nums));
-        for(int i=0;i<n;i+=3){
-            if(nums[i+2]-nums[i] > k){
-                return {};
+        for(int i=0;i<n;i+=3){//iterate it w step 3
+            if(nums[i+2]-nums[i]>k){//if the diff bw the largest and the smallest elmnt is more than k
+                return {};//not possible
             }
             res.push_back({nums[i],nums[i+1],nums[i+2]});
         }
