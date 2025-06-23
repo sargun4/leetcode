2c3850d14d7b39d4425e80c0c29a593e2a82c326
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-23 15:35:07 +0530

    Time: 2867 ms (6.14%) | Memory: 88.7 MB (27.19%) - LeetSync

--- 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
+++ 2202-sum-of-k-mirror-numbers/sum-of-k-mirror-numbers.cpp
@@ -11,21 +11,24 @@ public:
         }
         return true;
     }
+    //convet num to base k
     string convertToBaseK(long long num,int k){
         if(num==0) return "0";
         string res="";
         while(num>0){
-            res+=to_string(num%k);//convert num to base k
-            num=num/k;
+            res+=to_string(num%k);//add curr digit
+            num=num/k;//move to next digit
         }
         return res;
     }
     long long kMirror(int k, int n) {
-        long long sum=0;
-        int L=1;//creat L length palindromes
-        while(n>0){
+        long long sum=0;//sum of k-mirror numbers found so far
+        int L=1;//creat L length palindromes-start checkin w len=1
+        while(n>0){//Keep searching until we've found 'n' k-mirror numbers
             //get palindromes
-            int half_len=(L+1)/2;
+            int half_len=(L+1)/2;//Half len needed to generate L-digit palindrome
+            //generate the min n max nos for the 1st half;
+            //eg-for L=3 -> half_len=2, min_num=10, max_num=99
             long long min_num=pow(10,half_len-1);
             long long max_num=pow(10,half_len)-1;
             for(int num=min_num;num<=max_num;num++){
@@ -39,7 +42,7 @@ public:
                 }else{//odd
                     palind=first_half+second_half.substr(1);//leave 1st char of 2nd half
                 }
-                long long palind_num=stol(palind);
+                long long palind_num=stoll(palind);//Convert the string to long long
                 string baseK=convertToBaseK(palind_num,k);
                 if(isPalindrome(baseK)){
                     sum+=palind_num;//found 1 k-mirror num
