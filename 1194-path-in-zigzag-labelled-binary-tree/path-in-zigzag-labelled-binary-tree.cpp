60e71582592a96b58f86b3b9f05140c80a3854ec
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-25 15:35:55 +0530

    Time: 67 ms (4.29%) | Memory: 126.7 MB (5.15%) - LeetSync

--- 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.cpp
+++ 1194-path-in-zigzag-labelled-binary-tree/path-in-zigzag-labelled-binary-tree.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<int> pathInZigZagTree(int label) {
-        vector<vector<int>> tree;//Each row contains values at that lvl
+        vector<vector<int>> tree;//each row contains vals at that lvl
         //[[1],[3,2],[4,5,6,7]]
         int val=1;
         bool rev = false;//is reversed flag
@@ -13,13 +13,13 @@ public:
                 lvl.push_back(val++);
             }
             if(rev)
-                reverse(lvl.begin(), lvl.end());//reverse alt
+                reverse(lvl.begin(), lvl.end());
             tree.push_back(lvl);
-            rev=!rev;
+            rev=!rev;//reverse alt
         }
         //find the path from root to label
-        vector<int> path;
-        int lvl=tree.size()-1;
+        vector<int> path;//ans
+        int lvl=tree.size()-1;//no of rows-1
         //start from label, go upward to root
         while (lvl >= 0) {
             path.push_back(label);
@@ -29,11 +29,11 @@ public:
             //move to parent(idx-wise, parent of idx=idx/2)
             lvl--;
             if (lvl >= 0) {
-                int parentIdx = idx / 2;
-                label = tree[lvl][parentIdx];
+                int parentIdx=idx/2;
+                label=tree[lvl][parentIdx];//label found
             }
         }
-        reverse(path.begin(), path.end());
+        reverse(path.begin(), path.end());//rev the l
         return path;
     }
 };
