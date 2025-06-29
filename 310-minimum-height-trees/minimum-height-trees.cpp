8d2aa50a4f21d91aa1af5941e968317fb958f441
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-29 13:30:24 +0530

    Time: 51 ms (47.64%) | Memory: 64.1 MB (26.58%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,12 +1,13 @@
 #include<bits/stdc++.h>
 using namespace std;
-
+//We perform a layer-by-layer BFS from leaves, removing them repeatedly until we reach the last 1 or 2 nodes, which are the centroids(middlemost ndoes) of the tree and will produce Minimum Height Trees
+//Removing all leaves layer by layer eventually will leave the centroids-mhts
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
         unordered_map<int,vector<int>> adj;//undir graph
         vector<int>indeg(n);
-        if(n==1)return {0};
+        if(n==1)return {0};//base-if theres only 1 node,its the root of MHT
         for(auto &edge:edges){ 
             int u=edge[0];
             int v=edge[1];
@@ -15,27 +16,30 @@ public:
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        vector<int> res;
-        queue<int> q;
+        queue<int> q;//queue storing leaf nodes intially
         for(int i=0;i<n;i++){
             if(indeg[i]==1){//leaf nodes
                 q.push(i);
             }
         }
+        //each itern removes curr layer of leaves
+        //stop when <=2 nodes are left- centroids
         while(n>2){
             int size=q.size();
-            n-=size;
+            n-=size;//removing size nodes frm graph
             while(size--){
                 int u=q.front();
                 q.pop();
                 for(int &v:adj[u]){
-                    indeg[v]--;
-                    if(indeg[v]==1){
+                    indeg[v]--;//remove edge u-v
+                    if(indeg[v]==1){//if v becomes leaf
                         q.push(v);
                     }
                 }
             }
         }
+        //remaing 1 or 2 nodes are MHT roots
+        vector<int> res;
         while(!q.empty()){
             res.push_back(q.front());
             q.pop();
