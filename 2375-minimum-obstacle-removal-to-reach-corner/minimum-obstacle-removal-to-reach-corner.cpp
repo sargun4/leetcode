1e1549301c72a24b618080a99714f71df10bb6db
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-29 17:28:45 +0530

    Time: 244 ms (56.29%) | Memory: 108.8 MB (74.66%) - LeetSync

---
+++ 2375-minimum-obstacle-removal-to-reach-corner/minimum-obstacle-removal-to-reach-corner.cpp
@@ -0,0 +1,39 @@
+#define pi pair<int,int>
+class Solution {
+public:
+    bool isValid(int i,int j,int m,int n){
+        return i>=0 && j>=0 && i<m &&j<n;
+    }
+    vector<pi> dirns={{1,0},{-1,0},{0,1},{0,-1}};
+    int minimumObstacles(vector<vector<int>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        //dist matrix to keep min obstacle count to reach each cell
+        vector<vector<int>> dist(m,vector<int>(n,INT_MAX));
+        //minheap-//{min_obstacles_so_far,{i,j}}
+        priority_queue<pair<int,pi>,vector<pair<int,pi>>,greater<pair<int,pi>>> pq;
+        pq.push({0,{0,0}});//top-left (0,0) with 0 obstacles removed
+        //dijkstra-0 or 1
+        dist[0][0]=0;
+        while(!pq.empty()){
+            auto curr=pq.top(); pq.pop();
+            int mindist=curr.first;
+            int x=curr.second.first;
+            int y=curr.second.second;
+            for(auto d:dirns){
+                int nx=x+d.first;
+                int ny=y+d.second;
+                if(isValid(nx,ny,m,n)){
+            //cost to move: grid[nx][ny] is 0 (free) or 1 (obstacle to remove)
+                    int newdist=mindist+grid[nx][ny];
+                    if(newdist<dist[nx][ny]){
+                        dist[nx][ny]=newdist;
+                        pq.push({newdist,{nx,ny}});
+                    }
+                }
+            }
+        }
+        //minimum obstacles to reach bottom-right cell
+        return dist[m-1][n-1];
+    }
+};
