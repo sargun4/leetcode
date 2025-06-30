58c8b7fa4b3c1e94bf9b0714d16260e8e1d5abf9
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-06-30 22:46:15 +0530

    Time: 11 ms (17.45%) | Memory: 20.2 MB (16.73%) - LeetSync

--- 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.cpp
@@ -7,26 +7,28 @@ vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& red_edges, vect
             adj[e[0]].emplace_back(e[1],0);//red edges=0 
         for(auto e: blue_edges)
             adj[e[0]].emplace_back(e[1],1);//blue edges=1
-        vector<int> dist(n,-1); 
+        
+        vector<int> dist(n,-1);//minimum dist to each node from node 0, using alternating colors.
         
         queue<vector<int>> q;//{idx,dist,colour}
-        q.emplace(vector<int>{0,0,-1});
+        q.emplace(vector<int>{0,0,-1});//-1 color-no prev edge yet-so either red or blue for 1st move
         
-        while(!q.empty()) {
+        while(!q.empty()){
             auto curr = q.front();
             int idx=curr[0];
             int mindist=curr[1];
             int color=curr[2];
             q.pop();
+
             dist[idx]= (dist[idx]!=-1) ? dist[idx] : mindist;
             
-            for(auto &adj : adj[idx]) {
+            for(auto &neigh : adj[idx]) {
 				//Push the node to the queue only if the next edge color is different from the pervious edge color and also if we are visiting the edge
 				//for the first time.
-                if(color!=adj.second && adj.first!=-1) {
-                    q.emplace(vector<int>{adj.first, mindist + 1, adj.second});
+                if(color!=neigh.second && neigh.first!=-1) {
+                    q.emplace(vector<int>{neigh.first, mindist + 1, neigh.second});
 					//Update the value in the adjacency matrix to -1 to denote that the node has already been visited.
-                    adj.first = -1;
+                    neigh.first = -1;
                 }
             }
         }
