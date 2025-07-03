30e40d82ea30ee78415ee3c707164b9e1520bee4
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-03 23:48:20 +0530

    Time: 16 ms (96.07%) | Memory: 21.1 MB (54.46%) - LeetSync

--- 690-employee-importance/employee-importance.cpp
+++ 690-employee-importance/employee-importance.cpp
@@ -7,47 +7,48 @@ public:
     vector<int> subordinates;
 };
 */
-// class Solution {
-// public:
-//     //rec to calc total imp of a given employee
-//     int dfs(int id, unordered_map<int, Employee*>& empMap) {
-//         Employee* emp=empMap[id];
-//         int imp=emp->importance;//own val
-//         for (int subempId:emp->subordinates){//visit all its subords(subtree)
-//             imp+=dfs(subempId,empMap); //add imp of subordinates
-//         }
-//         return imp;
-//     }
-//     int getImportance(vector<Employee*> employees, int id) {
-//         unordered_map<int, Employee*> empMap;//{id:emp}
-//         for(Employee* emp:employees){
-//             empMap[emp->id] = emp;
-//         }
-//         return dfs(id,empMap);
-//     }
-// };
-
+//dfs
 class Solution {
 public:
-    int getImportance(vector<Employee*> employees, int id) {
-        unordered_map<int, Employee*> empMap; //{id:emp}
-        for (Employee* emp:employees){
-            empMap[emp->id]=emp;
+    //rec to calc total imp of a given employee
+    int dfs(int id, unordered_map<int, Employee*>& empMap) {
+        Employee* emp=empMap[id];
+        int imp=emp->importance;//own val
+        for (int subempId:emp->subordinates){//visit all its subords(subtree)
+            imp+=dfs(subempId,empMap); //add imp of subordinates
         }
-        //BFS
-        int totalImp = 0;
-        queue<int> q;
-        q.push(id);//start frm given id
-        while (!q.empty()) {
-            int currId = q.front();
-            q.pop();
-            Employee* currEmp = empMap[currId];
-            totalImp += currEmp->importance;
-            //push all direct subordinates into q
-            for(int subId:currEmp->subordinates){
-                q.push(subId);
-            }
+        return imp;
+    }
+    int getImportance(vector<Employee*> employees, int id){
+        unordered_map<int, Employee*> empMap;//{id:emp}
+        for(Employee* emp:employees){
+            empMap[emp->id] = emp;
         }
-        return totalImp;
+        return dfs(id,empMap);
     }
 };
+// //bfs
+// class Solution {
+// public:
+//     int getImportance(vector<Employee*> employees, int id) {
+//         unordered_map<int, Employee*> empMap; //{id:emp}
+//         for (Employee* emp:employees){
+//             empMap[emp->id]=emp;
+//         }
+//         //BFS
+//         int totalImp = 0;
+//         queue<int> q;
+//         q.push(id);//start frm given id
+//         while (!q.empty()) {
+//             int currId = q.front();
+//             q.pop();
+//             Employee* currEmp = empMap[currId];
+//             totalImp += currEmp->importance;
+//             //push all direct subordinates into q
+//             for(int subId:currEmp->subordinates){
+//                 q.push(subId);
+//             }
+//         }
+//         return totalImp;
+//     }
+// };
