df89177f495f7133773acbc3ab33332b76ce9cab
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-03 18:50:30 +0530

    Time: 23 ms (95.62%) | Memory: 54.4 MB (35.36%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.cpp
@@ -4,8 +4,9 @@ public:
     int numSubseq(vector<int>& nums, int target) {
         int mod=1e9 + 7;
         int n=nums.size();
-        sort(nums.begin(), nums.end());
-//precompute powers of 2 upto n (cuz for k elements, 2^k subsequences exist)
+        //1.sort
+        sort(nums.begin(), nums.end());//nlogn
+        //2.precompute powers of 2 upto n (cuz for k elements, 2^k subsequences exist)
         vector<int> power(n, 1);
         for (int i = 1; i < n; ++i) {
             power[i] = (power[i - 1] * 2) % mod;
@@ -15,12 +16,12 @@ public:
         //2ptr
         while(l<=r) {
             int sum=nums[l]+nums[r];
-            if (sum<=target) {
+            if (sum<=target){
                 //Total combinations=2^(right-left)            
                 res=(res+power[r-l])%mod;
                 l++;//move left fwd
             }else{
-                //if sum > target, max element (nums[right]) is too large
+                //if sum > target, max element (nums[r]) is too large
                 r--;//reduce max to make sum smaller
             }
         }
