bde367caf28d8c62f150bcf5de087cf3c2ad052a
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-04 01:56:19 +0530

    Time: 439 ms (12.88%) | Memory: 334.7 MB (6.45%) - LeetSync

--- 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
+++ 2461-amount-of-time-for-binary-tree-to-be-infected/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -1,43 +1,46 @@
 class Solution {
 public:
+//build an undirected graph from the binary tree
     void makegraph(unordered_map<int,vector<int>> &adj,int parent,TreeNode* curr){
         if(curr==NULL) return;
-        if(parent!=-1){
-            adj[curr->val].push_back(parent);
+        if(parent!=-1){//if curr node has a parent,connect it in graph
+            adj[curr->val].push_back(parent);//store it in grpah
         }
-        if(curr->left){
+        if(curr->left){//l child store
             adj[curr->val].push_back(curr->left->val);
-        }if(curr->right){
+        }if(curr->right){//r child
             adj[curr->val].push_back(curr->right->val);
         }
         makegraph(adj,curr->val,curr->left);
         makegraph(adj,curr->val,curr->right);
     }
     int amountOfTime(TreeNode* root, int start) {
-        unordered_map<int,vector<int>> adj;
-        makegraph(adj,-1,root);
-        queue<int> q;
-        unordered_set<int> vis;
-        q.push(start);
-        vis.insert(start);
-        int min=0;
+        unordered_map<int,vector<int>> adj;//unord graph-adj list repr
+ // Step 1- Convert the tree to graph
+        makegraph(adj,-1,root);// -1=root has no parent
+        queue<int> q;//q for bfs
+        unordered_set<int> vis;//track infectd nodes
+        q.push(start);//start frm infected ndoe
+        vis.insert(start);//mark it 
+        int min=0;//time
         while(!q.empty()){
-            int n=q.size();
-            while(n--){
-                int curr=q.front();
+            int n=q.size();//no of infected nodes at curr lvl-this sec
+            while(n--){//goin over all nodes at curr lvl
+                int curr=q.front();//curr infected node
                 q.pop();
-                for(int &neighbor:adj[curr]){
+                for(int &neighbor:adj[curr]){//infect all unvis neigbors-child or parent
                     if(vis.find(neighbor)==vis.end()){
-                        q.push(neighbor);
-                        vis.insert(neighbor);
+                        q.push(neighbor);//infect neigh
+                        vis.insert(neighbor);//mark as vis
                     }
                 }
             }
-            min++;
+            min++;//1more sec/lvl passed
         }     
-        return min-1;
+        return min-1;//since last lvl doesnt cause new infections,min-1
     }
 };
+
 //  //BFS
 // class Solution {
 // public:
