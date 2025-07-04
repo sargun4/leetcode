57814322add6370fb76a0887a6e58c7839de8a73
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-04 18:37:35 +0530

    Time: 51 ms (15.03%) | Memory: 32.2 MB (58.77%) - LeetSync

--- 876-hand-of-straights/hand-of-straights.cpp
+++ 876-hand-of-straights/hand-of-straights.cpp
@@ -2,22 +2,27 @@ class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         int n=hand.size();
-        if(n%groupSize!=0){
+        if(n%groupSize!=0){//hand's size shd be a multiple of grpSize
             return false;
         }
-        map<int,int> m;
+        map<int,int> cardCtrMap;//{handno:freq};
         for(int &handno:hand){
-            m[handno]++;
+            cardCtrMap[handno]++;
         }
-        while(!m.empty()){
-            int curr=m.begin()->first;
+        
+        while(!cardCtrMap.empty()){
+            //smallest available card
+            int curr=cardCtrMap.begin()->first;
+            //form a grp starting from curr of length groupSize
             for(int i=0;i<groupSize;i++){
-                if(m[curr+i]==0){
-                    return false;
+                //if a card is missing or has exhausted its occurences
+                if(cardCtrMap[curr+i]==0){
+                    return false;//cant form valid grp
                 }
-                m[curr+i]--;
-                if(m[curr+i]<1){
-                    m.erase(curr+i);
+                cardCtrMap[curr+i]--;
+                if(cardCtrMap[curr+i]<1){
+                    //remove card val if its occurences r exhausted
+                    cardCtrMap.erase(curr+i);
                 }
             }
         }
