906bd209125da038575d86ec571418907003dec0
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-05 20:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (51.99%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,36 +1,66 @@
-// O(n) 
 class Solution {
 public:
+    typedef pair<int,int> pii;
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> map;
-        int topfreq = 0;
-        for(int x: nums) {
-            map[x]++;
-            topfreq = max(topfreq, map[x]);
+        for(int &num:nums){
+            map[num]++;
         }
-
-        vector<int> ans;
-
-        unordered_map<int, vector<int>> freqmap;
-        for(auto it: map) {
-            freqmap[it.second].push_back(it.first);
-        }
-
-        int ctr=0;
-        while(ctr<k) {
-            if(freqmap.find(topfreq) != freqmap.end()) {
-                vector<int> v = freqmap[topfreq];
-                int n = v.size();
-                for(int i=0; i<n; i++) {
-                    ans.push_back(v[i]);
-                    ctr++;
-                    if(ctr == k)
-                        break;
-                }
+        //minheap to get the min freq elmnts at top so they can b del if size of pq exceeds k
+        priority_queue<pii,vector<pii>,greater<>> pq;//{freq:elmnt}
+        //push elmnts in minheap
+        for(auto it:map){//O(n)
+            int elmnt=it.first; 
+            int freq=it.second;
+            pq.push({freq,elmnt});
+            if(pq.size()>k){//only top k elmnts needed-so pop when size exceeds k-O(logk)
+                pq.pop();
             }
-            topfreq--;
         }
-
-        return ans;
+        vector<int> res;
+        while(!pq.empty()){
+            int val=pq.top().second;
+            res.push_back(val);
+            pq.pop();
+        }
+        return res;
     }
 };
+
+
+// // O(n) 
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+//         unordered_map<int, int> map;
+//         int topfreq=0;
+//         for(int x: nums) {
+//             map[x]++;
+//             topfreq = max(topfreq, map[x]);
+//         }
+
+//         vector<int> ans;
+
+//         unordered_map<int, vector<int>> freqmap;
+//         for(auto it: map) {
+//             freqmap[it.second].push_back(it.first);
+//         }
+
+//         int ctr=0;
+//         while(ctr<k) {
+//             if(freqmap.find(topfreq) != freqmap.end()) {
+//                 vector<int> v = freqmap[topfreq];
+//                 int n = v.size();
+//                 for(int i=0; i<n; i++) {
+//                     ans.push_back(v[i]);
+//                     ctr++;
+//                     if(ctr == k)
+//                         break;
+//                 }
+//             }
+//             topfreq--;
+//         }
+
+//         return ans;
+//     }
+// };
