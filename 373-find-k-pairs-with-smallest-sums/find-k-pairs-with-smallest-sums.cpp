586fb07e7ec958dd011f77ad5c6411fd0558f436
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-05 21:46:29 +0530

    Time: 103 ms (22.15%) | Memory: 135.6 MB (75.57%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.cpp
@@ -4,21 +4,24 @@ public:
     vector<vector<int>> kSmallestPairs(vector<int>& nums1, vector<int>& nums2, int k) {
         int m=nums1.size();
         int n=nums2.size();
-        priority_queue<p,vector<p>> pq;//maxheap- store {sum,(i,j)};
+        priority_queue<p,vector<p>> pq;//maxheap- store top k smallest sum pairs {sum,(i,j)};
+        //if heap size exceeds k, we remove the one with the largest sum
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){//for all pairs
                 int sum=nums1[i]+nums2[j];
                 if(pq.size()<k){
                     pq.push({sum,{i,j}});
                 }else if(pq.top().first>sum){
-                    pq.pop();
+                    pq.pop();//remove the worst pair-one w the largest sum among all the ones currently present in the pq
                     pq.push({sum,{i,j}});
-                }else{//top sum is smaller than curr sum, so break
+                }else{
+                //since arrs are sorted, future j will only increase the sum
                     break;
                 }
             }
         }
         vector<vector<int>> ans;
+        //get all pairs frm heap
         while(!pq.empty()){
             auto curr=pq.top();
             pq.pop();
