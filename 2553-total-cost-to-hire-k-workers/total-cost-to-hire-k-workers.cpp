8e06b7a796acefb95b1a45e9f0e112301a87cab8
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-05 21:50:54 +0530

    Time: 43 ms (56.33%) | Memory: 76.7 MB (92.15%) - LeetSync

--- 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
+++ 2553-total-cost-to-hire-k-workers/total-cost-to-hire-k-workers.cpp
@@ -9,26 +9,30 @@ public:
         priority_queue<int,vector<int>,greater<>> pq2;
         int hired=0;
         long long cost=0;
-        int i=0; int j=n-1;
+        int i=0;//ptr to start of arr
+        int j=n-1;//to end of arr
 
         while(hired<k){//k sessions
+            //fill front candidate minheap(if not full)
             while(pq1.size()<candidates && i<=j){
                 pq1.push(costs[i]);
                 i++;//move to next idx
             }
+            //fill back candidate minheap(if not full)
             while(pq2.size()<candidates && j>=i){
                 pq2.push(costs[j]);
                 j--;//moving frm last idx to no of candidates
             }
+            // Get top (min) from each heap (or INT_MAX if empty)
             //if pq1 has no elmnts , set min_pq1 to inf
             int min_pq1= pq1.size()>0 ? pq1.top():INT_MAX;
             //if pq2 has no elmnts , set min_pq2 to inf
             int min_pq2= pq2.size()>0 ? pq2.top():INT_MAX;
-
+            //Hire from the cheaper pool
             if(min_pq1<=min_pq2){//in case of tie,lower idx elmnt will be in pq1
                 cost+=min_pq1;
                 pq1.pop();
-            }else{//
+            }else{
                 cost+=min_pq2;
                 pq2.pop();
             }
