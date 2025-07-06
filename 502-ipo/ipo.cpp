da553ae5b282f77f02195e362b9e4757b9a55811
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-06 19:51:19 +0530

    Time: 70 ms (78.14%) | Memory: 125.3 MB (79.29%) - LeetSync

--- 502-ipo/ipo.cpp
+++ 502-ipo/ipo.cpp
@@ -1,25 +1,25 @@
-class Solution {
+class Solution {//TC-O(nlogn),SC-O(n)
 public:
     int findMaximizedCapital(int k, int w, vector<int>& profits, vector<int>& capital) {
         int n=profits.size(); 
-        vector<pair<int,int>> vec(n);
+        vector<pair<int,int>> vec(n);//{cost/capital_req,profit}
         for(int i=0;i<n;i++){
             vec[i]={capital[i],profits[i]};
         }
-        sort(begin(vec),end(vec)); //sort based on capitl
-        int i=0;
-        priority_queue<int> pq;
-        while(k--){
-            while(i<n && vec[i].first<=w){
-                pq.push(vec[i].second);
-                i++;
+        sort(begin(vec),end(vec));//sort based on capital
+        int i=0;//to iter on vec
+        priority_queue<int> pq;//maxheap
+        while(k--){//run till we hv done k projects
+            while(i<n && vec[i].first<=w){//if cost of proj <starting capital
+                pq.push(vec[i].second);//we can take it-so add profit to pq
+                i++;//nxt idx
             }
             if(pq.empty()){
                 break;
             }
-            w+=pq.top();
-            pq.pop();
+            w+=pq.top();//max profit at top-maxheap
+            pq.pop();//remove it frm pq since we hv now done this proj
         }
-        return w;
+        return w;//our max final capital after doing k projs
     }
 };
