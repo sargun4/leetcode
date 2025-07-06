c72c3977628d6fee01ae33d5875c5ff556bea2fa
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-06 19:11:30 +0530

    Time: 15 ms (38.40%) | Memory: 31.7 MB (67.80%) - LeetSync

--- 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
+++ 887-minimum-cost-to-hire-k-workers/minimum-cost-to-hire-k-workers.cpp
@@ -3,21 +3,21 @@ public:
     double mincostToHireWorkers(vector<int>& quality, vector<int>& wage, int k) {
         int n=quality.size();
         vector<pair<double,int>> wageToQualityRatio;//{ratio,quality}
-        for(int i=0;i<n;i++){
+        for(int i=0;i<n;i++){//get wage/quality ratio for each worker
             double ratio = (double)wage[i] / quality[i] ;
             wageToQualityRatio.push_back({ratio, quality[i]});
         }
-        sort(begin(wageToQualityRatio),end(wageToQualityRatio));
+        sort(begin(wageToQualityRatio),end(wageToQualityRatio));//sort asc by ratio
 
         double sum_quality=0;
         double mincost=DBL_MAX;
-        //max-heap to keep track of top k smallest qualities
+        //max-heap to keep track of top k lowest-quality workers at each ratio
         priority_queue<int> pq;
         for(int i=0;i<n;i++){
             double ratio=wageToQualityRatio[i].first;
             int quality=wageToQualityRatio[i].second;
 
-            pq.push(quality);
+            pq.push(quality);//add curr worker
             sum_quality+=quality;
             //if more than k workers,remove highest quality one
             if(pq.size()>k){
