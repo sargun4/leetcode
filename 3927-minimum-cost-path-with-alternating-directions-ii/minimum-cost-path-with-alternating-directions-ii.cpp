92416423be141e4c0d600972ad310cbb424edf17
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-07 02:13:34 +0530

    Time: 744 ms (52.63%) | Memory: 183.3 MB (68.42%) - LeetSync

---
+++ 3927-minimum-cost-path-with-alternating-directions-ii/minimum-cost-path-with-alternating-directions-ii.cpp
@@ -0,0 +1,52 @@
+//time%2!=0 : R or D movt
+class Solution {
+public:
+    typedef long long ll;
+    typedef pair<long long,pair<int,pair<int,int>>> p;
+    // bool isValid(int i,int j,int m,int n){
+    //     return i>=0 && j>=0 && i<m && j<n;
+    // }
+    // vector<vector<int>>dirns={{1,0},{0,1}};//only R and D
+    long long minCost(int m, int n, vector<vector<int>>& waitCost){
+        vector<vector<ll>> vis(m,vector<ll>(n,1e18));
+        vis[0][0]=1;
+        int startcost=1;//(0+1)*(0+1)
+        priority_queue<p,vector<p>,greater<p>> pq;
+        pq.push({startcost,{1,{0,0}}});//{cost,{time,{x,y}}};
+        while(!pq.empty()){
+            auto it=pq.top(); pq.pop();
+            ll cost=it.first;
+            int time=it.second.first;
+            int x=it.second.second.first;
+            int y=it.second.second.second;
+
+            if(x==m-1 && y==n-1){//reached bottom right cell
+                return cost;
+            }
+            if(time%2==0){//even time-get wait cost and add to curr cost
+                ll newcost=cost+waitCost[x][y];
+                pq.push({newcost,{time+1,{x,y}}});
+            }else{//odd time-R or D movt 
+                int nx=x+1; 
+                int ny=y+1;
+                if(nx<m){//D
+                    ll entrycost=1LL*(nx+1)*(ny);
+                    ll newcost=cost+entrycost;
+                    if(newcost<vis[nx][y]){
+                        vis[nx][y]=newcost;
+                        pq.push({newcost,{time+1,{nx,y}}});
+                    }
+                }
+                if(ny<n){//R
+                    ll entrycost=1LL*(nx)*(ny+1);
+                    ll newcost=cost+entrycost;
+                    if(newcost<vis[x][ny]){
+                        vis[x][ny]=newcost;
+                        pq.push({newcost,{time+1,{x,ny}}});
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};
