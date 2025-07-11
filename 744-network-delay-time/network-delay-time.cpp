021e3dd5f740428769f8b1ad35c8abbaadfee41a
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-12 02:48:23 +0530

    Time: 88 ms (47.16%) | Memory: 48.1 MB (18.53%) - LeetSync

---
+++ 744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,34 @@
+using p=pair<int,int>;
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k) {
+        vector<vector<p>> adj(n+1);
+        for(auto e:times){//directed frm u to v only
+        int u=e[0]; int v=e[1]; int wt=e[2];
+            adj[u].push_back({v,wt});
+            // adj[e[1]].push_back({e[0],e[2]});
+        }
+        const int INF=1e9;
+        vector<int> dist(n+1,INF);
+        //minheap
+        priority_queue<p,vector<p>,greater<>> pq;//{time,node}
+        dist[k]=0;
+        pq.push({0,k});//initially time=0 & start frm node= k
+        while(!pq.empty()){
+            auto [time,node]=pq.top(); pq.pop();
+            if(time>dist[node]) continue;
+            for(auto &[v,w]:adj[node]){
+                if(dist[v]>time+w){//store the min time
+                    dist[v]=time+w;
+                    pq.push({dist[v],v});
+                }
+            }
+        }
+        int mintime=0;
+        for(int i=1;i<=n;i++){//nodes labelled 1 to n
+            if(dist[i]==INF) return -1; //if node i's time didnt change-unreachble
+            mintime=max(mintime,dist[i]);
+        }
+        return mintime;
+    }
+};
