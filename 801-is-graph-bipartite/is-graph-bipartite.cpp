98b33d3328754f8496895e93277d2274605200dd
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-13 17:12:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (38.21%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.cpp
+++ 801-is-graph-bipartite/is-graph-bipartite.cpp
@@ -10,9 +10,10 @@ public:
             int u=q.front();
             q.pop();
             for(int &v:adj[u]){
+            //adj node is same color as node u-NOT bipartite
                 if(color[v]==color[u])
                     return false;
-                else if(color[v]==-1){//not vis yet
+                else if(color[v]==-1){//not vis yet,mark it w alt color
                     color[v]=1-color[u];
                     q.push(v);
                 }
@@ -22,10 +23,10 @@ public:
     }
     bool isBipartite(vector<vector<int>>& adj) {
         int n=adj.size();
-        vector<int> color(n, -1); //ALL UNCOlored initially
+        vector<int> color(n, -1); //ALL Uncolored initially
         //red=1; green=0; 
         for(int i=0;i<n;i++){
-            if(color[i]==-1){
+            if(color[i]==-1){//if not yet vis
                 if(bfs(adj,i,color,1)==false){
                     return false;
                 }
