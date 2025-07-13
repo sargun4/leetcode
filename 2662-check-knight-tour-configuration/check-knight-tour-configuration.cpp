7742ed860db7aea0f2dbb0df052f1ad04d11b521
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-14 02:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 34.5 MB (5.14%) - LeetSync

---
+++ 2662-check-knight-tour-configuration/check-knight-tour-configuration.cpp
@@ -0,0 +1,75 @@
+class Solution {
+public:
+    bool isValid(int i,int j,int m,int n){
+        return i>=0 && i<m && j>=0 &&j<n;
+    }
+    bool checkValidGrid(vector<vector<int>>& grid) {
+        int n=grid.size();
+        vector<vector<int>> dirns={
+            {-2,-1}, //x-2,y-1 : 2 up,1 left
+            {-2,+1}, //x-2,y+1 : 2 up,1 right
+            {+2,-1}, //x+2,y-1 : 2 down,1 left
+            {+2,+1}, //x+2,y+1 : 2 down,1 right
+            {-1,-2}, //x-1,y-2 : 1 up,2 left
+            {-1,+2}, //x-1,y+2 : 1 up,2 right
+            {+1,-2}, //x+1,y-2 : 1 down,2 left
+            {+1,+2}, //x+1,y+2 : 1 down,2 right
+        };
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        int x=0; int y=0;//start at(0,0)
+        for(int i=0;i<n*n;i++){//go over all grid cells
+            if(grid[x][y]!=i){
+                return false;
+            }
+            vis[x][y]=true;
+            for(auto &d:dirns){
+                int nx=x+d[0];
+                int ny=y+d[1];
+                if(isValid(nx,ny,n,n) && !vis[nx][ny] && grid[nx][ny]==i+1){
+                    x=nx;
+                    y=ny;
+                }
+            }
+        }
+        return true;
+    }
+}; 
+
+// class Solution {
+// public:
+// 	int minStepToReachTarget(vector<int>&KnightPos, vector<int>&TargetPos, int N){
+// 	    int er=TargetPos[0];
+//         int ec=TargetPos[1];
+//         queue<pair<int,pair<int,int>>> q;
+//         vector<vector<bool>> vis(N+1, vector<bool>(N+1, false));
+// 	    int sr=KnightPos[0];
+// 	    int sc=KnightPos[1];
+//         if(sr==er && sc==ec)
+//             return 0;
+//         int steps=0;
+// 	    q.push({steps,{sr-1,sc-1}});
+//         vis[sr-1][sc-1]=1;
+//         int dx[8]={2,2,-2,-2,1,1,-1,-1};
+//         int dy[8]={1, -1, 1, -1, 2, -2, 2, -2};
+//         while(!q.empty()){
+//             auto it=q.front();
+//             int steps=it.first;
+//             int i=it.second.first;
+//             int j=it.second.second;
+//             q.pop();
+//             if(i==er-1 && j==ec-1){
+//                 return steps;
+//             }
+//             for(int k=0;k<8;k++){
+//                 int nx=i+dx[k];
+//                 int ny=j+dy[k];
+//                 if(nx>=0 && ny>=0 && nx<N && ny<N && vis[nx][ny]==0){
+//                     vis[nx][ny]=1;
+//                     q.push({steps+1,{nx,ny}});
+//                 }
+//             }
+//             steps++;
+//         }
+//         return -1;
+// 	}
+// }; 
