9ff973bc04448964955f57cce0144a4da2202131
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-14 13:12:10 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (13.59%) - LeetSync

--- 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -1,27 +1,53 @@
 class Solution {
 public:
-    int binToDec(string s){
-        int dec=0;
-        int base=1;
-        int n=(int) s.length();
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='1'){
-                dec+=base;
-            }
-            base=base*2;
+    ListNode* revLL(ListNode* head){
+        if(head==NULL||head->next==NULL){
+            return head;
         }
-        return dec;
+        ListNode* last=revLL(head->next);
+        head->next->next=head;
+        head->next=NULL;
+        return last;
     }
     int getDecimalValue(ListNode* head) {
-        string s;
-        ListNode* curr=head;
-        while(curr!=NULL){
-            int data=curr->val;
-            char x=(data==1)? '1':'0';
-            s=s+x;
-            curr=curr->next;
+        head=revLL(head);
+        int res=0;
+        int power=0;
+        while(head!=NULL){
+            if(head->val==1){
+                res+=pow(2,power);
+            }
+            power++;
+            head=head->next;
         }
-        int ans=binToDec(s);
-        return ans;
+        return res;
     }
 };
+
+// class Solution {
+// public:
+//     int binToDec(string s){
+//         int dec=0;
+//         int base=1;
+//         int n=(int) s.length();
+//         for(int i=n-1;i>=0;i--){
+//             if(s[i]=='1'){
+//                 dec+=base;
+//             }
+//             base=base*2;
+//         }
+//         return dec;
+//     }
+//     int getDecimalValue(ListNode* head) {
+//         string s;
+//         ListNode* curr=head;
+//         while(curr!=NULL){
+//             int data=curr->val;
+//             char x=(data==1)? '1':'0';
+//             s=s+x;
+//             curr=curr->next;
+//         }
+//         int ans=binToDec(s);
+//         return ans;
+//     }
+// };
