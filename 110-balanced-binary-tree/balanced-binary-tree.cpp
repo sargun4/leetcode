624ce770c6bb4079a1054e5b6126ff1efe59c7c6
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-18 17:10:38 +0530

    Time: 0 ms (100.00%) | Memory: 23 MB (83.77%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -1,51 +1,31 @@
-//O(n)
-class Solution {
-public:
-    bool isBalanced(TreeNode *root){
-        return dfsHt(root)!=-1;
-    }
-    int dfsHt(TreeNode *root){
-        if(root==NULL) return 0;
-        
-        int lht=dfsHt(root->left);
-        if(lht==-1) return -1;
-        int rht=dfsHt(root->right);
-        if(rht==-1) return -1;
-        
-        if(abs(lht-rht)>1) return -1;
-        return max(lht,rht)+1;
-    }
-};
+// //O(n)
 // class Solution {
 // public:
-//     int depth (TreeNode *root) {
-//         if (root == NULL) return 0;
-//         return max (depth(root -> left), depth (root -> right)) + 1;
+//     bool isBalanced(TreeNode *root){
+//         return dfsHt(root)!=-1;
 //     }
-
-//     bool isBalanced (TreeNode *root) {
-//         if (root == NULL) return true;
+//     int dfsHt(TreeNode *root){
+//         if(root==NULL) return 0;
         
-//         int left=depth(root->left);
-//         int right=depth(root->right);
+//         int lht=dfsHt(root->left);
+//         if(lht==-1) return -1;
+//         int rht=dfsHt(root->right);
+//         if(rht==-1) return -1;
         
-//         return abs(left - right) <= 1 && isBalanced(root->left) && isBalanced(root->right);
-//     }
-// // };
-// class Solution {
-// public:
-// int dfsHeight (TreeNode *root) {
-//         if (root == NULL) return 0;
-        
-//         int leftHeight = dfsHeight (root -> left);
-//         if (leftHeight == -1) return -1;
-//         int rightHeight = dfsHeight (root -> right);
-//         if (rightHeight == -1) return -1;
-        
-//         if (abs(leftHeight - rightHeight) > 1)  return -1;
-//         return max (leftHeight, rightHeight) + 1;
-//     }
-//     bool isBalanced(TreeNode *root) {
-//         return dfsHeight (root) != -1;
+//         if(abs(lht-rht)>1) return -1;
+//         return max(lht,rht)+1;
 //     }
 // };
+class Solution {
+public:
+    int ht(TreeNode *root){
+        if(!root) return 0;
+        return max(ht(root->left),ht(root->right))+1;
+    }
+    bool isBalanced(TreeNode *root){
+        if(!root) return true;
+        int left=ht(root->left);
+        int right=ht(root->right);
+        return abs(left-right) <=1 && isBalanced(root->left) && isBalanced(root->right);
+    }
+}; 
