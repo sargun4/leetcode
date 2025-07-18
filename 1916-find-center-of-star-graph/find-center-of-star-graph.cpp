f88fefe2fb89275f9dea4cdf3fa448de9b174bb4
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-18 22:37:51 +0530

    Time: 55 ms (26.85%) | Memory: 121.3 MB (22.84%) - LeetSync

--- 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int findCenter(vector<vector<int>>& edges) {
-        unordered_map<int,int> degree;
+        unordered_map<int,int> degree;//{node:degree}
         for(auto &vec:edges){
             int u=vec[0];
             int v=vec[1];
@@ -9,8 +9,8 @@ public:
             degree[v]++;
         }
         for(auto &it:degree){
-            if(it.second==edges.size())
-                return it.first; //centre
+            if(it.second==edges.size())//node w (degree of node=no of edges) is the centre
+                return it.first; 
         }
 
         return -1;
