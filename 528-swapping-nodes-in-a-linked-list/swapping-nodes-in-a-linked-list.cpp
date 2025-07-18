7bf247b2abad70e32a239784f627588049996aa3
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-18 20:48:55 +0530

    Time: 7 ms (16.69%) | Memory: 185.3 MB (38.36%) - LeetSync

--- 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
+++ 528-swapping-nodes-in-a-linked-list/swapping-nodes-in-a-linked-list.cpp
@@ -8,7 +8,6 @@ public:
             curr=curr->next;
             len++;
         }
-        
         // cout<<len;
         int frmbeg=k; //kth node frm beignin
         int frmend=len-k+1; //kth node frm end
@@ -17,15 +16,12 @@ public:
         while(--frmbeg){
             n1=n1->next;
         }
- 
         ListNode* n2=head;
         while(--frmend){
             n2=n2->next;
         }
-          
         // cout<<n1->val<<endl;
         // cout<<n2->val<<endl;
-
         ListNode* temp=new ListNode(0);
         //jsut swap the vals,not the nodes
         temp->val=n1->val;
