e96c1e876c3cec98bb0b46bd673dee447b35d803
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-19 22:34:22 +0530

    Time: 15 ms (40.53%) | Memory: 98.5 MB (38.54%) - LeetSync

--- 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
+++ 2764-maximum-number-of-fish-in-a-grid/maximum-number-of-fish-in-a-grid.cpp
@@ -1,18 +1,25 @@
+// //bfs
+// class Solution {
+// public:
 
-#include<bits/stdc++.h>
-using namespace std; 
+//     int findMaxFish(vector<vector<int>>& grid) {
+//         m=grid.size();
+//         n=grid[0].size();
+//         vector<vector<int>> vis(m,vector<int>(n,0));
+//     }
+// };
+//dfs
 class Solution {
 public:
-    int dx[4]={-1,0,1,0};
-    int dy[4]={0,-1,0,1};
+    vector<vector<int>> dirns={{1,0},{0,1},{-1,0},{0,-1}};
     int m,n;
     int dfs(int i,int j,vector<vector<int>> &vis,vector<vector<int>> &grid){
         if(i<0||j<0||i>=m||j>=n||vis[i][j]) return 0;
         vis[i][j]=1;
         int ans=0;
-        for(int k=0;k<4;k++){
-            int x=i+dx[k];
-            int y=j+dy[k];
+        for(auto&d:dirns){
+            int x=i+d[0];
+            int y=j+d[1];
             if(x<m && x>=0 && y<n && y>=0 && !vis[x][y] && grid[x][y]){
                 ans+=grid[x][y]+dfs(x,y,vis,grid);
             }
