bb393fc3c4de7e1d6490ae344f96bc20218056d4
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-20 20:08:40 +0530

    Time: 301 ms (27.19%) | Memory: 152.3 MB (44.72%) - LeetSync

--- 854-making-a-large-island/making-a-large-island.cpp
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -5,8 +5,9 @@ public:
         return i>=0 && i<m && j>=0 && j<n;
     }
     vector<vector<int>> dirns={{1,0},{0,1},{-1,0},{0,-1}};
+    //dfs to label the island and compute its size
     int dfs(vector<vector<int>>& grid,int i,int j,vector<vector<bool>>&vis,int id){
-        //if we go out of bounds or cell=0 or cell alr vis, 
+        //if we go out of bounds or cell=0(water) or cell alr vis, 
         if(!isValid(i,j,n,n) || grid[i][j]==0 || vis[i][j]==true){
             return 0;
         }
@@ -24,19 +25,20 @@ public:
         n=grid.size();//row=col=n
         vector<vector<bool>> vis(n,vector<bool>(n,false));
         int maxarea=0;
-        int id=2;//unique id of island
+        int id=2;//unique id of island-(since grid already has 0 and 1)
         unordered_map<int,int> map;//{id:size_island}
+        // **Step 1: Label all islands and record their sizes**
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1 && !vis[i][j]){
                     int size=dfs(grid,i,j,vis,id);
-                    maxarea=max(maxarea,size);
+                    maxarea=max(maxarea,size);//track of largest existing island
                     map[id]=size;
                     id++;
                 }
             }
         }
-
+        // **Step 2: Try flipping each 0 and compute new island size**
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==0){//try converting all the 0 cells to unique id cells
