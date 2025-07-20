38cee4b09961687fd62a7b5c48d1e4ac1f198924
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-20 13:16:09 +0530

    Time: 2 ms (36.68%) | Memory: 14.8 MB (13.97%) - LeetSync

--- 684-redundant-connection/redundant-connection.cpp
+++ 684-redundant-connection/redundant-connection.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
+    // DFS to check if v is reachable from u
     bool dfs(unordered_map<int,vector<int>> &adj,int u,int v, vector<bool>&vis){
-        vis[u]=true;
-        if(u==v) return true;
+        vis[u]=true;//mark curr node u as vis to avoid revisiting
+        if(u==v) return true;//if we have reached v starting from u, then a path exists
+        //explore all neighbors of the current node u
         for(int &neigh:adj[u]){
+        //if neigh not vis,perform dfs for it
             if(vis[neigh]) continue;
+            //if v i reachable thru this neigh
             if(dfs(adj,neigh,v,vis)){
                 return true;
             }
         }
-        return false;
+        return false;//no path frm u to v found in dfs path
     }
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
@@ -18,12 +22,17 @@ public:
             int u=edges[i][0];
             int v=edges[i][1];
             vector<bool>vis(n,false);
+        // Check if adding this edge (u,v) creates a cycle:
+        // - If both u and v already exist in adj and a path from u to v exists,
+        //   adding this edge would close the cycle.
+        //if both u and v exist & r connected, this edge is redundant
             if(adj.find(u)!=adj.end() && adj.find(v)!=adj.end() && dfs(adj,u,v,vis)){
-                return edges[i];
+                return edges[i];//redudnant, return it
             }
+        //else add edge to the graph
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        return {};
+        return {};//wont reach here as problem guarantees a redundant edge
     }
 };
