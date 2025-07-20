f50b5fbcf12f3aeecc5d979ec3da26ec5b7372ea
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-20 19:46:33 +0530

    Time: 288 ms (36.79%) | Memory: 152 MB (44.88%) - LeetSync

---
+++ 854-making-a-large-island/making-a-large-island.cpp
@@ -0,0 +1,61 @@
+class Solution {
+public:
+    int n;
+    bool isValid(int i,int j,int m,int n){
+        return i>=0 && i<m && j>=0 && j<n;
+    }
+    vector<vector<int>> dirns={{1,0},{0,1},{-1,0},{0,-1}};
+    int dfs(vector<vector<int>>& grid,int i,int j,vector<vector<bool>>&vis,int id){
+        //if we go out of bounds or cell=0 or cell alr vis, 
+        if(!isValid(i,j,n,n) || grid[i][j]==0 || vis[i][j]==true){
+            return 0;
+        }
+        grid[i][j]=id;//same id assigned to all cells in that island
+        vis[i][j]=true;
+        int size=1;
+        for(vector<int>&d:dirns){
+            int nx=i+d[0];
+            int ny=j+d[1];
+            size+=dfs(grid,nx,ny,vis,id);
+        }
+        return size;
+    }
+    int largestIsland(vector<vector<int>>& grid){
+        n=grid.size();//row=col=n
+        vector<vector<bool>> vis(n,vector<bool>(n,false));
+        int maxarea=0;
+        int id=2;//unique id of island
+        unordered_map<int,int> map;//{id:size_island}
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==1 && !vis[i][j]){
+                    int size=dfs(grid,i,j,vis,id);
+                    maxarea=max(maxarea,size);
+                    map[id]=size;
+                    id++;
+                }
+            }
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]==0){//try converting all the 0 cells to unique id cells
+                    unordered_set<int> unique_ids;
+                    for(auto&d:dirns){
+                        int nx=i+d[0];
+                        int ny=j+d[1];
+                        if(isValid(nx,ny,n,n)&& grid[nx][ny]!=0){
+                            unique_ids.insert(grid[nx][ny]);
+                        }
+                    }
+                    int overallsize=1;
+                    for(auto &id:unique_ids){
+                        overallsize+=map[id];
+                    }
+                    maxarea=max(maxarea,overallsize);
+                }
+            }
+        }
+        return maxarea;
+    }
+};
