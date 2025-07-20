c1390aa3353433891a969052fe1bf9545ee09d79
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-20 23:15:52 +0530

    Time: 41 ms (85.40%) | Memory: 101.3 MB (79.52%) - LeetSync

--- 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
+++ 2220-find-all-possible-recipes-from-given-supplies/find-all-possible-recipes-from-given-supplies.cpp
@@ -3,18 +3,22 @@ class Solution {
 public:
     vector<string> findAllRecipes(vector<string>& recipes, vector<vector<string>>& ingredients, vector<string>& supplies) {
         int n=recipes.size();
+        //convert supplies to a set for O(1) lookup
         unordered_set<string> st(begin(supplies),end(supplies));
         unordered_map<string,vector<int>> adj;//{ingred->recipe};
+        //indegree[i]= no of missing ingredients
         vector<int> indegree(n,0);
         for(int i=0;i<n;i++){//go over all recipes
             for(string& ing:ingredients[i]){//and chekc each ingredient
                 if(!st.count(ing)){//if ing not present in supplies set
                     //this recipe is dependet on the ingredient
+                    //ingredient is not already in supplies,so we create dependency
                     adj[ing].push_back(i);
                     indegree[i]++;
                 }
             }
         }
+        //initialize q with all recipes that hv indegree=0
         queue<int> q;
         for(int i=0;i<n;i++){
             if(indegree[i]==0){
@@ -22,10 +26,13 @@ public:
             }
         }
         vector<string> res;
+        //bfs (Kahn's Toposort)
         while(!q.empty()){
             int node=q.front(); q.pop();
             string recipe=recipes[node];
             res.push_back(recipe);
+            //treat this recipe as a new "supply"
+            //for all recipes depending on it, reduce their indegree 
             for(int&neigh_idx:adj[recipe]){
                 indegree[neigh_idx]--;
                 if(indegree[neigh_idx]==0){
@@ -33,6 +40,7 @@ public:
                 }
             }
         }
+        //all recipes added to res are the ones we can make
         return res;
     }
 };
