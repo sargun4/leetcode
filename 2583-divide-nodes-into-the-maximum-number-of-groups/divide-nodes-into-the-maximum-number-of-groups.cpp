aaad53aca8f9890292aa0b3ff76c18b4f307ae52
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-20 18:38:27 +0530

    Time: 153 ms (56.53%) | Memory: 41.9 MB (95.24%) - LeetSync

--- 2583-divide-nodes-into-the-maximum-number-of-groups/divide-nodes-into-the-maximum-number-of-groups.cpp
+++ 2583-divide-nodes-into-the-maximum-number-of-groups/divide-nodes-into-the-maximum-number-of-groups.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-//dfs
+//dfs-max no of grps possible for each connected component of graph
     int getMaxFrmEachComp(unordered_map<int,vector<int>> &adj,int curr,vector<bool>&vis,vector<int>&lvls){
-        int maxgrp=lvls[curr];
+        int maxgrp=lvls[curr];//stores max no of lvls(grps) starting frm node curr
         vis[curr]=true;
+        // DFS across the component and take the max lvls[node] found.
         for(int &neigh:adj[curr]){
             if(!vis[neigh]){
                 maxgrp=max(maxgrp,getMaxFrmEachComp(adj,neigh,vis,lvls));
@@ -11,13 +12,14 @@ public:
         }
         return maxgrp;
     }
+//  ensures that valid grps can be formed (since only bipartite graphs can be layered like this)
     bool isBipartite(unordered_map<int,vector<int>> &adj,int node,vector<int>&colors,int currcolor){
         colors[node]=currcolor;
         for(int &neigh:adj[node]){
             if(colors[neigh]==colors[node]){
-                return false;
+                return false;//Same color as curr node so not bipartite
             }
-            if(colors[neigh]==-1){
+            if(colors[neigh]==-1){//if neigh still unvisited
                 if(isBipartite(adj,neigh,colors,1-currcolor)==false){
                     return false;
                 }
@@ -42,7 +44,7 @@ public:
                     }
                 }
             }
-            lvl++;
+            lvl++;//move to nxt lvl
         }
         return lvl-1;
     }
@@ -59,7 +61,7 @@ public:
         for(int node=0;node<n;node++){
             if(colors[node]==-1){//not yet colored
                 if(isBipartite(adj,node,colors,1)==false){
-                    return -1;
+                    return -1;//if any component is not bipartite,ans=-1
                 }
             }
         }
@@ -71,6 +73,7 @@ public:
         }
         int maxGrpsFrmEachComp=0;
         vector<bool> vis(n,false);
+        // For each connected component, we add the maximum levels of any node in that component.
         for(int node=0;node<n;node++){
             if(!vis[node]){
                 maxGrpsFrmEachComp+=getMaxFrmEachComp(adj,node,vis,lvls);
