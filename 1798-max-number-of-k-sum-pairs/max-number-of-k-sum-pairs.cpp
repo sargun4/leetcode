e93e8efec626d8665f7d872946b844ba5419396f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-21 21:09:40 +0530

    Time: 111 ms (34.64%) | Memory: 76.8 MB (10.46%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.cpp
@@ -19,3 +19,23 @@ public:
 //if complement exists in the hashmap & ctr > 0:
 // if a valid pair, increment ctr & decrease the frequency of the complement
 // else, store or increment frequency of i in the hashmap
+// //2ptr
+// class Solution {
+// public:
+//     int maxOperations(vector<int>& nums, int k) {
+//         sort(nums.begin(), nums.end());
+//         int i = 0, ctr = 0;
+//         int j = nums.size() - 1;
+//         while(i < j){
+//             int sum = nums[i] + nums[j];
+//             if(sum == k){
+//                 i++;
+//                 j--;
+//                 ctr++;
+//             }
+//             else if(sum < k) i++;
+//             else j--;
+//         }
+//         return ctr;
+//     }
+// };
