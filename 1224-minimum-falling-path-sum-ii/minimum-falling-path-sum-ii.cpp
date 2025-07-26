f55521057169bd639764db26d77ffe764d9bec84
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-26 20:34:15 +0530

    Time: 111 ms (64.16%) | Memory: 21.5 MB (23.49%) - LeetSync

--- 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
+++ 1224-minimum-falling-path-sum-ii/minimum-falling-path-sum-ii.cpp
@@ -1,32 +1,60 @@
+//memo T-O(n^3)
+// S-O(n^2):2d dp
 class Solution {  
-    int f(int i, int j, vector<vector<int>>& mat, vector<vector<int>>& dp) {
-        if(i < 0 || j < 0 || j >= mat[0].size()) {
-            return 1e8;
-        }  
-        if(dp[i][j] != -1) {
-            return dp[i][j];
-        } 
-        if(i == 0) {
-            return mat[0][j];
-        } 
-        int mini = 1e8; 
-        for(int k=0; k<mat[0].size(); k++) {
-            if(k != j) {
-                mini = min(mini, mat[i][j] + f(i-1, k, mat, dp));
-            }
-        } 
-        dp[i][j] = mini; 
-        return dp[i][j];
-    } 
 public:
+    //dp[i][j]=min falling path sum frm row i to row n-1 when jth col chosen in ith row
     int minFallingPathSum(vector<vector<int>>& mat) {
-        int mini = 1e8;
-        int n = mat.size(), m = mat[0].size(); 
-        // declared a dp array
-        vector<vector<int>> dp(n, vector<int>(m, -1)); 
-        for(int j=0; j<m; j++) {
-            mini = min(mini, f(n-1, j, mat, dp));
+        int n=mat.size();
+        vector<vector<int>> dp(n,vector<int>(n,INT_MAX));
+        for(int col=0;col<n;col++){
+            dp[n-1][col]=mat[n-1][col];
         }
-        return mini;
+        for(int row=n-2;row>=0;row--){
+            for(int col=0;col<n;col++){
+                int ans=INT_MAX;
+                for(int nextCol=0;nextCol<n;nextCol++){
+                    if(nextCol!=col){
+                        ans=min(ans,dp[row+1][nextCol]);
+                    }
+                }
+                dp[row][col]=mat[row][col]+ans;
+            }
+        }
+        int res=INT_MAX;
+        for(int col=0;col<n;col++){
+            res=min(res,dp[0][col]);
+        }
+        return res;
     }
 };
+// //memo T-O(n^3)
+// // S-O(n^2):2d dp
+// class Solution {  
+// public:
+//     int n;
+//     int dp[201][201];
+//     int solve(int col,int row,vector<vector<int>>&mat){
+//         if(row==n-1){
+//             return mat[row][col];
+//         }
+//         if(dp[row][col]!=-1){
+//             return dp[row][col];
+//         }
+//         int ans=INT_MIN;
+//         for(int nextcol=0;nextcol<n;nextcol++){
+//             if(nextcol!=col){//nextcol shdnt be = curr col
+//                 ans=min(ans,solve(nextcol,row+1,mat));
+//             }
+//         }
+//         return dp[row][col]=ans+mat[row][col];
+//     }
+//     int minFallingPathSum(vector<vector<int>>& mat) {
+//         n=mat.size();
+//         memset(dp,-1,sizeof(dp));
+//         int res=INT_MAX;
+//         for(int col=0;col<n;col++){//choose the col to start with
+//             res=min(res,solve(col,0,mat));
+//         }
+//         return res;
+//     }
+// };
