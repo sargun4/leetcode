d3f142561411ae062008e4a4ce643f8d0d26493f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-27 20:30:37 +0530

    Time: 14 ms (52.83%) | Memory: 19.3 MB (23.83%) - LeetSync

--- 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
+++ 116-populating-next-right-pointers-in-each-node/populating-next-right-pointers-in-each-node.cpp
@@ -1,51 +1,54 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* left;
-    Node* right;
-    Node* next;
-
-    Node() : val(0), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val) : val(_val), left(NULL), right(NULL), next(NULL) {}
-
-    Node(int _val, Node* _left, Node* _right, Node* _next)
-        : val(_val), left(_left), right(_right), next(_next) {}
-};
-*/
 class Solution {
 public:
     Node* connect(Node* root) {
-        if(!root || !root->left) return root;
-        //trav each lvl using bfs and connect the next ptrs
+        if(root==NULL) return root;
         queue<Node*> q;
-        q.push(root);
-        q.push(NULL);
-        Node* prev=NULL;
+        Node* node = root;
+        q.push(node);
         while(!q.empty()){
-            Node* curr=q.front(); q.pop();
-            int len=q.size();
-            if(curr==NULL){
-                if(len==0){
-                    break;
-                }else{
-                    q.push(NULL);
-                }
-            }else{
-                if(curr->left){
-                    q.push(curr->left);
-                }
-                if(curr->right){
-                    q.push(curr->right);
-                }
-                if(prev!=NULL){
-                    prev->next=curr;
-                }
+            int sz=  q.size();
+            for(int i=0; i<sz; i++){
+                Node* temp = q.front();
+                q.pop();
+                if(i<sz-1) temp->next=q.front();
+                if(temp->left) q.push(temp->left);
+                if(temp->right) q.push(temp->right);
             }
-            prev=curr;
         }
         return root;
     }
 };
+// class Solution {
+// public:
+//     Node* connect(Node* root) {
+//         if(!root || !root->left) return root;
+//         //trav each lvl using bfs and connect the next ptrs
+//         queue<Node*> q;
+//         q.push(root);
+//         q.push(NULL);
+//         Node* prev=NULL;
+//         while(!q.empty()){
+//             Node* curr=q.front(); q.pop();
+//             int len=q.size();
+//             if(curr==NULL){
+//                 if(len==0){
+//                     break;
+//                 }else{
+//                     q.push(NULL);
+//                 }
+//             }else{
+//                 if(curr->left){
+//                     q.push(curr->left);
+//                 }
+//                 if(curr->right){
+//                     q.push(curr->right);
+//                 }
+//                 if(prev!=NULL){
+//                     prev->next=curr;
+//                 }
+//             }
+//             prev=curr;
+//         }
+//         return root;
+//     }
+// };
