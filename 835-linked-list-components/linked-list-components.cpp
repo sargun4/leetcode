87e60cf96e29816f43f73ae353c3aef296fdab67
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-27 20:11:29 +0530

    Time: 7 ms (62.89%) | Memory: 25.5 MB (39.21%) - LeetSync

---
+++ 835-linked-list-components/linked-list-components.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int numComponents(ListNode* head, vector<int>& nums) {
+        unordered_set<int> set(nums.begin(),nums.end());
+        int res=0;
+        while(head!=NULL){
+            if(set.count(head->val)){//if head's val exisit in nums
+                if(head->next==NULL || !set.count(head->next->val)){//if we reached end of LL or if head's next val doesnt exist in set, that means its a breaking pt=- so we''l get 1more conn component
+                    res++;
+                }
+            }
+            head=head->next;
+        }
+        return res;
+    }
+};
