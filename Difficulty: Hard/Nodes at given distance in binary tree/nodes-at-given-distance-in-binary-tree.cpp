07135ded2a8bd79e02648d4b0f1ff93b0f5adb7d
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-29 11:50:55 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.cpp
+++ Difficulty: Hard/Nodes at given distance in binary tree/nodes-at-given-distance-in-binary-tree.cpp
@@ -1,94 +1,3 @@
-//{ Driver Code Starts
-#include<bits/stdc++.h>
-using namespace std;
-
-// Tree Node
-struct Node
-{
-    int data;
-    Node* left;
-    Node* right;
-};
-
-// Utility function to create a new Tree Node
-Node* newNode(int val)
-{
-    Node* temp = new Node;
-    temp->data = val;
-    temp->left = NULL;
-    temp->right = NULL;
-    
-    return temp;
-}
-
-// Function to Build Tree
-Node* buildTree(string str)
-{   
-    // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
-    vector<string> ip;
-    
-    istringstream iss(str);
-    for(string str; iss >> str; )
-        ip.push_back(str);
-        
-    // Create the root of the tree
-    Node* root = newNode(stoi(ip[0]));
-        
-    // Push the root to the queue
-    queue<Node*> queue;
-    queue.push(root);
-        
-    // Starting from the second element
-    int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
-        // Get and remove the front of the queue
-        Node* currNode = queue.front();
-        queue.pop();
-            
-        // Get the current node's value from the string
-        string currVal = ip[i];
-            
-        // If the left child is not null
-        if(currVal != "N") {
-                
-            // Create the left child for the current node
-            currNode->left = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->left);
-        }
-            
-        // For the right child
-        i++;
-        if(i >= ip.size())
-            break;
-        currVal = ip[i];
-            
-        // If the right child is not null
-        if(currVal != "N") {
-                
-            // Create the right child for the current node
-            currNode->right = newNode(stoi(currVal));
-                
-            // Push it to the queue
-            queue.push(currNode->right);
-        }
-        i++;
-    }
-    
-    return root;
-}
-
-
-// } Driver Code Ends
-
- 
 // - Mark each node to its parent to traverse upwards
 // - We will do a BFS traversal starting from the target node
 // - As long as we have not seen our node previously, Traverse up, left, right until reached Kth distance
@@ -96,20 +5,19 @@ Node* buildTree(string str)
 // queue is our result
 
 class Solution {
-    Node* findNode(Node* root, int target) {
-        if (root == nullptr) {
-            return nullptr;
+    Node* findNode(Node* root, int target) {//find target node rec
+        if(root==NULL){
+            return NULL;
         }
-        if (root->data == target) {
+        if (root->data ==target) {
             return root;
         }
-        Node* leftResult = findNode(root->left, target);
+        Node* leftResult=findNode(root->left, target);
         if (leftResult) {
             return leftResult;
         }
         return findNode(root->right, target);
     }
-
     void markParents(Node* root, unordered_map<Node*,Node*> &parent_track,int target){
         queue<Node*>q;
         q.push(root);
@@ -135,14 +43,13 @@ public:
         queue<Node*>q;
 
         Node* targetNode = findNode(root, target);
-        if (targetNode == nullptr) {
-            // Target not found in the tree
-            return {};
+        if (targetNode == NULL) {
+            return {};//target not found in the tree
         }
         q.push(targetNode);
         vis[targetNode] = true;
         int currlvl = 0;
-        while(!q.empty()){ //bfs to go upto k lvl frm target node and using our hashtable
+        while(!q.empty()){ //bfs to go upto k lvl frm target node and using our map
             int size=q.size();
             if(currlvl++ == k) break; //reched kth lvl
             for(int i=0;i<size;i++){
@@ -175,34 +82,3 @@ public:
  
 
 
-
-//{ Driver Code Starts.
-
-int main()
-{
-    int t;
-    cin>>t;
-    getchar();
-    
-    Solution x = Solution();
-    
-    while(t--)
-    {
-        string s;
-        getline(cin,s);
-        Node* head = buildTree(s);
-        
-        int target, k;
-        cin>> target >> k;
-        getchar();
-        
-        vector <int> res = x.KDistanceNodes(head, target, k);
-        
-        for( int i=0; i<res.size(); i++ )
-            cout<< res[i] << " ";
-        cout<<endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
