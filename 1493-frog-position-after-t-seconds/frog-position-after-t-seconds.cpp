15a10d19eddc97b70626a80584aad83445e2d22a
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-07-30 22:00:20 +0530

    Time: 3 ms (88.55%) | Memory: 21.1 MB (19.02%) - LeetSync

--- 1493-frog-position-after-t-seconds/frog-position-after-t-seconds.cpp
+++ 1493-frog-position-after-t-seconds/frog-position-after-t-seconds.cpp
@@ -1,3 +1,12 @@
+// - Start BFS from node 1 at time 0, with prob 1.0
+// - At each step:
+//   - Count unvis children
+//   - Divide prob equally among children
+// - If time t is reached:
+//   - If current node is target → return current prob
+//   - Else, continue processing (but we skip enqueuing further neighs)
+// - If the frog reaches a dead-end (no unvis children) before time t, it stays in place
+
 class Solution {
 public:
     double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
@@ -8,14 +17,16 @@ public:
             adj[v].push_back(u);
         }
         vector<int> vis(n+1,0);
-        queue<tuple<int,int,double>> q; //{node, time, probability}
+        queue<tuple<int,int,double>> q; //{node, time, prob}
         q.push({1,0,1.0});//start at node=1, w time=0 and prob=1.0
-        vis[1]=1;
+        vis[1]=1;//mark node 1 as vis
+        //bfs
         while(!q.empty()){
             auto [node,time,prob]=q.front(); q.pop();
-            if(time==t){
-                if(node==target) return prob;
-                continue;
+            if(time==t){//reached time limit t
+                if(node==target)//reached target
+                    return prob;
+                continue;//else, skip furthr trav
             }
             int unvisChildren=0;
             for(int neigh:adj[node]){
@@ -23,17 +34,19 @@ public:
                     unvisChildren++;
                 }
             }
-            if(unvisChildren==0){//dead end-frog stays here
+            // If no unvis children,
+            if(unvisChildren==0){//dead end-frog stays at curr node
                 if(node==target) return prob;
                 continue;
             }
+            //push unvis neighs into the queue with divided prob
             for(int neigh:adj[node]){
                 if(!vis[neigh]){
                     vis[neigh]=true;
                     q.push({neigh,time+1,prob/unvisChildren});
                 }
             }
-        }
-        return 0.0;
+        }        
+        return 0.0;//if target is never reached
     }
 };
