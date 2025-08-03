bf50c9f84326f232455504c2c1bc7da927b215c5
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-03 20:09:01 +0530

    Time: 78 ms (14.80%) | Memory: 74.7 MB (24.22%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -1,11 +1,9 @@
-#include <bits/stdc++.h>
-using namespace std;
+typedef pair<double,int> p;
 class Solution {//dijktsrtra
 public:
-    typedef pair<double,int> P;
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-        unordered_map<int,vector<pair<int,double>>> adj;
-        vector<double>res(n,0);
+        unordered_map<int,vector<pair<int,double>>> adj;//{node:{neigh,prob}}
+        vector<double>ans(n,0);
         for(int i=0;i<edges.size();i++){//make adj list
             int u=edges[i][0];
             int v=edges[i][1];
@@ -13,22 +11,23 @@ public:
             adj[u].push_back({v, prob});
             adj[v].push_back({u, prob});
         }
-        priority_queue<P> pq;//max heap
-        res[start_node]=1; //prob to reach start node is 1
+        priority_queue<p> pq;//max heap
+        ans[start_node]=1; //prob to reach start node is 1
         pq.push({1.0,start_node});
         while(!pq.empty()){
-            double currprob=pq.top().first;
-            int currnode=pq.top().second;
+            auto curr=pq.top();
+            double prob=curr.first;
+            int node=curr.second;
             pq.pop();
-            for(auto &neigh:adj[currnode]){
+            for(auto &neigh:adj[node]){
                 double adjprob=neigh.second;
                 int adjnode=neigh.first;
-                if(res[adjnode]<currprob*adjprob){
-                    res[adjnode]=currprob*adjprob;
-                    pq.push({res[adjnode],adjnode});
+                if(ans[adjnode]<prob*adjprob){
+                    ans[adjnode]=prob*adjprob;
+                    pq.push({ans[adjnode],adjnode});
                 }
             }
         }
-        return res[end_node];
+        return ans[end_node];
     }
 };
