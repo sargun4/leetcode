c15cfd17dd02f4231609f3bf817fa4ef4251734c
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-06 19:37:41 +0530

    Time: 1548 ms (25.76%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,14 @@
+SELECT name 
+FROM SalesPerson
+WHERE sales_id NOT IN 
+(SELECT sales_id FROM Orders
+WHERE com_id=(SELECT com_id FROM Company WHERE name='RED'));
+
+-- select s.name
+-- from SalesPerson s
+-- where s.name not in
+--     (select s.name
+--     from SalesPerson s
+--         left join Orders o on s.sales_id = o.sales_id
+--         left join Company c on o.com_id = c.com_id
+--     where c.name = 'Red')
