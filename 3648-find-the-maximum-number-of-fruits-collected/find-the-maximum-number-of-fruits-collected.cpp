3d615a10cf0ea94f48de2cb3320270e052a15d49
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-07 21:12:30 +0530

    Time: 108 ms (46.10%) | Memory: 157.1 MB (52.60%) - LeetSync

--- 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int n;
     vector<vector<int>> dp;
+    //child 1- Collects fruits along the main diag [i][i]
     int DiagChildCollect(vector<vector<int>>& fruits){
         int ctr=0;
         for(int i=0;i<n;i++){
@@ -9,17 +10,17 @@ public:
         }
         return ctr;
     }
+    //child 2:Top right to bottom
     int child2Collect(int i,int j,vector<vector<int>>& fruits){
         if(i>=n || j<0 || j>=n){
             return 0;
         }
         if(i==n-1 && j==n-1){
-            return 0;//cuz [n-1][n-1] was alr collectde by diag child
+            return 0;//dont collect cuz [n-1][n-1] was alr collectd by diag child
         }
-        if(i==j || i>j){//diag or beyond it-
+        if(i==j || i>j){//diag or beyond it
             return 0;
         }
-        
         if(dp[i][j]!=-1){
             return dp[i][j];
         }
@@ -29,6 +30,7 @@ public:
 
         return dp[i][j]= max({bottomLeft,bottomDown,bottomRight});
     }
+    //child 3:bttoom left to right
     int child3Collect(int i, int j,vector<vector<int>>& fruits){
         if(i>=n || j<0 || j>=n){
             return 0;
@@ -36,6 +38,7 @@ public:
         if(i==n-1 && j==n-1){
             return 0;//cuz [n-1][n-1] was alr collectde by diag child
         }
+        //skip diag or invalid region (child3)
         if(i==j || j>i){
             return 0;
         }
@@ -55,6 +58,6 @@ public:
         int c2=child2Collect(0,n-1,fruits);
         int c3=child3Collect(n-1,0,fruits);
 
-        return c1+c2+c3;
+        return c1+c2+c3;//total colectd
     }
 };
