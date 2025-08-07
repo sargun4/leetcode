21661dc88fee0546de16844c0ada68f18cd3d6ed
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-07 21:04:15 +0530

    Time: 98 ms (50.00%) | Memory: 157.2 MB (52.60%) - LeetSync

---
+++ 3648-find-the-maximum-number-of-fruits-collected/find-the-maximum-number-of-fruits-collected.cpp
@@ -0,0 +1,60 @@
+class Solution {
+public:
+    int n;
+    vector<vector<int>> dp;
+    int DiagChildCollect(vector<vector<int>>& fruits){
+        int ctr=0;
+        for(int i=0;i<n;i++){
+            ctr+=fruits[i][i];//only diag collect
+        }
+        return ctr;
+    }
+    int child2Collect(int i,int j,vector<vector<int>>& fruits){
+        if(i>=n || j<0 || j>=n){
+            return 0;
+        }
+        if(i==n-1 && j==n-1){
+            return 0;//cuz [n-1][n-1] was alr collectde by diag child
+        }
+        if(i==j || i>j){//diag or beyond it-
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int bottomLeft=fruits[i][j]+child2Collect(i+1,j-1,fruits);
+        int bottomDown=fruits[i][j]+child2Collect(i+1,j,fruits);
+        int bottomRight=fruits[i][j]+child2Collect(i+1,j+1,fruits);
+
+        return dp[i][j]= max({bottomLeft,bottomDown,bottomRight});
+    }
+    int child3Collect(int i, int j,vector<vector<int>>& fruits){
+        if(i>=n || j<0 || j>=n){
+            return 0;
+        }
+        if(i==n-1 && j==n-1){
+            return 0;//cuz [n-1][n-1] was alr collectde by diag child
+        }
+        if(i==j || j>i){
+            return 0;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int upRight=fruits[i][j]+child3Collect(i-1,j+1,fruits);
+        int right=fruits[i][j]+child3Collect(i,j+1,fruits);
+        int bottomRight=fruits[i][j]+child3Collect(i+1,j+1,fruits);
+
+        return dp[i][j]=max({upRight,right,bottomRight});
+    }
+    int maxCollectedFruits(vector<vector<int>>& fruits) {
+        n=fruits.size();
+        dp.resize(n,vector<int>(n,-1));
+        int c1=DiagChildCollect(fruits);
+        int c2=child2Collect(0,n-1,fruits);
+        int c3=child3Collect(n-1,0,fruits);
+
+        return c1+c2+c3;
+    }
+};
