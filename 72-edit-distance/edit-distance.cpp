5172e61edd26829688865a143fd6b71d77b7386f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-08 15:47:18 +0530

    Time: 61 ms (13.17%) | Memory: 81.7 MB (10.26%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,3 +1,37 @@
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(int idx1, int idx2, string word1, string word2) {
+    // If 1st string is empty, the only option is to
+    // insert all characters of 2nd string into 1st
+        if(idx1 < 0)
+            return idx2+1;
+    // If 2nd string is empty, the only option is to
+    // insert all characters of 1st string into 2nd
+        if(idx2<0)
+            return idx1+1;
+
+        if(dp[idx1][idx2]!=-1)
+            return dp[idx1][idx2];
+//if last chars r equal, no operation is needed for these positions-just solve for remaing part
+        if(word1[idx1] == word2[idx2])
+            return dp[idx1][idx2] = 0 + solve(idx1-1, idx2-1, word1, word2);     
+//3cases       
+    //insert word2[idx2] into word-this fixes the last char of word2, but idx1 stays the same,so move idx2 1 step left
+        int insert = 1 + solve(idx1, idx2-1, word1, word2);
+    //Delete word1[idx1]. This moves idx1 left, but idx2 stays the same.
+        int del = 1 + solve(idx1-1, idx2, word1, word2);
+    //Replace word1[idx1] with word2[idx2]-Move both indices left.
+        int replace = 1 + solve(idx1-1, idx2-1, word1, word2);
+
+        return dp[idx1][idx2] = min({insert, del, replace});
+    }
+    int minDistance(string word1, string word2) {
+        int n = word1.size(), m = word2.size();
+        dp.resize(n, vector<int>(m, -1));
+        return solve(n-1, m-1, word1, word2);
+    }
+};
 // class Solution {
 //  public:
 //   int minDistance(string word1, string word2) {
@@ -22,35 +56,3 @@
 //     return dp[m][n];
 //   }
 // };
-class Solution {
-public:
-    vector<vector<int>> dp;
-    int solve(int idx1, int idx2, string word1, string word2) {
-    // If 1st string is empty, the only option is to
-    // insert all characters of 2nd string into 1st
-        if(idx1 < 0)
-            return idx2+1;
-    // If 2nd string is empty, the only option is to
-    // insert all characters of 1st string into 2nd
-        if(idx2<0)
-            return idx1+1;
-
-        if(dp[idx1][idx2]!=-1)
-            return dp[idx1][idx2];
-
-        if(word1[idx1] == word2[idx2])
-            return dp[idx1][idx2] = 0 + solve(idx1-1, idx2-1, word1, word2);            
-
-        int insert = 1 + solve(idx1, idx2-1, word1, word2);
-        int del = 1 + solve(idx1-1, idx2, word1, word2);
-        int replace = 1 + solve(idx1-1, idx2-1, word1, word2);
-
-        return dp[idx1][idx2] = min({insert, del, replace});
-    }
-
-    int minDistance(string word1, string word2) {
-        int n = word1.size(), m = word2.size();
-        dp.resize(n, vector<int>(m, -1));
-        return solve(n-1, m-1, word1, word2);
-    }
-};
