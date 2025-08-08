a3d8cc8baa938d91840155af767c5b092ab94e2a
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-08 19:51:25 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (89.43%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.cpp
+++ 96-unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -1,29 +1,30 @@
-class Solution {//tab
-public:
-    int numTrees(int n) {
-        vector<int> dp(n+1);//dp[i]=no of unique BSTs using i nodes
-        dp[0]=dp[1]=1;//basecase:0 or 1 node = only 1 bst
-        for(int i=2;i<=n;i++) 
-            for(int j=1;j<=i;j++)
-                //for each j from 1 to i:
-                // -j as root
-                // -left subtree has (j-1) nodes
-                // -right subtree has (i-j) nodes
-                dp[i]+=dp[j-1]*dp[i-j];
-        return dp[n];
-    }
-};
-//memo
-// class Solution {
+// class Solution {//tab
 // public:
-//     int dp[20]{};
 //     int numTrees(int n) {
-//         if(n <= 1) return 1;
-//         if(dp[n]) return dp[n];
-//         for(int i = 1; i <= n; i++) 
-            // int left=numTrees(i-1);//trees in left subtree
-            // int right=numTrees(n-i);//trees in right subtree
-            // dp[n]+=left*right;//total combinations
+//         vector<int> dp(n+1);//dp[i]=no of unique BSTs using i nodes
+//         dp[0]=dp[1]=1;//basecase:0 or 1 node = only 1 bst
+//         for(int i=2;i<=n;i++) 
+//             for(int j=1;j<=i;j++)
+//                 //for each j from 1 to i:
+//                 // -j as root
+//                 // -left subtree has (j-1) nodes
+//                 // -right subtree has (i-j) nodes
+//                 dp[i]+=dp[j-1]*dp[i-j];
 //         return dp[n];
 //     }
 // };
+// memo
+class Solution {
+public:
+    int dp[20];
+    int numTrees(int n) {
+        if(n <= 1) return 1;
+        if(dp[n]) return dp[n];
+        for(int i = 1; i <= n; i++){
+            int left=numTrees(i-1);//trees in left subtree
+            int right=numTrees(n-i);//trees in right subtree
+            dp[n]+=left*right;//total combinations
+        }
+        return dp[n];
+    }
+};
