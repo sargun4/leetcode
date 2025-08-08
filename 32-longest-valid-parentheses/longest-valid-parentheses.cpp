8597f74dc2f306fd3093f0ceb2f31e36e708d377
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-08 15:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (23.52%) - LeetSync

--- 32-longest-valid-parentheses/longest-valid-parentheses.cpp
+++ 32-longest-valid-parentheses/longest-valid-parentheses.cpp
@@ -3,22 +3,22 @@ public:
     int longestValidParentheses(string s) {
         stack<int> st;
         st.push(-1);
-        int maxi=0;
+        int maxlen=0;
         int n=s.length();
         for(int i = 0; i < n; i++) {
             char c = s[i];
             if(c == '(') {
-                st.push(i); // Push the index of the '(' into stack
+                st.push(i); //push the index of the '(' into stack
             }else{
-                st.pop(); // Pop the top index as we found a matching ')'
+                st.pop(); //pop the top index as we found a matching ')'
                 if(st.empty()) {
-                    st.push(i); // Push the current index as a new base if stack is empty
+                    st.push(i); //push the current index as a new base if stack is empty
                 }else{
-                    int len = i - st.top();//length of the current valid substring
-                    maxi = max(maxi, len);
+                    int len=i-st.top();//len of the curr valid substring
+                    maxlen = max(maxlen,len);
                 }
             }
         }
-        return maxi;
+        return maxlen;
     }
 };
