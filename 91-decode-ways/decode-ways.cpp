ac5ee338c1d4bfdb171556dbca0afd265c363b1e
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-08 16:02:07 +0530

    Time: 1 ms (31.96%) | Memory: 10.8 MB (18.16%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,36 +1,35 @@
 #include<bits/stdc++.h>
 using namespace std;
-// class Solution {
-// public:
-//     vector<int> dp;
-//     //memoization-TD
-//     int str_to_int(string s){
-//         if(s.size()==1) return s[0]-'0';
-//         return (s[0]-'0')*10+(s[1]-'0');
-//     }
-//     int f(string s,int idx){
-//         //base case;
-//         if(s[idx]=='0') return 0;
-//         if(idx>=s.size()) return 0;
-//         if(idx==s.size()-1){
-//             if(s[idx]=='0') return 0;
-//             else return 1;
-//         }
-//         if(idx==s.size()-2){
-//             bool canform2digits=str_to_int(s.substr(idx,2))<=26;
-//             return f(s,idx+1)+(canform2digits);
-//         }
-
-//         if(dp[idx]!=-1) return dp[idx];
-//         bool canform2digits=str_to_int(s.substr(idx,2))<=26;
-//         return dp[idx]=f(s,idx+1)+((canform2digits) ? f(s,idx+2):0);
-//     }
-//     int numDecodings(string s){
-//         dp.clear();dp.resize(105,-1);
-
-//         return f(s,0);
-//     }
-// };
+class Solution {
+public:
+    vector<int> dp;
+    //memoization-TD
+    int str_to_int(string s){
+        if(s.size()==1) return s[0]-'0';
+        return (s[0]-'0')*10+(s[1]-'0');
+    }
+    int f(string s,int idx){
+        //base case;
+        if(s[idx]=='0') return 0;
+        if(idx>=s.size()) return 0;
+        if(idx==s.size()-1){
+            if(s[idx]=='0') return 0;
+            else return 1;
+        }
+        if(idx==s.size()-2){
+            bool canform2digits=str_to_int(s.substr(idx,2))<=26;
+            return f(s,idx+1)+(canform2digits);
+        }
+        if(dp[idx]!=-1) return dp[idx];
+        bool canform2digits=str_to_int(s.substr(idx,2))<=26;
+        return dp[idx]=f(s,idx+1)+((canform2digits) ? f(s,idx+2):0);
+    }
+    int numDecodings(string s){
+        dp.clear();
+        dp.resize(105,-1);
+        return f(s,0);
+    }
+};
 
 // //memo
 // class Solution {
@@ -63,27 +62,27 @@ using namespace std;
 // // #dp[i]=no. of decode ways for str s frm idx i to n
 
 
-//tabulation
-class Solution {
-public:
-    int dp[101];
+// //tabulation
+// class Solution {
+// public:
+//     int dp[101];
     
-    int numDecodings(string s){
-        int n=s.length();
-        vector<int> dp(n+1,0);
-        dp[n]=1;
-        for(int i=n-1;i>=0;i--){
-            if(s[i]=='0'){
-                dp[i]=0;
-            }else{
-                dp[i]=(dp[i+1]); //single digit
-                if(i+1<n){
-                    if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6')){
-                        dp[i]+=dp[i+2];
-                    }
-                }
-            }
-        }
-        return dp[0]; //s[0 to n] full string ka decode ways
-    }
-};  
+//     int numDecodings(string s){
+//         int n=s.length();
+//         vector<int> dp(n+1,0);
+//         dp[n]=1;
+//         for(int i=n-1;i>=0;i--){
+//             if(s[i]=='0'){
+//                 dp[i]=0;
+//             }else{
+//                 dp[i]=(dp[i+1]); //single digit
+//                 if(i+1<n){
+//                     if(s[i]=='1'||(s[i]=='2' && s[i+1]<='6')){
+//                         dp[i]+=dp[i+2];
+//                     }
+//                 }
+//             }
+//         }
+//         return dp[0]; //s[0 to n] full string ka decode ways
+//     }
+// };  
