b35db406865fcf59c2fcd87201f5873d3553badf
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-08 15:28:17 +0530

    Time: 0 ms (100.00%) | Memory: 11.2 MB (96.66%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,24 +1,24 @@
-//memoization
 class Solution {
 public:
-    int m,n;
-    int t[101][101];
-    int solve(vector<vector<int>>& grid,int i,int j){
-        if(i<0||i>=m||j<0||j>=n||grid[i][j]==1){
+    int dp[101][101];
+    int solve(int i,int j,int m,int n,vector<vector<int>>& grid){
+        if(i<0|| j<0|| i>=m || j>=n || grid[i][j]==1){
             return 0;
         }
-        if(t[i][j]!=-1){
-            return t[i][j]; 
+        if(dp[i][j]!=-1){
+            return dp[i][j];
         }
-        if(i==m-1 && j==n-1) return 1;
-        int right=solve(grid,i,j+1);
-        int down=solve(grid,i+1,j);
-        return t[i][j]=right+down;
+        if(i==m-1 && j==n-1){//reached bottom right cell
+            return 1;
+        }
+        int down=solve(i+1,j,m,n,grid);
+        int right=solve(i,j+1,m,n,grid);
+        return dp[i][j]=down+right;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        m=obstacleGrid.size();
-        n=obstacleGrid[0].size();
-        memset(t,-1,sizeof(t));
-        return solve(obstacleGrid,0,0);
+    int uniquePathsWithObstacles(vector<vector<int>>& grid){
+        int m=grid.size();
+        int n=grid[0].size();
+        memset(dp,-1,sizeof(dp));
+        return solve(0,0,m,n,grid);
     }
 };
