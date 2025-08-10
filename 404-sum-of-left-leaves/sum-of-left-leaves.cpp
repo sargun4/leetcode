49fec4dadfae2afa09201b34fcac39df9beea7fd
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-10 19:44:16 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (24.10%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -1,28 +1,47 @@
-
-
 class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
-        int res = 0; 
-        // Update result if root is not NULL
-        if (root != NULL){
-            // If left of root is NULL, then add val of
-            // left child
-            if (isLeaf(root->left))
-                res += root->left->val;
-            else // Else recur for left child of root
-                res += sumOfLeftLeaves(root->left);
-                
-            // Recur for right child of root and update res
-            res += sumOfLeftLeaves(root->right);
+        int res = 0;
+        queue<TreeNode*> q;
+        q.push(root);
+        int sum=0;
+        while(!q.empty()){
+            auto node=q.front(); q.pop();
+            if(isLeaf(node->left)){
+                sum+=node->left->val;
+            }
+            if(node->left) q.push(node->left);
+            if(node->right) q.push(node->right);
         }
-        return res;
+        return sum;
     }
     bool isLeaf(TreeNode*node){
-        if (node == NULL)
-            return false;
-        if (node->left == NULL && node->right == NULL)
-            return true;
+        if(!node) return false;
+        if(!node->left && !node->right) return true;
         return false;
     }
 };
+////dfs
+// class Solution {
+// public:
+//     int sumOfLeftLeaves(TreeNode* root) {
+//         int res = 0;
+//         //update res if root is not NULL
+//         if (root != NULL){
+//             //if left of root = NULL, then add val of left child
+//             if (isLeaf(root->left))
+//                 res += root->left->val;
+//             else //else recur for left child of root
+//                 res += sumOfLeftLeaves(root->left);
+                
+//             //recur for right child of root and update res
+//             res += sumOfLeftLeaves(root->right);
+//         }
+//         return res;
+//     }
+//     bool isLeaf(TreeNode*node){
+//         if (!node) return false;
+//         if (!node->left && !node->right) return true;
+//         return false;
+//     }
+// };
