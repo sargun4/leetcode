9c2c2816b6c9b164d9970833313271e9188e8883
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-10 19:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (73.82%) - LeetSync

--- 257-binary-tree-paths/binary-tree-paths.cpp
+++ 257-binary-tree-paths/binary-tree-paths.cpp
@@ -1,26 +1,47 @@
-//bfs
+// //bfs
+// class Solution {
+// public:
+//     vector<string> binaryTreePaths(TreeNode* root) {
+//         vector<string> ans;
+//         queue<pair<TreeNode*,string>> q;
+//         q.push({root,to_string(root->val)});
+//         while(!q.empty()){
+//             auto curr=q.front(); q.pop();
+//             TreeNode* node=curr.first;
+//             if(node){
+//                 auto currpath=curr.second;
+//                 if(!node->left && !node->right){//reached leaf
+//                     ans.push_back(currpath);
+//                 }
+//                 if(node->left){
+//                     q.push({node->left,currpath+"->"+to_string(node->left->val)});
+//                 }
+//                 if(node->right){
+//                     q.push({node->right,currpath+"->"+to_string(node->right->val)});
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
+// };
+
+//dfs
 class Solution {
 public:
     vector<string> binaryTreePaths(TreeNode* root) {
-        vector<string> ans;
-        queue<pair<TreeNode*,string>> q;
-        q.push({root,to_string(root->val)});
-        while(!q.empty()){
-            auto curr=q.front(); q.pop();
-            TreeNode* node=curr.first;
-            if(node){
-                auto currpath=curr.second;
-                if(!node->left && !node->right){//reached leaf
-                    ans.push_back(currpath);
-                }
-                if(node->left){
-                    q.push({node->left,currpath+"->"+to_string(node->left->val)});
-                }
-                if(node->right){
-                    q.push({node->right,currpath+"->"+to_string(node->right->val)});
-                }
-            }
+        vector<string> res;
+        dfs(root,res,"");
+        return res;
+    }
+    void dfs(TreeNode*root,vector<string>&res,string path){
+        if(!root)return;
+        path+=to_string(root->val);
+        if(!root->left && !root->right){//leaf
+            res.push_back(path);
+        }else{//not yet reached leaf
+            path+="->";
         }
-        return ans;
+        if(root->left) dfs(root->left,res,path);
+        if(root->right) dfs(root->right,res,path);
     }
 };
