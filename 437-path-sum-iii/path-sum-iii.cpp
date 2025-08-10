d08291b6d74aa95d4566652f9ceb477ae9a0abf2
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-10 20:01:50 +0530

    Time: 13 ms (32.38%) | Memory: 19.2 MB (48.96%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.cpp
+++ 437-path-sum-iii/path-sum-iii.cpp
@@ -22,7 +22,6 @@ public:
             q.pop();
             sum=0;
             ctr+=count(node,(long long)targetSum);//count all paths starting at this node
-
             if(node->left) q.push(node->left);
             if(node->right) q.push(node->right);
         }
