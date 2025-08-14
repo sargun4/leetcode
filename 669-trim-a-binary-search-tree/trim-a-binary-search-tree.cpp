ce2cc9ff0b6bd2f46352ee0919a3db4d7783f75f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-14 19:43:37 +0530

    Time: 0 ms (100.00%) | Memory: 24.9 MB (58.90%) - LeetSync

--- 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
+++ 669-trim-a-binary-search-tree/trim-a-binary-search-tree.cpp
@@ -26,21 +26,21 @@ class Solution {
 public:
     TreeNode* trimBST(TreeNode* root, int low, int high) {
         if(root == NULL) return NULL;
-        // If the curr node's value is less than low,
-        // then we need to trim its left subtree and return the trimmed right subtree->
+        //if the curr node's value is less than low,
+        //then we need to trim its left subtree and return the trimmed right subtree
         if (root->val < low) {
             return trimBST(root->right, low, high);
         }
-        // If the curr node's value is greater than high,
-        // then we need to trim its right subtree and return the trimmed left subtree->
+        //if the curr node's value is greater than high,
+        //then we need to trim its right subtree and return the trimmed left subtree
         if (root->val > high) {
             return trimBST(root->left, low, high);
         }
-        // If the curr node's value is within the range [low, high],
-         // then we need to recursively trim both its left and right subtrees->
+        //if the curr node's val is within the range [low, high],
+         // then we need to recursively trim both its left and right subtrees
         root->left=trimBST(root->left, low, high);
         root->right=trimBST(root->right, low, high);
-        // Return the updated root after the trimming process->
+        // Return the updated root after the trimming process
         return root;
     }
 };
