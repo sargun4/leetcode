1c1683b8d4637a198ddfcf82617b3ffcf73ef76e
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-15 22:52:11 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (76.89%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,38 +1,33 @@
-// #include <bits/stdc++.h>
-// using namespace std;
-// //memo- O(n)
-// class Solution {
-// public:
-//     int dp[101];
-//     int solve(vector<int>& nums,int i,int n){
-//         if(i>n){
-//             return 0;
-//         }
-//         if(dp[i]!=-1) return dp[i];
-//         int steal=nums[i]+solve(nums,i+2,n);//steals ith house & moves to i+2 since we cant rob adjacent house
-//         int skip=solve(nums,i+1,n);//skips this house, and move to next hoeus
-//         return dp[i]=max(steal,skip);
-//     }
-//     int rob(vector<int>& nums) {
-//         int n=nums.size();
-//         if(n==1) return nums[0];
-//         if(n==2) return max(nums[0],nums[1]);
+//memo- O(n)
+class Solution {
+public:
+    int dp[101];
+    int solve(vector<int>& nums,int i,int n){
+        if(i>n){
+            return 0;
+        }
+        if(dp[i]!=-1) return dp[i];
+        int steal=nums[i]+solve(nums,i+2,n);//steals ith house & moves to i+2 since we cant rob adjacent house
+        int skip=solve(nums,i+1,n);//skips this house, and move to next hoeus
+        return dp[i]=max(steal,skip);
+    }
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
 
-//         memset(dp,-1,sizeof(dp));
-//         //case1=take first house(0th idx)
-//         int take_0thidx_house=solve(nums,0,n-2);
+        memset(dp,-1,sizeof(dp));
+        //case1=take first house(0th idx)
+        int take_0thidx_house=solve(nums,0,n-2);
 
-//         memset(dp,-1,sizeof(dp));
-//         //case2=take second house(1st idx)
-//         int take_1stidx_house=solve(nums,1,n-1);
+        memset(dp,-1,sizeof(dp));
+        //case2=take second house(1st idx)
+        int take_1stidx_house=solve(nums,1,n-1);
 
-//         return max(take_0thidx_house,take_1stidx_house);
-//     }
-// };
-
-
-#include <bits/stdc++.h>
-using namespace std;
+        return max(take_0thidx_house,take_1stidx_house);
+    }
+};
+ 
 // //O(n)
 // //tab
 // class Solution {
@@ -64,26 +59,26 @@ using namespace std;
         
 //     }
 // };
-class Solution {
-public:
-    int solve(vector<int>& nums,int l,int r){
-        int prev=0;
-        int prevprev=0;
-        for(int i=l;i<=r;i++){
-            int skip=prev;
-            int take=nums[i]+prevprev;
-            int temp=max(skip,take);
-            prevprev=prev;
-            prev=temp;
-        }
-        return prev;
-    }
-    int rob(vector<int>& nums) {
-        int n=nums.size();
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
-        int take1sthouse=solve(nums,0,n-2);
-        int skip1sthouse=solve(nums,1,n-1);
-        return max(take1sthouse,skip1sthouse);
-    }
-};
+// class Solution {
+// public:
+//     int solve(vector<int>& nums,int l,int r){
+//         int prev=0;
+//         int prevprev=0;
+//         for(int i=l;i<=r;i++){
+//             int skip=prev;
+//             int take=nums[i]+prevprev;
+//             int temp=max(skip,take);
+//             prevprev=prev;
+//             prev=temp;
+//         }
+//         return prev;
+//     }
+//     int rob(vector<int>& nums) {
+//         int n=nums.size();
+//         if(n==1) return nums[0];
+//         if(n==2) return max(nums[0],nums[1]);
+//         int take1sthouse=solve(nums,0,n-2);
+//         int skip1sthouse=solve(nums,1,n-1);
+//         return max(take1sthouse,skip1sthouse);
+//     }
+// };
