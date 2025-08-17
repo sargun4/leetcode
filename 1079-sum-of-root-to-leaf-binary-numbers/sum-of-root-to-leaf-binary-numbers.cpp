7495d11817ccecb5d1c0359bf5de6e4c6da7a06d
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-18 02:27:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (33.50%) - LeetSync

--- 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
+++ 1079-sum-of-root-to-leaf-binary-numbers/sum-of-root-to-leaf-binary-numbers.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int binToDec(string &s){
-        int k=0; int deci=0;
+        int k=0;//power of 2 keep incrementng
+        int deci=0;
         int n=s.size();
         for(int idx=n-1;idx>=0;idx--){
             deci+=pow(2,k++)*(s[idx]-'0');
@@ -10,7 +11,7 @@ public:
     }
     int sumRootToLeaf(TreeNode* root) {
         int sum=0;
-        queue<pair<TreeNode*,string>>q;
+        queue<pair<TreeNode*,string>>q;//{node,path}
         q.push({root,""});
         while(!q.empty()){
             int n=q.size();
