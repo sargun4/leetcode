1c3af36a04e7d8ad55c74c8eeff580c88dd406e8
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-18 14:09:42 +0530

    Time: 0 ms (100.00%) | Memory: 13.6 MB (83.50%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.cpp
@@ -1,38 +1,46 @@
-// class Solution { //O(n^2) brute force
-// public:
-//     int maxdiff;
-//     void FindMaxDiffUtil(TreeNode* root,TreeNode* child){
-//         if(root==NULL || child==NULL) return;
-//         maxdiff=max(maxdiff,abs(root->val-child->val));
-//         FindMaxDiffUtil(root,child->left);
-//         FindMaxDiffUtil(root,child->right);
-//     }
-//     void FindMaxDiff(TreeNode*root){
-//         if(root==NULL) return;
-//         FindMaxDiffUtil(root,root->left);
-//         FindMaxDiffUtil(root,root->right);
-
-//         FindMaxDiff(root->left);
-//         FindMaxDiff(root->right);
-//     }
-//     int maxAncestorDiff(TreeNode* root) {
-//         maxdiff=-1;
-//         FindMaxDiff(root);
-//         return maxdiff;
-//     }
-// };
 
 class Solution {
 public:
-    int FindMaxDiff(TreeNode* root,int minval,int maxval){
-        if(root==NULL) return abs(minval-maxval);
-        minval=min(minval,root->val);
-        maxval=max(maxval,root->val);
-        int l=FindMaxDiff(root->left,minval,maxval);
-        int r=FindMaxDiff(root->right,minval,maxval);
-        return max(l,r);
-    }
     int maxAncestorDiff(TreeNode* root){
-        return FindMaxDiff(root,root->val,root->val);
+        queue<pair<TreeNode*,pair<int,int>>> q; //{node:{max,min}};
+        q.push({root,{root->val,root->val}});
+        int ans=0;
+        while(!q.empty()){
+            int n=q.size();
+            while(n--){
+                auto curr=q.front(); q.pop();
+                TreeNode* node=curr.first;
+                int mx=curr.second.first;
+                int mi=curr.second.second;
+                //at each lvl,compare each node val
+                if(node->left){
+                    int maxval=max(mx,node->left->val);
+                    int minval=min(mi,node->left->val);
+                    q.push({node->left,{maxval,minval}});
+                    ans=max(ans,maxval-minval);
+                }
+                if(node->right){
+                    int maxval=max(mx,node->right->val);
+                    int minval=min(mi,node->right->val);
+                    q.push({node->right,{maxval,minval}});
+                    ans=max(ans,maxval-minval);
+                }
+            }
+        }
+        return ans;
     }
 };
+// class Solution {
+// public:
+//     int FindMaxDiff(TreeNode* root,int minval,int maxval){
+//         if(root==NULL) return abs(minval-maxval);
+//         minval=min(minval,root->val);
+//         maxval=max(maxval,root->val);
+//         int l=FindMaxDiff(root->left,minval,maxval);
+//         int r=FindMaxDiff(root->right,minval,maxval);
+//         return max(l,r);
+//     }
+//     int maxAncestorDiff(TreeNode* root){
+//         return FindMaxDiff(root,root->val,root->val);
+//     }
+// };
