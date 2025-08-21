91b13064a5b25ce76865eb4b7712890b945db67d
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-21 16:46:52 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (20.38%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.cpp
@@ -1,17 +1,41 @@
 class Solution {
 public:
-    int solve(TreeNode* root,int curr){
-        if(!root) return 0;
-        curr=curr*10+root->val;
-        //leaf node
-        if(root->left==NULL && root->right==NULL){
-            return curr;
-        }
-        int l=solve(root->left,curr);
-        int r=solve(root->right,curr);
-        return l+r;
-    }
     int sumNumbers(TreeNode* root) {
-        return solve(root,0);
+        if(!root) return 0;
+        queue<pair<TreeNode*,int>> q;//{node,sum}
+        q.push({root,root->val});
+        int totalsum=0;
+        int sum; TreeNode* node;
+        while(!q.empty()){
+            auto curr=q.front(); q.pop();
+            node=curr.first;
+            sum=curr.second;
+            if(!node->left && !node->right){//leaf node
+                totalsum+=sum;
+            }else{
+                if(node->left){
+                    q.push({node->left,sum*10+node->left->val});
+                }if(node->right){
+                    q.push({node->right,sum*10+node->right->val});
+                }
+            }
+        }
+        return totalsum;
     }
 };
+// class Solution {
+// public:
+//     int solve(TreeNode* root,int curr){
+//         if(!root) return 0;
+//         curr=curr*10+root->val;
+//         if(root->left==NULL && root->right==NULL){//leaf node
+//             return curr;
+//         }
+//         int l=solve(root->left,curr);
+//         int r=solve(root->right,curr);
+//         return l+r;
+//     }
+//     int sumNumbers(TreeNode* root) {
+//         return solve(root,0);
+//     }
+// };
