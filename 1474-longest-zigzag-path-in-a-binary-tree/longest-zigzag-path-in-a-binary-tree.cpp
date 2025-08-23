658a96a058ca18cd9f2ffdb62f3824ea1e680932
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-23 15:33:33 +0530

    Time: 3 ms (65.06%) | Memory: 100.2 MB (22.85%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -8,7 +8,7 @@
 //              solve(root->left,!flag,ctr+1);
 //              solve(root->right,1,1);
 //         }else{
-//               solve(root->right,!flag,ctr+1);
+//              solve(root->right,!flag,ctr+1);
 //              solve(root->left,0,1);
 //         }
 //     }
@@ -26,14 +26,14 @@ public:
     int longestZigZag(TreeNode* root) {
         int ans=0;
         queue<pair<TreeNode*,pair<int,int>>>q; //{node, {ans, dir}}
-        q.push({root,{0, 0}});
+        q.push({root,{0,0}});
         while(!q.empty()){
             auto p=q.front();
             q.pop();
             TreeNode* node = p.first;
-            int ctr = p.second.first;//len of longest zigzag path till this node
-            int dir = p.second.second;//dirn = 1 or -1
-            ans=max(ans, ctr);
+            int ctr=p.second.first;//len of longest zigzag path till this node
+            int dir=p.second.second;//dirn = 1 or -1
+            ans=max(ans,ctr);
             if(node->left){
                 if(dir==1) //if right-to-left, 
                     q.push({node->left, {ctr+1, -1}});//enqueue l child w (ctr+1) & dirn -1
