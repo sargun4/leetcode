91a55f16c8ddd47a12074d1400872cc3d68c600d
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-23 15:14:55 +0530

    Time: 15 ms (27.69%) | Memory: 100.2 MB (22.85%) - LeetSync

--- 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
+++ 1474-longest-zigzag-path-in-a-binary-tree/longest-zigzag-path-in-a-binary-tree.cpp
@@ -10,7 +10,6 @@
 //         }else{
 //               solve(root->right,!flag,ctr+1);
 //              solve(root->left,0,1);
-
 //         }
 //     }
 //     int longestZigZag(TreeNode* root) {
@@ -25,29 +24,27 @@
 class Solution {
 public:
     int longestZigZag(TreeNode* root) {
-        int ans = 0;
+        int ans=0;
         queue<pair<TreeNode*,pair<int,int>>>q; //{node, {ans, dir}}
-        q.push({root, {0, 0}});
+        q.push({root,{0, 0}});
         while(!q.empty()){
-            auto p = q.front();
+            auto p=q.front();
             q.pop();
             TreeNode* node = p.first;
-            int ctr = p.second.first;
-            int dir = p.second.second;
-
-            ans = max(ans, ctr);
-
+            int ctr = p.second.first;//len of longest zigzag path till this node
+            int dir = p.second.second;//dirn = 1 or -1
+            ans=max(ans, ctr);
             if(node->left){
                 if(dir==1) //if right-to-left, 
-                    q.push({node->left, {ctr+1, -1}}); //enqueue left child w (ctr + 1) & dirn -1.
+                    q.push({node->left, {ctr+1, -1}});//enqueue l child w (ctr+1) & dirn -1
                 else
-                    q.push({node->left, {1, -1}});//enqueue left child w a path length of 1 & dirn -1.
+                    q.push({node->left, {1, -1}});//enqueue left child w a path len of 1 & dirn -1
             }
             if(node->right){
-                if(dir==-1)  //left-to-right,
-                    q.push({node->right, {ctr+1, 1}});//enqueue right child w (ctr + 1) & dirn 1.
+                if(dir==-1)//left-to-right,
+                    q.push({node->right, {ctr+1, 1}});//enqueue right child w (ctr+1) & dirn 1
                 else
-                    q.push({node->right, {1, 1}});//enqueue right child w a path length of 1 & dirn 1.
+                    q.push({node->right, {1, 1}});//enqueue right child w a path len of 1 & dirn 1
             }
         }
         return ans;
