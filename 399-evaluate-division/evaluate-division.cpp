84cc3c54e13c74906e5623d57f0204d27afc919b
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-23 17:14:55 +0530

    Time: 0 ms (100.00%) | Memory: 11.7 MB (90.56%) - LeetSync

--- 399-evaluate-division/evaluate-division.cpp
+++ 399-evaluate-division/evaluate-division.cpp
@@ -3,22 +3,23 @@ class Solution {
 public:
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries){
         //make directed adj frm the nodes in equations
-    //for each equation (a/b=k), add edges (a,b) & (b,a)to the adj with edge values k & 1/k, respectively
-        unordered_map<string,unordered_map<string,double>> adj;//{num:{}}
+      //for each equation (a/b=k), add edges (a,b) & (b,a)to the adj with edge values k & 1/k, respectively
+        unordered_map<string,unordered_map<string,double>> adj;
+        //{node:{neigh,wt}};
         for(int i=0;i<equations.size();i++){
-            string dividend=equations[i][0];
-            string divisor=equations[i][1];
-            double val=values[i];
-            adj[dividend][divisor]=val;
-            adj[divisor][dividend]=1/val;
-        } 
+            string dividend=equations[i][0];//num
+            string divisor=equations[i][1];//denom
+            double val=values[i];//division val
+            adj[dividend][divisor]=val;// a->b = k
+            adj[divisor][dividend]=1/val;// b->a = 1/k
+        }
 
         vector<double> res;
 
         for (auto& query : queries) {
             string dividend = query[0];
             string divisor = query[1];
-            if (adj.find(dividend) == adj.end() || adj.find(divisor) == adj.end()) {//if y isnt reached or either x or y is not present in adj, return -1.
+            if (adj.find(dividend) == adj.end() || adj.find(divisor) == adj.end()) {//if either x or y is not present in adj, return -1.
                 res.push_back(-1.0);
             } else {
                 res.push_back(bfs(dividend, divisor, adj));
@@ -26,15 +27,16 @@ public:
         }
         return res;
     }
+    //bfs to get path frm start to end
     double bfs(string&start,string&end,unordered_map<string,unordered_map<string,double>> &adj){
         queue<pair<string,double>> q; //{node,prod_till_this_node};
-        unordered_set<string> vis;
-        q.push({start,1.0});
+        unordered_set<string> vis;//to avoid cycles
+        q.push({start,1.0});//start node,prod=1;
         while(!q.empty()){
             auto curr=q.front(); q.pop();
             string node=curr.first;
             double val=curr.second;
-            if(node==end) return val;
+            if(node==end) return val;//found target node
             vis.insert(node);
             for(auto&neigh:adj[node]){
                 string neighNode=neigh.first;
