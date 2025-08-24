5a46b6e01a24db3223cd6414c9aaeb163e2dbd70
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-24 19:56:46 +0530

    Time: 15 ms (57.93%) | Memory: 65.8 MB (48.27%) - LeetSync

--- 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
+++ 1285-balance-a-binary-search-tree/balance-a-binary-search-tree.cpp
@@ -1,4 +1,3 @@
- 
 class Solution {
 private:
     void inorder(TreeNode* root, vector<int>& in){
@@ -9,20 +8,20 @@ private:
         in.push_back(root->val);
         inorder(root->right, in); 
     } 
-    TreeNode* inorderToBst(int s, int e, vector<int>& inorderVal){
+    TreeNode* inorderToBst(int s, int e, vector<int>& inorderArr){
         if(s>e){
             return NULL; 
         } 
         int mid= (s+e)/2; 
-        TreeNode* root = new TreeNode(inorderVal[mid]);
-        root->left = inorderToBst(s,mid-1,inorderVal);
-        root->right = inorderToBst(mid+1,e,inorderVal); 
+        TreeNode* root = new TreeNode(inorderArr[mid]);
+        root->left = inorderToBst(s,mid-1,inorderArr);
+        root->right = inorderToBst(mid+1,e,inorderArr); 
         return root; 
     }
 public:
     TreeNode* balanceBST(TreeNode* root) { 
-        vector<int> inorderVal;
-        inorder(root,inorderVal); 
-        return inorderToBst(0, inorderVal.size()-1, inorderVal);
+        vector<int> inorderArr;
+        inorder(root,inorderArr); 
+        return inorderToBst(0, inorderArr.size()-1, inorderArr);
     }
 };
