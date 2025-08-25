55cd64b3c860bf319d09a8c11b1f52123c6b728f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-08-25 11:54:01 +0530

    Time: 31 ms (21.63%) | Memory: 31.3 MB (13.50%) - LeetSync

--- 498-diagonal-traverse/diagonal-traverse.cpp
+++ 498-diagonal-traverse/diagonal-traverse.cpp
@@ -1,18 +1,18 @@
-#include <bits/stdc++.h>
-using namespace std;
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& mat) {
         int m=mat.size();
         int n=mat[0].size();
         vector<int> res;
-        map<int,vector<int>> mp;
+        //for any element mat[i][j], the diag it belongs to is uniquely determined by (i + j).
+        map<int,vector<int>> mp; //{diag_idx:{num1,num2...}}
         //fill the map using =i+j
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 mp[i+j].push_back(mat[i][j]);
             }
         }
+        //map= {{0:{1},1:{2,4},2:{3,5,7},3:{6,8},4:{9}}};
         bool flip=true;
         for(auto &it:mp){
             if(flip){
@@ -21,7 +21,7 @@ public:
             for(int &num:it.second){
                 res.push_back(num);
             }
-            flip=!flip;
+            flip=!flip;//flip dirn fr zig zag
         }
         return res;
     }
