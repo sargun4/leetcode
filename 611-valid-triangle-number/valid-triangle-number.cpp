509c6b3fec0937de4174c26abcd30486f120bd02
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-09-26 19:12:30 +0530

    Time: 185 ms (15.38%) | Memory: 16.7 MB (42.88%) - LeetSync

---
+++ 611-valid-triangle-number/valid-triangle-number.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int binSearch(vector<int>& nums,int l,int r,int target){
+        int k=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(nums[mid]<target){
+                k=mid;//valid
+                l=mid+1;
+            }else{
+                r=mid-1;
+            }
+        }
+        return k;
+    }
+    int triangleNumber(vector<int>& nums) {
+        int n=nums.size();
+        if(n<3) return 0;
+        sort(begin(nums),end(nums));
+        int ctr=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]==0){//side len =0 then no traingle possible
+                continue;
+            }
+            for(int j=i+1;j<n;j++){
+                int sum=nums[i]+nums[j];
+                int k=binSearch(nums,j+1,n-1,sum);
+                if(k!=-1){
+                    ctr+=(k-j);
+                }
+            }
+        }
+        return ctr;
+    }
+};
