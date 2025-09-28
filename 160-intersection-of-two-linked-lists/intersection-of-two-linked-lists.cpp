7707c0a1190a2fbc5f119eb518af3777d3aca361
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-09-28 17:43:16 +0530

    Time: 38 ms (48.28%) | Memory: 18.4 MB (98.42%) - LeetSync

--- 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/intersection-of-two-linked-lists.cpp
@@ -1,60 +1,60 @@
-// #1
-// Time Complexity : O(m + n)
-// Space Complexity : O(m)
-class Solution {
-public:
-    ListNode*getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* curr=headA;
-        unordered_set<ListNode*> addr_set;
-        while(curr!=NULL){
-            addr_set.insert(curr);
-            curr=curr->next;
-        }
-        curr=headB;
-        ListNode*intersecnode=NULL;
-        while(curr!=NULL){
-            if(addr_set.find(curr)!=addr_set.end()){
-                intersecnode=curr;
-                break;
-            }
-            curr=curr->next;
-        }
-        return intersecnode;
-    }
-};
-// #2
-// Time Complexity : O(m + n)
-// Space Complexity : O(1)
-// class Solution { 
+// // #1
+// // Time Complexity : O(m + n)
+// // Space Complexity : O(m)
+// class Solution {
 // public:
 //     ListNode*getIntersectionNode(ListNode *headA, ListNode *headB) {
-//         ListNode*curr=headA;
-//         int a=0,b=0;//a=len(1st LL), b=len(2nd LL)
-//         while (curr!=NULL){
-//             a++;
+//         ListNode* curr=headA;
+//         unordered_set<ListNode*> addr_set;
+//         while(curr!=NULL){
+//             addr_set.insert(curr);
 //             curr=curr->next;
 //         }
 //         curr=headB;
-//         while (curr!=NULL){
-//             b++;
+//         ListNode*intersecnode=NULL;
+//         while(curr!=NULL){
+//             if(addr_set.find(curr)!=addr_set.end()){
+//                 intersecnode=curr;
+//                 break;
+//             }
 //             curr=curr->next;
 //         }
-//         int diff=abs(a-b);
-//         if(a<b){
-//             while(diff--){
-//                 headB=headB->next;
-//             }
-//         }else{
-//             while(diff--){
-//                 headA=headA->next;
-//             }
-//         }
-//         while(headA && headB){
-//             if(headA==headB) return headA;
-//             headA=headA->next;
-//             headB=headB->next;
-//         }
-//         return NULL;
+//         return intersecnode;
 //     }
 // };
+// #2
+// Time Complexity : O(m + n)
+// Space Complexity : O(1)
+class Solution { 
+public:
+    ListNode*getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode*curr=headA;
+        int a=0,b=0;//a=len(1st LL), b=len(2nd LL)
+        while (curr!=NULL){
+            a++;
+            curr=curr->next;
+        }
+        curr=headB;
+        while (curr!=NULL){
+            b++;
+            curr=curr->next;
+        }
+        int diff=abs(a-b);
+        if(a<b){
+            while(diff--){
+                headB=headB->next;
+            }
+        }else{
+            while(diff--){
+                headA=headA->next;
+            }
+        }
+        while(headA && headB){
+            if(headA==headB) return headA;
+            headA=headA->next;
+            headB=headB->next;
+        }
+        return NULL;
+    }
+};
  
