ea2486e2249c1bf3ec2189c41be76d6954cd7a5f
sargun4 <132887555+sargun4@users.noreply.github.com>
2025-09-30 18:18:51 +0530

    Time: 3 ms (17.98%) | Memory: 50.3 MB (34.56%) - LeetSync

--- 485-max-consecutive-ones/max-consecutive-ones.cpp
+++ 485-max-consecutive-ones/max-consecutive-ones.cpp
@@ -8,14 +8,14 @@ public:
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] == 1) {
                 currlen++;
-            } else {
+            }else{
                 // Update the maximum length if the current length is greater
                 maxlen = max(maxlen, currlen);
-                // Reset the current length for consecutive zeros
+                //Reset the current length for consecutive zeros
                 currlen = 0;
             }
         }
-        // Update the maximum length after the loop (in case the sequence ends with ones)
+        //Update the maximum length after the loop (in case the sequence ends with ones)
         maxlen = max(maxlen, currlen);
         return maxlen;
     }
